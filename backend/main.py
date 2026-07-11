30e9068966bf1b843fb1877ef38df0d8adf70867
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-10 21:19:58 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,24 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.api.router import router
 from app.core.version import APP_NAME, APP_VERSION
+from app.database.base import Base
+from app.database.database import engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Cria automaticamente todas as tabelas do banco
+    Base.metadata.create_all(bind=engine)
+    yield
+
 
 app = FastAPI(
     title=APP_NAME,
     version=APP_VERSION,
+    lifespan=lifespan,
 )
 
 app.include_router(router)
