c6a1d951fce0d6cf87ed2da9a1b8351072361db2
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-13 11:34:58 -0300

    Implement ProjectRepository with CRUD methods

--- backend/app/repositories/project_repository.py
+++ backend/app/repositories/project_repository.py
@@ -1,1 +1,26 @@
+from sqlalchemy.orm import Session
 
+from app.models.project import Project
+
+
+class ProjectRepository:
+
+    @staticmethod
+    def list(db: Session):
+        return db.query(Project).all()
+
+    @staticmethod
+    def get(db: Session, project_id: str):
+        return db.query(Project).filter(Project.id == project_id).first()
+
+    @staticmethod
+    def create(db: Session, project: Project):
+        db.add(project)
+        db.commit()
+        db.refresh(project)
+        return project
+
+    @staticmethod
+    def delete(db: Session, project: Project):
+        db.delete(project)
+        db.commit()
