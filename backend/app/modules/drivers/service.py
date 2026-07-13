5c08dd228c543ca5e6e5709a5a905c36713788f4
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-13 16:53:53 -0300

    Refactor supported method in DriverService
    Refactored supported method to use DriverRepository.

--- backend/app/modules/drivers/service.py
+++ backend/app/modules/drivers/service.py
@@ -1,31 +1,9 @@
+
+from app.repositories.driver_repository import DriverRepository
+
+
 class DriverService:
 
     @staticmethod
     def supported():
-        return [
-            {
-                "id": "serial",
-                "name": "Serial",
-                "description": "COM / USB",
-            },
-            {
-                "id": "tcp",
-                "name": "TCP/IP",
-                "description": "Ethernet / Wi-Fi",
-            },
-            {
-                "id": "mqtt",
-                "name": "MQTT",
-                "description": "Broker MQTT",
-            },
-            {
-                "id": "http",
-                "name": "HTTP",
-                "description": "REST API",
-            },
-            {
-                "id": "websocket",
-                "name": "WebSocket",
-                "description": "Real-time",
-            },
-        ]
+        return DriverRepository.supported()
