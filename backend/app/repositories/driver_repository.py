3de7e0de127a7e5ed27a0bcd2e47ea653745c1c7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-13 16:50:42 -0300

    Implement supported method in DriverRepository
    Added a static method to return supported driver types with descriptions.

--- backend/app/repositories/driver_repository.py
+++ backend/app/repositories/driver_repository.py
@@ -1,1 +1,31 @@
+class DriverRepository:
 
+    @staticmethod
+    def supported():
+        return [
+            {
+                "id": "serial",
+                "name": "Serial",
+                "description": "COM / USB",
+            },
+            {
+                "id": "tcp",
+                "name": "TCP/IP",
+                "description": "Ethernet / Wi-Fi",
+            },
+            {
+                "id": "mqtt",
+                "name": "MQTT",
+                "description": "Broker MQTT",
+            },
+            {
+                "id": "http",
+                "name": "HTTP",
+                "description": "REST API",
+            },
+            {
+                "id": "websocket",
+                "name": "WebSocket",
+                "description": "Real-time",
+            },
+        ]
