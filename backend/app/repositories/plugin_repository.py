85dd2c0a54528186212f2b71d61b342e2ed1fa89
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-13 21:47:38 -0300

    Update plugin_repository.py

--- backend/app/repositories/plugin_repository.py
+++ backend/app/repositories/plugin_repository.py
@@ -1,1 +1,26 @@
+from sqlalchemy.orm import Session
 
+from app.models.plugin import Plugin
+
+
+class PluginRepository:
+
+    @staticmethod
+    def list(db: Session):
+        return db.query(Plugin).all()
+
+    @staticmethod
+    def get(db: Session, plugin_id: str):
+        return db.query(Plugin).filter(Plugin.id == plugin_id).first()
+
+    @staticmethod
+    def create(db: Session, plugin: Plugin):
+        db.add(plugin)
+        db.commit()
+        db.refresh(plugin)
+        return plugin
+
+    @staticmethod
+    def delete(db: Session, plugin: Plugin):
+        db.delete(plugin)
+        db.commit()
