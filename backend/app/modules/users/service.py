991671bac2318946bbe8729256e3d1e32fcbd8a0
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-13 21:34:28 -0300

    Update service.py

--- backend/app/modules/users/service.py
+++ backend/app/modules/users/service.py
@@ -1,1 +1,44 @@
+from sqlalchemy.orm import Session
 
+from app.models.user import User
+from app.repositories.user_repository import UserRepository
+
+
+class UserService:
+
+    @staticmethod
+    def list(db: Session):
+        return UserRepository.list(db)
+
+    @staticmethod
+    def get(db: Session, user_id: str):
+        return UserRepository.get(db, user_id)
+
+    @staticmethod
+    def create(
+        db: Session,
+        name: str,
+        email: str,
+        password: str,
+    ):
+        user = User(
+            name=name,
+            email=email,
+            password=password,
+        )
+
+        return UserRepository.create(db, user)
+
+    @staticmethod
+    def delete(
+        db: Session,
+        user_id: str,
+    ):
+        user = UserRepository.get(db, user_id)
+
+        if not user:
+            return False
+
+        UserRepository.delete(db, user)
+
+        return True
