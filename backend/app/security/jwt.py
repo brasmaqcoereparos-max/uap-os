67fd900a492b0e207d99e7795e6222a991411a3b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-14 15:40:52 -0300

    Update jwt.py

--- backend/app/security/jwt.py
+++ backend/app/security/jwt.py
@@ -1,1 +1,31 @@
+from datetime import datetime, timedelta, timezone
 
+import jwt
+
+SECRET_KEY = "CHANGE_THIS_SECRET_IN_PRODUCTION"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM,
+    )
+
+
+def verify_access_token(token: str):
+    return jwt.decode(
+        token,
+        SECRET_KEY,
+        algorithms=[ALGORITHM],
+    )
