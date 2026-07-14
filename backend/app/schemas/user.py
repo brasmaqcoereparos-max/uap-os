23eb32af63f6e74dac65acb56d52bbf1ee81a71c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-13 21:28:48 -0300

    Update user.py

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,1 +1,23 @@
+from pydantic import BaseModel, EmailStr
 
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+
+class UserUpdate(BaseModel):
+    name: str
+    email: EmailStr
+    role: str
+
+
+class UserResponse(BaseModel):
+    id: str
+    name: str
+    email: EmailStr
+    role: str
+
+    class Config:
+        from_attributes = True
