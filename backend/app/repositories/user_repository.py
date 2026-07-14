897529dd9cadada4e85ef146abd944068c15aa3d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-14 19:47:44 -0300

    Update user_repository.py

--- backend/app/repositories/user_repository.py
+++ backend/app/repositories/user_repository.py
@@ -11,20 +11,33 @@ class UserRepository:
 
     @staticmethod
     def get(db: Session, user_id: str):
-        return db.query(User).filter(User.id == user_id).first()
+        return db.query(User).filter(
+            User.id == user_id
+        ).first()
 
     @staticmethod
-    def get_by_email(db: Session, email: str):
-        return db.query(User).filter(User.email == email).first()
+    def get_by_email(
+        db: Session,
+        email: str,
+    ):
+        return db.query(User).filter(
+            User.email == email
+        ).first()
 
     @staticmethod
-    def create(db: Session, user: User):
+    def create(
+        db: Session,
+        user: User,
+    ):
         db.add(user)
         db.commit()
         db.refresh(user)
         return user
 
     @staticmethod
-    def delete(db: Session, user: User):
+    def delete(
+        db: Session,
+        user: User,
+    ):
         db.delete(user)
         db.commit()
