5df4563a36db22e6dc3776c98a7d8185a1cd8e0d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-14 07:58:41 -0300

    Update service.py

--- backend/app/modules/runtime/service.py
+++ backend/app/modules/runtime/service.py
@@ -1,1 +1,40 @@
+from sqlalchemy.orm import Session
 
+from app.models.runtime import Runtime
+from app.repositories.runtime_repository import RuntimeRepository
+
+
+class RuntimeService:
+
+    @staticmethod
+    def list(db: Session):
+        return RuntimeRepository.list(db)
+
+    @staticmethod
+    def get(db: Session, runtime_id: str):
+        return RuntimeRepository.get(db, runtime_id)
+
+    @staticmethod
+    def create(
+        db: Session,
+        name: str,
+    ):
+        runtime = Runtime(
+            name=name,
+        )
+
+        return RuntimeRepository.create(db, runtime)
+
+    @staticmethod
+    def delete(
+        db: Session,
+        runtime_id: str,
+    ):
+        runtime = RuntimeRepository.get(db, runtime_id)
+
+        if not runtime:
+            return False
+
+        RuntimeRepository.delete(db, runtime)
+
+        return True
