58617819237d811d288e7b517f6b50a35cc5c2c4
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-14 08:16:13 -0300

    Update service.py

--- backend/app/modules/dashboard/service.py
+++ backend/app/modules/dashboard/service.py
@@ -1,1 +1,42 @@
+from sqlalchemy.orm import Session
 
+from app.models.dashboard import Dashboard
+from app.repositories.dashboard_repository import DashboardRepository
+
+
+class DashboardService:
+
+    @staticmethod
+    def list(db: Session):
+        return DashboardRepository.list(db)
+
+    @staticmethod
+    def get(db: Session, dashboard_id: str):
+        return DashboardRepository.get(db, dashboard_id)
+
+    @staticmethod
+    def create(
+        db: Session,
+        name: str,
+        description: str,
+    ):
+        dashboard = Dashboard(
+            name=name,
+            description=description,
+        )
+
+        return DashboardRepository.create(db, dashboard)
+
+    @staticmethod
+    def delete(
+        db: Session,
+        dashboard_id: str,
+    ):
+        dashboard = DashboardRepository.get(db, dashboard_id)
+
+        if not dashboard:
+            return False
+
+        DashboardRepository.delete(db, dashboard)
+
+        return True
