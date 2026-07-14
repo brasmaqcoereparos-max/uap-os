5f1c79ea3a99997fa9770d116a14ccf851bee702
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-14 08:06:11 -0300

    Update dashboard.py

--- backend/app/models/dashboard.py
+++ backend/app/models/dashboard.py
@@ -1,1 +1,27 @@
+import uuid
 
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from app.database.base import Base
+
+
+class Dashboard(Base):
+    __tablename__ = "dashboards"
+
+    id: Mapped[str] = mapped_column(
+        String,
+        primary_key=True,
+        default=lambda: str(uuid.uuid4()),
+    )
+
+    name: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False,
+    )
+
+    description: Mapped[str] = mapped_column(
+        String(255),
+        default="",
+    )
