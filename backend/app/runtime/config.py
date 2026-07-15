ccbd0fabdc16145464c017bcb559b859f1c031ee
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 16:25:23 -0300

    Update config.py

--- backend/app/runtime/config.py
+++ backend/app/runtime/config.py
@@ -1,1 +1,47 @@
+import os
 
+
+class RuntimeConfig:
+
+    ENGINE_CYCLE_TIME = float(
+        os.getenv("ENGINE_CYCLE_TIME", "0.1")
+    )
+
+    LOG_LEVEL = os.getenv(
+        "LOG_LEVEL",
+        "INFO",
+    )
+
+    MQTT_HOST = os.getenv(
+        "MQTT_HOST",
+        "localhost",
+    )
+
+    MQTT_PORT = int(
+        os.getenv(
+            "MQTT_PORT",
+            "1883",
+        )
+    )
+
+    MODBUS_PORT = int(
+        os.getenv(
+            "MODBUS_PORT",
+            "502",
+        )
+    )
+
+    SERIAL_BAUDRATE = int(
+        os.getenv(
+            "SERIAL_BAUDRATE",
+            "115200",
+        )
+    )
+
+    AUTO_RECONNECT = os.getenv(
+        "AUTO_RECONNECT",
+        "true",
+    ).lower() == "true"
+
+
+runtime_config = RuntimeConfig()
