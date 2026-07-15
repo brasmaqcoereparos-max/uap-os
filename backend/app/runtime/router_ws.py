a1d1cbfc6a05989eecd4f3754184f3d5f799c561
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 15:54:09 -0300

    Update router_ws.py

--- backend/app/runtime/router_ws.py
+++ backend/app/runtime/router_ws.py
@@ -1,1 +1,24 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
+from app.runtime.websocket_manager import websocket_manager
+
+router = APIRouter(tags=["WebSocket"])
+
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket_manager.connect(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            await websocket_manager.broadcast(
+                {
+                    "type": "message",
+                    "data": data,
+                }
+            )
+
+    except WebSocketDisconnect:
+        websocket_manager.disconnect(websocket)
