b2224f63db1f668ce293aa2defba363eb3e32eff
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 20:59:02 -0300

    Update service.py

--- backend/app/modules/education/service.py
+++ backend/app/modules/education/service.py
@@ -1,1 +1,18 @@
+class EducationService:
 
+    def __init__(self):
+        self.education_mode = False
+
+    def enable(self):
+        self.education_mode = True
+
+    def disable(self):
+        self.education_mode = False
+
+    def status(self):
+        return {
+            "education_mode": self.education_mode
+        }
+
+
+education_service = EducationService()
