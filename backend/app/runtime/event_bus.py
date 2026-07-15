a7f3fa318b760acef55dca9b74a8bb7494e9f364
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 05:40:19 -0300

    Update event_bus.py

--- backend/app/runtime/event_bus.py
+++ backend/app/runtime/event_bus.py
@@ -1,1 +1,21 @@
+from collections import defaultdict
 
+
+class EventBus:
+
+    def __init__(self):
+        self.listeners = defaultdict(list)
+
+    def subscribe(self, event_name: str, callback):
+        self.listeners[event_name].append(callback)
+
+    def unsubscribe(self, event_name: str, callback):
+        if callback in self.listeners[event_name]:
+            self.listeners[event_name].remove(callback)
+
+    def emit(self, event_name: str, data=None):
+        for callback in self.listeners[event_name]:
+            callback(data)
+
+
+event_bus = EventBus()
