58ea81e539fbddac9cde54ef730f5c8bae1b61a2
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 16:05:47 -0300

    Update command_queue.py

--- backend/app/runtime/command_queue.py
+++ backend/app/runtime/command_queue.py
@@ -1,1 +1,29 @@
+from queue import Queue, Empty
 
+
+class CommandQueue:
+
+    def __init__(self):
+        self.queue = Queue()
+
+    def put(
+        self,
+        command: dict,
+    ):
+        self.queue.put(command)
+
+    def get(self):
+        try:
+            return self.queue.get_nowait()
+        except Empty:
+            return None
+
+    def size(self):
+        return self.queue.qsize()
+
+    def clear(self):
+        while not self.queue.empty():
+            self.queue.get()
+
+
+command_queue = CommandQueue()
