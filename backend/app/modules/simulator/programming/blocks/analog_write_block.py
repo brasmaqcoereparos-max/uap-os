fa65d2d415b1060ad1e8d17fa5ce7dd34b37ad88
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 22:51:57 -0300

    Update analog_write_block.py

--- backend/app/modules/simulator/programming/blocks/analog_write_block.py
+++ backend/app/modules/simulator/programming/blocks/analog_write_block.py
@@ -1,1 +1,22 @@
+from app.modules.simulator.programming.blocks.base_block import BaseBlock
 
+
+class AnalogWriteBlock(BaseBlock):
+
+    def __init__(
+        self,
+        block_id,
+        pin,
+        value,
+    ):
+        super().__init__(
+            block_id,
+            "Analog Write",
+        )
+
+        self.pin = pin
+        self.value = value
+
+    def execute(self, context):
+        context[f"A{self.pin}"] = self.value
+        return context
