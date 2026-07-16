d7f1b8f45b516984012fc0c190eb671df71c3ec8
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 21:44:28 -0300

    Update virtual_humidity.py

--- backend/app/modules/simulator/components/virtual_humidity.py
+++ backend/app/modules/simulator/components/virtual_humidity.py
@@ -1,1 +1,23 @@
+import random
 
+from app.modules.simulator.components.virtual_sensor import VirtualSensor
+
+
+class VirtualHumidity(VirtualSensor):
+
+    def __init__(
+        self,
+        sensor_id,
+        name,
+    ):
+        super().__init__(
+            sensor_id,
+            name,
+            "HUMIDITY",
+        )
+
+    def update(self):
+        self.value = random.randint(
+            20,
+            90,
+        )
