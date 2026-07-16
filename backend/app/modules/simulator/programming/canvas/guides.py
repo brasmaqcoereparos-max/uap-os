bebd81b7d31700f63a3b373bf67d62f76c9574c7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 17:17:26 -0300

    Update guides.py

--- backend/app/modules/simulator/programming/canvas/guides.py
+++ backend/app/modules/simulator/programming/canvas/guides.py
@@ -1,1 +1,36 @@
+class GuideManager:
 
+    def __init__(self):
+
+        self.vertical = []
+
+        self.horizontal = []
+
+    def add_vertical(
+        self,
+        x,
+    ):
+
+        self.vertical.append(x)
+
+    def add_horizontal(
+        self,
+        y,
+    ):
+
+        self.horizontal.append(y)
+
+    def clear(self):
+
+        self.vertical.clear()
+        self.horizontal.clear()
+
+    def to_dict(self):
+
+        return {
+            "vertical": self.vertical,
+            "horizontal": self.horizontal,
+        }
+
+
+guides = GuideManager()
