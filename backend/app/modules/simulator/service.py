ae25aae9fd26c8b3660a1ede2981894b4b7ab523
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 21:11:13 -0300

    Update service.py

--- backend/app/modules/simulator/service.py
+++ backend/app/modules/simulator/service.py
@@ -1,1 +1,19 @@
+class SimulatorService:
 
+    def __init__(self):
+        self.devices = {}
+
+    def add(self, device):
+        self.devices[device["id"]] = device
+
+    def list(self):
+        return list(self.devices.values())
+
+    def get(self, device_id):
+        return self.devices.get(device_id)
+
+    def remove(self, device_id):
+        self.devices.pop(device_id, None)
+
+
+simulator_service = SimulatorService()
