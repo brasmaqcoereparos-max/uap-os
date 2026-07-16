e1cab1c5811b5e4319d602630bc952835346a1eb
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 23:16:34 -0300

    Update connection.py

--- backend/app/modules/simulator/programming/canvas/connection.py
+++ backend/app/modules/simulator/programming/canvas/connection.py
@@ -1,1 +1,22 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class Connection:
+
+    source: str
+
+    target: str
+
+    source_port: int = 0
+
+    target_port: int = 0
+
+    def to_dict(self):
+
+        return {
+            "source": self.source,
+            "target": self.target,
+            "source_port": self.source_port,
+            "target_port": self.target_port,
+        }
