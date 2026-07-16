c5e2f5c43f15243b31b79895258dcde7b6c03c7b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 22:48:54 -0300

    Update digital_write_block.py

--- backend/app/modules/simulator/programming/blocks/digital_write_block.py
+++ backend/app/modules/simulator/programming/blocks/digital_write_block.py
@@ -1,1 +1,22 @@
+from app.modules.simulator.programming.blocks.base_block import BaseBlock
 
+
+class DigitalWriteBlock(BaseBlock):
+
+    def __init__(
+        self,
+        block_id,
+        pin,
+        value,
+    ):
+        super().__init__(
+            block_id,
+            "Digital Write",
+        )
+
+        self.pin = pin
+        self.value = value
+
+    def execute(self, context):
+        context[f"D{self.pin}"] = self.value
+        return context
