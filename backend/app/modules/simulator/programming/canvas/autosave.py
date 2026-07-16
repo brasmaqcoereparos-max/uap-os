e635d18facc8356a3d6ab3443526e0327bfe6135
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 14:55:31 -0300

    Update autosave.py

--- backend/app/modules/simulator/programming/canvas/autosave.py
+++ backend/app/modules/simulator/programming/canvas/autosave.py
@@ -1,1 +1,46 @@
+from datetime import datetime
 
+
+class AutoSave:
+
+    def __init__(self):
+
+        self.enabled = True
+
+        self.interval = 300
+
+        self.last_save = None
+
+    def save(self):
+
+        self.last_save = datetime.now()
+
+    def set_interval(self, seconds):
+
+        if seconds < 30:
+            seconds = 30
+
+        self.interval = seconds
+
+    def enable(self):
+
+        self.enabled = True
+
+    def disable(self):
+
+        self.enabled = False
+
+    def to_dict(self):
+
+        return {
+            "enabled": self.enabled,
+            "interval": self.interval,
+            "last_save": (
+                self.last_save.isoformat()
+                if self.last_save
+                else None
+            ),
+        }
+
+
+autosave = AutoSave()
