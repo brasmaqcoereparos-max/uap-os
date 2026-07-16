ad42f10cab3d491fb5be84d587944ceaf9d761c4
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 02:43:15 -0300

    Update python_generator.py

--- backend/app/modules/simulator/codegen/python_generator.py
+++ backend/app/modules/simulator/codegen/python_generator.py
@@ -1,1 +1,12 @@
+from app.modules.simulator.codegen.base_generator import (
+    BaseGenerator,
+)
 
+
+class PythonGenerator(BaseGenerator):
+
+    LANGUAGE = "Python"
+
+    def generate_node(self, node):
+
+        return f"# {node.name}"
