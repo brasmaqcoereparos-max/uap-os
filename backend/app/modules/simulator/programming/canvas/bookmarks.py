61a17d2ef143322b0f3788b943cfa0963d2c001a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 17:13:26 -0300

    Update bookmarks.py

--- backend/app/modules/simulator/programming/canvas/bookmarks.py
+++ backend/app/modules/simulator/programming/canvas/bookmarks.py
@@ -1,1 +1,43 @@
+class BookmarkManager:
 
+    def __init__(self):
+
+        self.bookmarks = {}
+
+    def add(
+        self,
+        name,
+        x,
+        y,
+        zoom,
+    ):
+
+        self.bookmarks[name] = {
+            "x": x,
+            "y": y,
+            "zoom": zoom,
+        }
+
+    def remove(
+        self,
+        name,
+    ):
+
+        self.bookmarks.pop(
+            name,
+            None,
+        )
+
+    def get(
+        self,
+        name,
+    ):
+
+        return self.bookmarks.get(name)
+
+    def all(self):
+
+        return self.bookmarks
+
+
+bookmarks = BookmarkManager()
