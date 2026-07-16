4b9be06aa84c009ba6ca1fc4f33098a9006e88df
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 06:10:52 -0300

    Update alignment.py

--- backend/app/modules/simulator/programming/canvas/alignment.py
+++ backend/app/modules/simulator/programming/canvas/alignment.py
@@ -1,1 +1,83 @@
+from app.modules.simulator.programming.canvas.canvas import canvas
 
+
+class Alignment:
+
+    def left(self):
+
+        nodes = [
+            n
+            for n in canvas.all_nodes()
+            if n.selected
+        ]
+
+        if not nodes:
+            return
+
+        x = min(
+            node.x
+            for node in nodes
+        )
+
+        for node in nodes:
+            node.x = x
+
+    def right(self):
+
+        nodes = [
+            n
+            for n in canvas.all_nodes()
+            if n.selected
+        ]
+
+        if not nodes:
+            return
+
+        x = max(
+            node.x
+            for node in nodes
+        )
+
+        for node in nodes:
+            node.x = x
+
+    def top(self):
+
+        nodes = [
+            n
+            for n in canvas.all_nodes()
+            if n.selected
+        ]
+
+        if not nodes:
+            return
+
+        y = min(
+            node.y
+            for node in nodes
+        )
+
+        for node in nodes:
+            node.y = y
+
+    def bottom(self):
+
+        nodes = [
+            n
+            for n in canvas.all_nodes()
+            if n.selected
+        ]
+
+        if not nodes:
+            return
+
+        y = max(
+            node.y
+            for node in nodes
+        )
+
+        for node in nodes:
+            node.y = y
+
+
+alignment = Alignment()
