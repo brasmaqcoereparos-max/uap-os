2be3a6c194526874f96a0173e31f4c1dbf596d8b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 17:22:01 -0300

    Update clipboard_manager.py

--- backend/app/modules/simulator/programming/canvas/clipboard_manager.py
+++ backend/app/modules/simulator/programming/canvas/clipboard_manager.py
@@ -1,1 +1,40 @@
+import copy
 
+
+class ClipboardManager:
+
+    def __init__(self):
+
+        self.data = None
+
+    def copy_nodes(self, nodes):
+
+        self.data = copy.deepcopy(nodes)
+
+    def paste_nodes(self):
+
+        if self.data is None:
+            return []
+
+        nodes = copy.deepcopy(self.data)
+
+        for node in nodes:
+
+            node.id = None
+
+            node.x += 40
+
+            node.y += 40
+
+        return nodes
+
+    def clear(self):
+
+        self.data = None
+
+    def has_data(self):
+
+        return self.data is not None
+
+
+clipboard_manager = ClipboardManager()
