9e701ccfd2173dd328d0d94c74382126930c877f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 08:08:24 -0300

    Update grid.py

--- backend/app/modules/simulator/programming/canvas/grid.py
+++ backend/app/modules/simulator/programming/canvas/grid.py
@@ -1,1 +1,54 @@
+class CanvasGrid:
 
+    def __init__(self):
+
+        self.enabled = True
+
+        self.visible = True
+
+        self.size = 20
+
+        self.snap = True
+
+    def to_dict(self):
+
+        return {
+            "enabled": self.enabled,
+            "visible": self.visible,
+            "size": self.size,
+            "snap": self.snap,
+        }
+
+    def enable(self):
+
+        self.enabled = True
+
+    def disable(self):
+
+        self.enabled = False
+
+    def show(self):
+
+        self.visible = True
+
+    def hide(self):
+
+        self.visible = False
+
+    def set_size(self, size):
+
+        if size < 5:
+            size = 5
+
+        self.size = size
+
+    def enable_snap(self):
+
+        self.snap = True
+
+    def disable_snap(self):
+
+        self.snap = False
+
+
+grid = CanvasGrid()
