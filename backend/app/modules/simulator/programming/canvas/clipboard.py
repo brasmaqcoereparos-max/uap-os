e21653387946013118550b1c2b6efe43803fe9ba
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 04:02:48 -0300

    Update clipboard.py

--- backend/app/modules/simulator/programming/canvas/clipboard.py
+++ backend/app/modules/simulator/programming/canvas/clipboard.py
@@ -1,1 +1,34 @@
+import copy
 
+
+class Clipboard:
+
+    def __init__(self):
+
+        self.data = None
+
+    def copy(self, node):
+
+        self.data = copy.deepcopy(node)
+
+    def paste(self):
+
+        if self.data is None:
+            return None
+
+        node = copy.deepcopy(self.data)
+
+        node.id = None
+
+        node.x += 40
+
+        node.y += 40
+
+        return node
+
+    def clear(self):
+
+        self.data = None
+
+
+clipboard = Clipboard()
