fe2112c83599e03c524e52fd701a189a79ed6d28
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 17:11:46 -0300

    Update templates.py

--- backend/app/modules/simulator/programming/canvas/templates.py
+++ backend/app/modules/simulator/programming/canvas/templates.py
@@ -1,1 +1,46 @@
+import copy
 
+
+class CanvasTemplates:
+
+    def __init__(self):
+
+        self.templates = {}
+
+    def save(
+        self,
+        name,
+        canvas,
+    ):
+
+        self.templates[name] = copy.deepcopy(
+            canvas.status()
+        )
+
+    def load(
+        self,
+        name,
+    ):
+
+        return copy.deepcopy(
+            self.templates.get(name)
+        )
+
+    def delete(
+        self,
+        name,
+    ):
+
+        self.templates.pop(
+            name,
+            None,
+        )
+
+    def list(self):
+
+        return sorted(
+            self.templates.keys()
+        )
+
+
+templates = CanvasTemplates()
