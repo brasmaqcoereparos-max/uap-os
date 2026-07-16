9caa5458bfd38646c5b338256db5a04fbd716cc2
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 21:23:06 -0300

    Update virtual_relay.py

--- backend/app/modules/simulator/devices/virtual_relay.py
+++ backend/app/modules/simulator/devices/virtual_relay.py
@@ -1,1 +1,15 @@
+from app.modules.simulator.devices.virtual_device import VirtualDevice
 
+
+class VirtualRelay(VirtualDevice):
+
+    def __init__(
+        self,
+        device_id,
+        name,
+    ):
+        super().__init__(
+            device_id,
+            name,
+            "RELAY",
+        )
