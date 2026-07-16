5a6fe4f12857a85fd254f017677fdb8c8d4f1a68
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 15:08:46 -0300

    Update session.py

--- backend/app/modules/simulator/programming/canvas/session.py
+++ backend/app/modules/simulator/programming/canvas/session.py
@@ -1,1 +1,37 @@
+from datetime import datetime
 
+
+class Session:
+
+    def __init__(self):
+
+        self.started = datetime.now()
+
+        self.project = None
+
+        self.modified = False
+
+    def open_project(self, name):
+
+        self.project = name
+
+        self.modified = False
+
+    def mark_modified(self):
+
+        self.modified = True
+
+    def save(self):
+
+        self.modified = False
+
+    def to_dict(self):
+
+        return {
+            "started": self.started.isoformat(),
+            "project": self.project,
+            "modified": self.modified,
+        }
+
+
+session = Session()
