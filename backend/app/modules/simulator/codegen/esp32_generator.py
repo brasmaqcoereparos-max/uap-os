a53b7ea237c072c0ad95d04245a5c453426bcead
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 02:57:31 -0300

    Update esp32_generator.py

--- backend/app/modules/simulator/codegen/esp32_generator.py
+++ backend/app/modules/simulator/codegen/esp32_generator.py
@@ -6,3 +6,21 @@ from app.modules.simulator.codegen.arduino_generator import (
 class ESP32Generator(ArduinoGenerator):
 
     LANGUAGE = "ESP32"
+
+    def header(self):
+
+        return """// UAP OS
+// ESP32 Arduino Framework
+
+void setup() {
+
+}
+
+void loop() {
+"""
+
+    def footer(self):
+
+        return """
+}
+"""
