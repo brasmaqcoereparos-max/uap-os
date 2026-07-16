20c547830c61a170bb50d44c975495e583bd9b52
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 21:21:49 -0300

    Update virtual_led.py

--- backend/app/modules/simulator/devices/virtual_led.py
+++ backend/app/modules/simulator/devices/virtual_led.py
@@ -1,1 +1,15 @@
+from app.modules.simulator.devices.virtual_device import VirtualDevice
 
+
+class VirtualLED(VirtualDevice):
+
+    def __init__(
+        self,
+        device_id,
+        name,
+    ):
+        super().__init__(
+            device_id,
+            name,
+            "LED",
+        )
