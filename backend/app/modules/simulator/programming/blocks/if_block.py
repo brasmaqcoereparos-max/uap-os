8fe92be8ace8a5d62439a2038b977ba3544642ac
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 22:46:46 -0300

    Update if_block.py

--- backend/app/modules/simulator/programming/blocks/if_block.py
+++ backend/app/modules/simulator/programming/blocks/if_block.py
@@ -1,1 +1,26 @@
+from app.modules.simulator.programming.blocks.base_block import BaseBlock
 
+
+class IfBlock(BaseBlock):
+
+    def __init__(
+        self,
+        block_id,
+        variable,
+        value,
+    ):
+        super().__init__(
+            block_id,
+            "If",
+        )
+
+        self.variable = variable
+        self.value = value
+
+    def execute(self, context):
+        context["condition"] = (
+            context.get(self.variable)
+            == self.value
+        )
+
+        return context
