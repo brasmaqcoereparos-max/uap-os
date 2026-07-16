7ee630431116e5817f952b1060804f805156b77b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 14:44:35 -0300

    Update validator.py

--- backend/app/modules/simulator/programming/canvas/validator.py
+++ backend/app/modules/simulator/programming/canvas/validator.py
@@ -1,1 +1,63 @@
+class CanvasValidator:
 
+    def validate(
+        self,
+        canvas,
+    ):
+
+        errors = []
+
+        ids = set()
+
+        for node in canvas.all_nodes():
+
+            if node.id in ids:
+
+                errors.append(
+
+                    {
+
+                        "type": "duplicate_id",
+
+                        "node": node.id,
+
+                    }
+
+                )
+
+            ids.add(node.id)
+
+        for connection in canvas.all_connections():
+
+            if canvas.get_node(connection.source) is None:
+
+                errors.append(
+
+                    {
+
+                        "type": "missing_source",
+
+                        "node": connection.source,
+
+                    }
+
+                )
+
+            if canvas.get_node(connection.target) is None:
+
+                errors.append(
+
+                    {
+
+                        "type": "missing_target",
+
+                        "node": connection.target,
+
+                    }
+
+                )
+
+        return errors
+
+
+validator = CanvasValidator()
