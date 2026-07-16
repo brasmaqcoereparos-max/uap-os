8b11a52e4e6e24a2d9c3b15e219ea0601490564f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 02:40:33 -0300

    Update arduino_generator.py

--- backend/app/modules/simulator/codegen/arduino_generator.py
+++ backend/app/modules/simulator/codegen/arduino_generator.py
@@ -1,1 +1,28 @@
+from app.modules.simulator.codegen.base_generator import (
+    BaseGenerator,
+)
 
+
+class ArduinoGenerator(BaseGenerator):
+
+    LANGUAGE = "Arduino"
+
+    def header(self):
+
+        return """void setup(){
+
+}
+
+void loop(){
+
+"""
+
+    def footer(self):
+
+        return """
+}
+"""
+
+    def generate_node(self, node):
+
+        return f"  // {node.name}"
