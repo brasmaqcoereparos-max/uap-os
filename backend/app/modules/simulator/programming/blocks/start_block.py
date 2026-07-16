70424a685ae0948a88497e7a5ce1c6d281917c1e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 22:44:29 -0300

    Update start_block.py

--- backend/app/modules/simulator/programming/blocks/start_block.py
+++ backend/app/modules/simulator/programming/blocks/start_block.py
@@ -1,1 +1,8 @@
+from app.modules.simulator.programming.blocks.base_block import BaseBlock
 
+
+class StartBlock(BaseBlock):
+
+    def execute(self, context):
+        context["started"] = True
+        return context
