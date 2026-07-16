8d8f7956db72eed9888afea43f50967185af2c32
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 21:59:40 -0300

    Update raspberry_pi.py

--- backend/app/modules/simulator/boards/raspberry_pi.py
+++ backend/app/modules/simulator/boards/raspberry_pi.py
@@ -1,1 +1,17 @@
+from app.modules.simulator.boards.virtual_board import VirtualBoard
 
+
+class RaspberryPiBoard(VirtualBoard):
+
+    def __init__(
+        self,
+        board_id: str,
+        name: str,
+    ):
+        super().__init__(
+            board_id=board_id,
+            name=name,
+            board_type="Raspberry Pi",
+            digital_pins=40,
+            analog_pins=0,
+        )
