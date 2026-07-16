7000dc23c2709774fa03ce83b4cadf141a15ba9f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 21:43:42 -0300

    Update virtual_temperature.py

--- backend/app/modules/simulator/components/virtual_temperature.py
+++ backend/app/modules/simulator/components/virtual_temperature.py
@@ -1,1 +1,23 @@
+import random
 
+from app.modules.simulator.components.virtual_sensor import VirtualSensor
+
+
+class VirtualTemperature(VirtualSensor):
+
+    def __init__(
+        self,
+        sensor_id,
+        name,
+    ):
+        super().__init__(
+            sensor_id,
+            name,
+            "TEMPERATURE",
+        )
+
+    def update(self):
+        self.value = round(
+            random.uniform(18, 35),
+            1,
+        )
