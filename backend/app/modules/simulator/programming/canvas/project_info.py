20474eb345832c954a1911a21e18298fbf950f84
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 14:34:02 -0300

    Update project_info.py

--- backend/app/modules/simulator/programming/canvas/project_info.py
+++ backend/app/modules/simulator/programming/canvas/project_info.py
@@ -1,1 +1,33 @@
+from datetime import datetime
 
+
+class ProjectInfo:
+
+    def __init__(self):
+
+        self.name = "Novo Projeto"
+
+        self.author = ""
+
+        self.version = "1.0.0"
+
+        self.created = datetime.now()
+
+        self.modified = datetime.now()
+
+    def touch(self):
+
+        self.modified = datetime.now()
+
+    def to_dict(self):
+
+        return {
+            "name": self.name,
+            "author": self.author,
+            "version": self.version,
+            "created": self.created.isoformat(),
+            "modified": self.modified.isoformat(),
+        }
+
+
+project_info = ProjectInfo()
