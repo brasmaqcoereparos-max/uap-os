e7e1abe29801da6245c056c2d3c502ac5408c160
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 17:37:15 -0300

    Update color_palette.py

--- backend/app/modules/simulator/programming/canvas/color_palette.py
+++ backend/app/modules/simulator/programming/canvas/color_palette.py
@@ -1,1 +1,48 @@
+class ColorPalette:
 
+    def __init__(self):
+
+        self.colors = [
+
+            "#4CAF50",
+
+            "#2196F3",
+
+            "#FF9800",
+
+            "#9C27B0",
+
+            "#F44336",
+
+            "#607D8B",
+
+            "#009688",
+
+            "#795548",
+
+        ]
+
+    def add(
+        self,
+        color,
+    ):
+
+        if color not in self.colors:
+
+            self.colors.append(color)
+
+    def remove(
+        self,
+        color,
+    ):
+
+        if color in self.colors:
+
+            self.colors.remove(color)
+
+    def all(self):
+
+        return self.colors
+
+
+palette = ColorPalette()
