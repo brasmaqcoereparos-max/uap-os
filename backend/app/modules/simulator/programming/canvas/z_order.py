6bdd0a22aea31478dbb4c157543312975f80814e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 17:29:54 -0300

    Update z_order.py

--- backend/app/modules/simulator/programming/canvas/z_order.py
+++ backend/app/modules/simulator/programming/canvas/z_order.py
@@ -1,1 +1,34 @@
+class ZOrder:
 
+    def __init__(self):
+
+        self.order = []
+
+    def register(self, node_id):
+
+        if node_id not in self.order:
+
+            self.order.append(node_id)
+
+    def bring_to_front(self, node_id):
+
+        if node_id in self.order:
+
+            self.order.remove(node_id)
+
+            self.order.append(node_id)
+
+    def send_to_back(self, node_id):
+
+        if node_id in self.order:
+
+            self.order.remove(node_id)
+
+            self.order.insert(0, node_id)
+
+    def all(self):
+
+        return self.order
+
+
+z_order = ZOrder()
