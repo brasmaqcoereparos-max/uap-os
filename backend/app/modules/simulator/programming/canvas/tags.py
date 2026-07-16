0e265dac1fdcb0d7cee162ea84ff389ce3cf0453
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 17:33:34 -0300

    Update tags.py

--- backend/app/modules/simulator/programming/canvas/tags.py
+++ backend/app/modules/simulator/programming/canvas/tags.py
@@ -1,1 +1,51 @@
+class TagManager:
 
+    def __init__(self):
+
+        self.tags = {}
+
+    def add(
+        self,
+        node_id,
+        tag,
+    ):
+
+        self.tags.setdefault(
+            node_id,
+            set(),
+        ).add(tag)
+
+    def remove(
+        self,
+        node_id,
+        tag,
+    ):
+
+        if node_id in self.tags:
+
+            self.tags[node_id].discard(tag)
+
+    def get(
+        self,
+        node_id,
+    ):
+
+        return sorted(
+            self.tags.get(
+                node_id,
+                set(),
+            )
+        )
+
+    def clear(
+        self,
+        node_id,
+    ):
+
+        self.tags.pop(
+            node_id,
+            None,
+        )
+
+
+tags = TagManager()
