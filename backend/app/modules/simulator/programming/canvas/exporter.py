7c52019f6f1e4e9b1113fe6ca56551682fad5f90
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 14:47:11 -0300

    Update exporter.py

--- backend/app/modules/simulator/programming/canvas/exporter.py
+++ backend/app/modules/simulator/programming/canvas/exporter.py
@@ -1,1 +1,22 @@
+import json
 
+
+class CanvasExporter:
+
+    def export(
+        self,
+        canvas,
+    ):
+
+        return json.dumps(
+
+            canvas.status(),
+
+            indent=4,
+
+            ensure_ascii=False,
+
+        )
+
+
+exporter = CanvasExporter()
