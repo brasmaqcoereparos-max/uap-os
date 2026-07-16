fa5a4a5a112dbab98a94999558a083dc383a7e8e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 08:16:06 -0300

    Update ruler.py

--- backend/app/modules/simulator/programming/canvas/ruler.py
+++ backend/app/modules/simulator/programming/canvas/ruler.py
@@ -1,1 +1,41 @@
+class CanvasRuler:
 
+    def __init__(self):
+
+        self.enabled = True
+
+        self.unit = "px"
+
+        self.origin_x = 0
+
+        self.origin_y = 0
+
+    def enable(self):
+
+        self.enabled = True
+
+    def disable(self):
+
+        self.enabled = False
+
+    def set_origin(self, x, y):
+
+        self.origin_x = x
+        self.origin_y = y
+
+    def move_origin(self, dx, dy):
+
+        self.origin_x += dx
+        self.origin_y += dy
+
+    def to_dict(self):
+
+        return {
+            "enabled": self.enabled,
+            "unit": self.unit,
+            "origin_x": self.origin_x,
+            "origin_y": self.origin_y,
+        }
+
+
+ruler = CanvasRuler()
