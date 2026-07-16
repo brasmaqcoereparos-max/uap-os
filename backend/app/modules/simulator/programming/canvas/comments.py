89038fcf90842612cc50a9e44d6e89986cf4514c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 17:35:28 -0300

    Update comments.py

--- backend/app/modules/simulator/programming/canvas/comments.py
+++ backend/app/modules/simulator/programming/canvas/comments.py
@@ -1,1 +1,67 @@
+class Comment:
 
+    def __init__(
+        self,
+        x,
+        y,
+        text,
+    ):
+
+        self.x = x
+
+        self.y = y
+
+        self.text = text
+
+    def to_dict(self):
+
+        return {
+
+            "x": self.x,
+
+            "y": self.y,
+
+            "text": self.text,
+
+        }
+
+
+class CommentManager:
+
+    def __init__(self):
+
+        self.comments = []
+
+    def add(
+        self,
+        x,
+        y,
+        text,
+    ):
+
+        self.comments.append(
+
+            Comment(
+                x,
+                y,
+                text,
+            )
+
+        )
+
+    def clear(self):
+
+        self.comments.clear()
+
+    def all(self):
+
+        return [
+
+            c.to_dict()
+
+            for c in self.comments
+
+        ]
+
+
+comments = CommentManager()
