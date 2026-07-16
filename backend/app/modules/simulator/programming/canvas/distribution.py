990f46c9d064927bf0f72ee889eab25f60cb606c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 17:27:43 -0300

    Update distribution.py

--- backend/app/modules/simulator/programming/canvas/distribution.py
+++ backend/app/modules/simulator/programming/canvas/distribution.py
@@ -1,1 +1,38 @@
+class Distribution:
 
+    def horizontal(self, nodes):
+
+        if len(nodes) < 3:
+            return
+
+        nodes.sort(key=lambda n: n.x)
+
+        left = nodes[0].x
+
+        right = nodes[-1].x
+
+        spacing = (right - left) / (len(nodes) - 1)
+
+        for i, node in enumerate(nodes):
+
+            node.x = int(left + spacing * i)
+
+    def vertical(self, nodes):
+
+        if len(nodes) < 3:
+            return
+
+        nodes.sort(key=lambda n: n.y)
+
+        top = nodes[0].y
+
+        bottom = nodes[-1].y
+
+        spacing = (bottom - top) / (len(nodes) - 1)
+
+        for i, node in enumerate(nodes):
+
+            node.y = int(top + spacing * i)
+
+
+distribution = Distribution()
