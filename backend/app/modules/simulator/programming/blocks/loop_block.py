79455f4ae6e5bd809f6498553266c7c78fc070c0
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 22:47:41 -0300

    Update loop_block.py

--- backend/app/modules/simulator/programming/blocks/loop_block.py
+++ backend/app/modules/simulator/programming/blocks/loop_block.py
@@ -1,1 +1,20 @@
+from app.modules.simulator.programming.blocks.base_block import BaseBlock
 
+
+class LoopBlock(BaseBlock):
+
+    def __init__(
+        self,
+        block_id,
+        count,
+    ):
+        super().__init__(
+            block_id,
+            "Loop",
+        )
+
+        self.count = count
+
+    def execute(self, context):
+        context["loop"] = self.count
+        return context
