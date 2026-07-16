57639a47b2e8f5d6fab01552c61fb501a3929ee6
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 17:32:24 -0300

    Update macros.py

--- backend/app/modules/simulator/programming/canvas/macros.py
+++ backend/app/modules/simulator/programming/canvas/macros.py
@@ -1,1 +1,47 @@
+import copy
 
+
+class MacroManager:
+
+    def __init__(self):
+
+        self.macros = {}
+
+    def save(
+        self,
+        name,
+        nodes,
+    ):
+
+        self.macros[name] = copy.deepcopy(nodes)
+
+    def load(
+        self,
+        name,
+    ):
+
+        if name not in self.macros:
+            return []
+
+        return copy.deepcopy(
+            self.macros[name]
+        )
+
+    def remove(
+        self,
+        name,
+    ):
+
+        self.macros.pop(
+            name,
+            None,
+        )
+
+    def all(self):
+
+        return sorted(
+            self.macros.keys()
+        )
+
+
+macros = MacroManager()
