4423ec1afba9f79790e595de00e4ef061a754828
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 21:42:10 -0300

    Update virtual_sensor.py

--- backend/app/modules/simulator/components/virtual_sensor.py
+++ backend/app/modules/simulator/components/virtual_sensor.py
@@ -1,1 +1,29 @@
+from random import random
 
+
+class VirtualSensor:
+
+    def __init__(
+        self,
+        sensor_id,
+        name,
+        sensor_type,
+    ):
+        self.id = sensor_id
+        self.name = name
+        self.type = sensor_type
+        self.value = 0
+
+    def read(self):
+        return self.value
+
+    def update(self):
+        pass
+
+    def status(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+            "value": self.value,
+        }
