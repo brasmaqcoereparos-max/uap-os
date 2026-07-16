fd9d4bade9360ce66696fee2cd138d5c5d46659e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 21:45:05 -0300

    Update virtual_ultrasonic.py

--- backend/app/modules/simulator/components/virtual_ultrasonic.py
+++ backend/app/modules/simulator/components/virtual_ultrasonic.py
@@ -1,1 +1,23 @@
+import random
 
+from app.modules.simulator.components.virtual_sensor import VirtualSensor
+
+
+class VirtualUltrasonic(VirtualSensor):
+
+    def __init__(
+        self,
+        sensor_id,
+        name,
+    ):
+        super().__init__(
+            sensor_id,
+            name,
+            "ULTRASONIC",
+        )
+
+    def update(self):
+        self.value = round(
+            random.uniform(5, 400),
+            2,
+        )
