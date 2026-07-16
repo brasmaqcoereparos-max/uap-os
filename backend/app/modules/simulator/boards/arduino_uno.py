2ed2719cce55e3b5038d71f6dcfca146e7c55ba8
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 21:58:10 -0300

    Update arduino_uno.py

--- backend/app/modules/simulator/boards/arduino_uno.py
+++ backend/app/modules/simulator/boards/arduino_uno.py
@@ -1,1 +1,17 @@
+from app.modules.simulator.boards.virtual_board import VirtualBoard
 
+
+class ArduinoUNO(VirtualBoard):
+
+    def __init__(
+        self,
+        board_id: str,
+        name: str,
+    ):
+        super().__init__(
+            board_id=board_id,
+            name=name,
+            board_type="Arduino UNO",
+            digital_pins=14,
+            analog_pins=6,
+        )
