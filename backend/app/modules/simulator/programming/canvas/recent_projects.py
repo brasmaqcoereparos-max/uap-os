a86f28e097d3b141cdf16ee0881476ae8b87703d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 15:02:53 -0300

    Update recent_projects.py

--- backend/app/modules/simulator/programming/canvas/recent_projects.py
+++ backend/app/modules/simulator/programming/canvas/recent_projects.py
@@ -1,1 +1,32 @@
+from datetime import datetime
 
+
+class RecentProjects:
+
+    def __init__(self):
+
+        self.projects = []
+
+        self.limit = 10
+
+    def add(self, path):
+
+        self.projects.insert(
+
+            0,
+
+            {
+                "path": path,
+                "date": datetime.now().isoformat(),
+            },
+
+        )
+
+        self.projects = self.projects[: self.limit]
+
+    def all(self):
+
+        return self.projects
+
+
+recent_projects = RecentProjects()
