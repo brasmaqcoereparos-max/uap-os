42d56404687b96cafabc919b28c8b9a4461ebbed
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 02:42:19 -0300

    Update micropython_generator.py

--- backend/app/modules/simulator/codegen/micropython_generator.py
+++ backend/app/modules/simulator/codegen/micropython_generator.py
@@ -1,1 +1,12 @@
+from app.modules.simulator.codegen.base_generator import (
+    BaseGenerator,
+)
 
+
+class MicroPythonGenerator(BaseGenerator):
+
+    LANGUAGE = "MicroPython"
+
+    def generate_node(self, node):
+
+        return f"# {node.name}"
