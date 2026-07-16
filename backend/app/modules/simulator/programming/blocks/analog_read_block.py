3ccc80537e05336a8b6fc650695572f8173c59d8
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 22:51:06 -0300

    Update analog_read_block.py

--- backend/app/modules/simulator/programming/blocks/analog_read_block.py
+++ backend/app/modules/simulator/programming/blocks/analog_read_block.py
@@ -1,1 +1,24 @@
+from app.modules.simulator.programming.blocks.base_block import BaseBlock
 
+
+class DigitalReadBlock(BaseBlock):
+
+    def __init__(
+        self,
+        block_id,
+        pin,
+    ):
+        super().__init__(
+            block_id,
+            "Digital Read",
+        )
+
+        self.pin = pin
+
+    def execute(self, context):
+        context["value"] = context.get(
+            f"D{self.pin}",
+            0,
+        )
+
+        return context
