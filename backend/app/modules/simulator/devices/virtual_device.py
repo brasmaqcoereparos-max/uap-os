78b27e56b2a5eb643b3c3e55c00de36fc59db9b9
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 21:21:08 -0300

    Update virtual_device.py

--- backend/app/modules/simulator/devices/virtual_device.py
+++ backend/app/modules/simulator/devices/virtual_device.py
@@ -1,1 +1,29 @@
+class VirtualDevice:
 
+    def __init__(
+        self,
+        device_id,
+        name,
+        device_type,
+    ):
+        self.id = device_id
+        self.name = name
+        self.type = device_type
+        self.state = False
+
+    def on(self):
+        self.state = True
+
+    def off(self):
+        self.state = False
+
+    def toggle(self):
+        self.state = not self.state
+
+    def status(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+            "state": self.state,
+        }
