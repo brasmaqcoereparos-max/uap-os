ced77dd361f74bc05788b0b175d4a9fbdb377998
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 02:44:14 -0300

    Update router.py

--- backend/app/modules/simulator/codegen/router.py
+++ backend/app/modules/simulator/codegen/router.py
@@ -1,1 +1,55 @@
+from fastapi import APIRouter
 
+from app.modules.simulator.codegen.arduino_generator import (
+    ArduinoGenerator,
+)
+from app.modules.simulator.codegen.esp32_generator import (
+    ESP32Generator,
+)
+from app.modules.simulator.codegen.micropython_generator import (
+    MicroPythonGenerator,
+)
+from app.modules.simulator.codegen.python_generator import (
+    PythonGenerator,
+)
+
+router = APIRouter(
+    prefix="/codegen",
+    tags=["Code Generator"],
+)
+
+
+@router.get("/arduino")
+def arduino():
+
+    return {
+        "language": "Arduino",
+        "code": ArduinoGenerator().generate(),
+    }
+
+
+@router.get("/esp32")
+def esp32():
+
+    return {
+        "language": "ESP32",
+        "code": ESP32Generator().generate(),
+    }
+
+
+@router.get("/micropython")
+def micropython():
+
+    return {
+        "language": "MicroPython",
+        "code": MicroPythonGenerator().generate(),
+    }
+
+
+@router.get("/python")
+def python():
+
+    return {
+        "language": "Python",
+        "code": PythonGenerator().generate(),
+    }
