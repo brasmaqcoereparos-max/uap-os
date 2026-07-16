ae9ad586eb48c0ed775e77bdcd75d1ec5df54d18
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 14:41:52 -0300

    Update statistics.py

--- backend/app/modules/simulator/programming/canvas/statistics.py
+++ backend/app/modules/simulator/programming/canvas/statistics.py
@@ -1,1 +1,62 @@
+class CanvasStatistics:
 
+    def build(
+        self,
+        canvas,
+    ):
+
+        return {
+
+            "nodes": len(
+                canvas.all_nodes()
+            ),
+
+            "connections": len(
+                canvas.all_connections()
+            ),
+
+            "selected": len(
+
+                [
+
+                    n
+
+                    for n in canvas.all_nodes()
+
+                    if n.selected
+
+                ]
+
+            ),
+
+            "hidden": len(
+
+                [
+
+                    n
+
+                    for n in canvas.all_nodes()
+
+                    if not n.visible
+
+                ]
+
+            ),
+
+            "locked": len(
+
+                [
+
+                    n
+
+                    for n in canvas.all_nodes()
+
+                    if n.locked
+
+                ]
+
+            ),
+        }
+
+
+statistics = CanvasStatistics()
