10b9055329453de3d37aee7ea75cb32f467304d5
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 02:39:47 -0300

    Update base_generator.py

--- backend/app/modules/simulator/codegen/base_generator.py
+++ backend/app/modules/simulator/codegen/base_generator.py
@@ -1,1 +1,36 @@
+from app.modules.simulator.programming.canvas.canvas import canvas
 
+
+class BaseGenerator:
+
+    LANGUAGE = "TEXT"
+
+    def header(self):
+        return ""
+
+    def footer(self):
+        return ""
+
+    def generate_node(self, node):
+        return f"// {node.name}"
+
+    def generate(self):
+
+        code = []
+
+        header = self.header()
+
+        if header:
+            code.append(header)
+
+        for node in canvas.nodes.values():
+            code.append(
+                self.generate_node(node)
+            )
+
+        footer = self.footer()
+
+        if footer:
+            code.append(footer)
+
+        return "\n".join(code)
