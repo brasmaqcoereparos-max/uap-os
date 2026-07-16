a8b0d11b04d566775b4c6ca2b67a8563397e90f7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 22:45:13 -0300

    Update delay_block.py

--- backend/app/modules/simulator/programming/blocks/delay_block.py
+++ backend/app/modules/simulator/programming/blocks/delay_block.py
@@ -1,1 +1,25 @@
+import time
 
+from app.modules.simulator.programming.blocks.base_block import BaseBlock
+
+
+class DelayBlock(BaseBlock):
+
+    def __init__(
+        self,
+        block_id,
+        milliseconds,
+    ):
+        super().__init__(
+            block_id,
+            "Delay",
+        )
+
+        self.milliseconds = milliseconds
+
+    def execute(self, context):
+        time.sleep(
+            self.milliseconds / 1000
+        )
+
+        return context
