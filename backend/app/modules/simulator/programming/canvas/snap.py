7ea39f7ac24ec722ca48a9afe39da9a2014f5f8c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-16 17:15:12 -0300

    Update snap.py

--- backend/app/modules/simulator/programming/canvas/snap.py
+++ backend/app/modules/simulator/programming/canvas/snap.py
@@ -1,1 +1,32 @@
+class SnapManager:
 
+    def __init__(self):
+
+        self.grid = True
+
+        self.nodes = True
+
+        self.guides = True
+
+    def enable_all(self):
+
+        self.grid = True
+        self.nodes = True
+        self.guides = True
+
+    def disable_all(self):
+
+        self.grid = False
+        self.nodes = False
+        self.guides = False
+
+    def to_dict(self):
+
+        return {
+            "grid": self.grid,
+            "nodes": self.nodes,
+            "guides": self.guides,
+        }
+
+
+snap = SnapManager()
