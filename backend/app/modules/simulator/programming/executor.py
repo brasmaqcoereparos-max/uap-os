310065372046fffa4de9176df69e383000441d61
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 22:20:35 -0300

    Update executor.py

--- backend/app/modules/simulator/programming/executor.py
+++ backend/app/modules/simulator/programming/executor.py
@@ -1,1 +1,19 @@
+from app.modules.simulator.programming.workspace import workspace
 
+
+class BlockExecutor:
+
+    def execute(self):
+
+        execution = []
+
+        for block in workspace.blocks:
+
+            execution.append(
+                f"Executing {block.name}"
+            )
+
+        return execution
+
+
+executor = BlockExecutor()
