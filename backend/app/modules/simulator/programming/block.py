1d86d095cc9a8a0bcb8fb7d9c34d650b051c0e1a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 22:18:23 -0300

    Update block.py

--- backend/app/modules/simulator/programming/block.py
+++ backend/app/modules/simulator/programming/block.py
@@ -1,1 +1,22 @@
+from dataclasses import dataclass, field
+from typing import Dict, Any
+import uuid
 
+
+@dataclass
+class Block:
+
+    name: str
+    block_type: str
+
+    id: str = field(default_factory=lambda: str(uuid.uuid4()))
+
+    config: Dict[str, Any] = field(default_factory=dict)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.block_type,
+            "config": self.config,
+        }
