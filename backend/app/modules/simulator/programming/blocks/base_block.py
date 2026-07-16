9a8662c88aaca361f9cf40bc142f8190a0425ba9
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-15 22:43:37 -0300

    Update base_block.py

--- backend/app/modules/simulator/programming/blocks/base_block.py
+++ backend/app/modules/simulator/programming/blocks/base_block.py
@@ -1,1 +1,12 @@
+class BaseBlock:
 
+    def __init__(
+        self,
+        block_id: str,
+        name: str,
+    ):
+        self.id = block_id
+        self.name = name
+
+    def execute(self, context):
+        return context
