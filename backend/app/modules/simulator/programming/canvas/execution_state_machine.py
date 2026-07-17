2ddf7985994fd32adacfe381605b6755ec757564
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 20:45:19 -0300

    Update execution_state_machine.py

--- backend/app/modules/simulator/programming/canvas/execution_state_machine.py
+++ backend/app/modules/simulator/programming/canvas/execution_state_machine.py
@@ -1,1 +1,48 @@
+from enum import Enum
 
+
+class EngineState(Enum):
+
+    STOPPED = "stopped"
+    RUNNING = "running"
+    PAUSED = "paused"
+    FINISHED = "finished"
+    ERROR = "error"
+
+
+class ExecutionStateMachine:
+
+    def __init__(self):
+
+        self.state = EngineState.STOPPED
+
+    def start(self):
+
+        self.state = EngineState.RUNNING
+
+    def pause(self):
+
+        self.state = EngineState.PAUSED
+
+    def resume(self):
+
+        self.state = EngineState.RUNNING
+
+    def stop(self):
+
+        self.state = EngineState.STOPPED
+
+    def finish(self):
+
+        self.state = EngineState.FINISHED
+
+    def error(self):
+
+        self.state = EngineState.ERROR
+
+    def current(self):
+
+        return self.state.value
+
+
+execution_state_machine = ExecutionStateMachine()
