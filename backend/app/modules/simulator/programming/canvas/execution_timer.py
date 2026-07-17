61a2ed46b7608d13b62dacb68cb9e6dfd69e1250
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 20:35:47 -0300

    Update execution_timer.py

--- backend/app/modules/simulator/programming/canvas/execution_timer.py
+++ backend/app/modules/simulator/programming/canvas/execution_timer.py
@@ -1,1 +1,43 @@
+import time
 
+
+class ExecutionTimer:
+
+    def __init__(self):
+
+        self.started = None
+
+        self.finished = None
+
+    def start(self):
+
+        self.started = time.perf_counter()
+
+    def stop(self):
+
+        self.finished = time.perf_counter()
+
+    def elapsed(self):
+
+        if self.started is None:
+
+            return 0
+
+        end = self.finished or time.perf_counter()
+
+        return round(
+
+            end - self.started,
+
+            6,
+
+        )
+
+    def reset(self):
+
+        self.started = None
+
+        self.finished = None
+
+
+execution_timer = ExecutionTimer()
