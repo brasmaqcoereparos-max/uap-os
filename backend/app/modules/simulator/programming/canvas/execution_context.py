76d2f34c84339f02449c25d9c59caa9a4c5c8470
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 17:39:45 -0300

    Update execution_context.py

--- backend/app/modules/simulator/programming/canvas/execution_context.py
+++ backend/app/modules/simulator/programming/canvas/execution_context.py
@@ -1,1 +1,51 @@
+class ExecutionContext:
 
+    def __init__(self):
+
+        self.variables = {}
+
+        self.inputs = {}
+
+        self.outputs = {}
+
+    def set_variable(
+        self,
+        name,
+        value,
+    ):
+
+        self.variables[name] = value
+
+    def get_variable(
+        self,
+        name,
+        default=None,
+    ):
+
+        return self.variables.get(
+            name,
+            default,
+        )
+
+    def clear(self):
+
+        self.variables.clear()
+
+        self.inputs.clear()
+
+        self.outputs.clear()
+
+    def to_dict(self):
+
+        return {
+
+            "variables": self.variables,
+
+            "inputs": self.inputs,
+
+            "outputs": self.outputs,
+
+        }
+
+
+execution_context = ExecutionContext()
