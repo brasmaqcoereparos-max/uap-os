64fc9d74e07eaa5a8f6ddce360fdcce2cb6f58a8
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 20:50:16 -0300

    Update execution_memory.py

--- backend/app/modules/simulator/programming/canvas/execution_memory.py
+++ backend/app/modules/simulator/programming/canvas/execution_memory.py
@@ -1,1 +1,35 @@
+class ExecutionMemory:
 
+    def __init__(self):
+
+        self.memory = {}
+
+    def write(
+        self,
+        address,
+        value,
+    ):
+
+        self.memory[address] = value
+
+    def read(
+        self,
+        address,
+        default=0,
+    ):
+
+        return self.memory.get(
+            address,
+            default,
+        )
+
+    def reset(self):
+
+        self.memory.clear()
+
+    def dump(self):
+
+        return self.memory.copy()
+
+
+execution_memory = ExecutionMemory()
