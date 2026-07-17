f60147ea1cf3e2cc143064492cbbaba600174ade
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 20:59:32 -0300

    Update execution_clock.py

--- backend/app/modules/simulator/programming/canvas/execution_clock.py
+++ backend/app/modules/simulator/programming/canvas/execution_clock.py
@@ -1,1 +1,47 @@
+import time
 
+
+class ExecutionClock:
+
+    def __init__(self):
+
+        self.started = None
+
+    def start(self):
+
+        self.started = time.time()
+
+    def millis(self):
+
+        if self.started is None:
+
+            return 0
+
+        return int(
+
+            (time.time() - self.started)
+
+            * 1000
+
+        )
+
+    def seconds(self):
+
+        if self.started is None:
+
+            return 0
+
+        return round(
+
+            time.time() - self.started,
+
+            3,
+
+        )
+
+    def reset(self):
+
+        self.started = None
+
+
+execution_clock = ExecutionClock()
