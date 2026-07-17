9b1737b4989cc850b81e1aefa8162dc0059e2e22
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 07:12:54 -0300

    Update node_clipboard.py

--- backend/app/modules/simulator/programming/canvas/node_clipboard.py
+++ backend/app/modules/simulator/programming/canvas/node_clipboard.py
@@ -1,1 +1,42 @@
+import copy
 
+
+class NodeClipboard:
+
+    def __init__(self):
+
+        self.nodes = []
+
+    def copy(
+        self,
+        nodes,
+    ):
+
+        self.nodes = copy.deepcopy(nodes)
+
+    def paste(self):
+
+        result = copy.deepcopy(
+            self.nodes
+        )
+
+        for node in result:
+
+            node.id = None
+
+            node.x += 30
+
+            node.y += 30
+
+        return result
+
+    def clear(self):
+
+        self.nodes.clear()
+
+    def empty(self):
+
+        return len(self.nodes) == 0
+
+
+node_clipboard = NodeClipboard()
