1fa4da55921adf4010438574bda28939dd6d2651
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 10:51:21 -0300

    Update node_profiler.py

--- backend/app/modules/simulator/programming/canvas/node_profiler.py
+++ backend/app/modules/simulator/programming/canvas/node_profiler.py
@@ -1,1 +1,54 @@
+from datetime import datetime
 
+
+class NodeProfiler:
+
+    def __init__(self):
+
+        self.data = {}
+
+    def begin(
+        self,
+        node_id,
+    ):
+
+        self.data[node_id] = {
+
+            "start": datetime.now(),
+
+            "elapsed": 0,
+
+        }
+
+    def finish(
+        self,
+        node_id,
+    ):
+
+        if node_id not in self.data:
+
+            return
+
+        elapsed = (
+
+            datetime.now()
+
+            - self.data[node_id]["start"]
+
+        ).total_seconds()
+
+        self.data[node_id]["elapsed"] = elapsed
+
+    def get(
+        self,
+        node_id,
+    ):
+
+        return self.data.get(node_id)
+
+    def clear(self):
+
+        self.data.clear()
+
+
+node_profiler = NodeProfiler()
