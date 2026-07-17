450b2d2a1d1ce1327f788b54d7bcada44c781b47
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 06:55:11 -0300

    Update node_registry.py

--- backend/app/modules/simulator/programming/canvas/node_registry.py
+++ backend/app/modules/simulator/programming/canvas/node_registry.py
@@ -1,1 +1,39 @@
+class NodeRegistry:
 
+    def __init__(self):
+
+        self.registry = {}
+
+    def register(
+        self,
+        block_type,
+        metadata,
+    ):
+
+        self.registry[block_type] = metadata
+
+    def unregister(
+        self,
+        block_type,
+    ):
+
+        self.registry.pop(
+            block_type,
+            None,
+        )
+
+    def get(
+        self,
+        block_type,
+    ):
+
+        return self.registry.get(
+            block_type,
+        )
+
+    def all(self):
+
+        return self.registry.copy()
+
+
+node_registry = NodeRegistry()
