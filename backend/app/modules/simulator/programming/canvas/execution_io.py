0df4c4468e02a5aece7b17cd4ac5178c7f0d4d40
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 20:52:03 -0300

    Update execution_io.py

--- backend/app/modules/simulator/programming/canvas/execution_io.py
+++ backend/app/modules/simulator/programming/canvas/execution_io.py
@@ -1,1 +1,64 @@
+class ExecutionIO:
 
+    def __init__(self):
+
+        self.inputs = {}
+
+        self.outputs = {}
+
+    def input(
+        self,
+        name,
+        value,
+    ):
+
+        self.inputs[name] = value
+
+    def output(
+        self,
+        name,
+        value,
+    ):
+
+        self.outputs[name] = value
+
+    def read(
+        self,
+        name,
+        default=None,
+    ):
+
+        return self.inputs.get(
+            name,
+            default,
+        )
+
+    def write(
+        self,
+        name,
+        default=None,
+    ):
+
+        return self.outputs.get(
+            name,
+            default,
+        )
+
+    def clear(self):
+
+        self.inputs.clear()
+
+        self.outputs.clear()
+
+    def status(self):
+
+        return {
+
+            "inputs": self.inputs,
+
+            "outputs": self.outputs,
+
+        }
+
+
+execution_io = ExecutionIO()
