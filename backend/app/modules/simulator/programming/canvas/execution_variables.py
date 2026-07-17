48145f7826a652397b73464a81db5fe27b429e83
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 17:56:20 -0300

    Update execution_variables.py

--- backend/app/modules/simulator/programming/canvas/execution_variables.py
+++ backend/app/modules/simulator/programming/canvas/execution_variables.py
@@ -1,1 +1,52 @@
+class ExecutionVariables:
 
+    def __init__(self):
+
+        self.variables = {}
+
+    def set(
+        self,
+        name,
+        value,
+    ):
+
+        self.variables[name] = value
+
+    def get(
+        self,
+        name,
+        default=None,
+    ):
+
+        return self.variables.get(
+            name,
+            default,
+        )
+
+    def exists(
+        self,
+        name,
+    ):
+
+        return name in self.variables
+
+    def remove(
+        self,
+        name,
+    ):
+
+        self.variables.pop(
+            name,
+            None,
+        )
+
+    def clear(self):
+
+        self.variables.clear()
+
+    def all(self):
+
+        return self.variables.copy()
+
+
+execution_variables = ExecutionVariables()
