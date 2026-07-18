c74d3e31e9533dd8490c2a2ad6df4eb21de3af39
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 05:27:02 -0300

    Update device_configuration.py

--- backend/app/modules/simulator/programming/canvas/device_configuration.py
+++ backend/app/modules/simulator/programming/canvas/device_configuration.py
@@ -1,1 +1,29 @@
+class DeviceConfiguration:
 
+    def __init__(self):
+
+        self.config = {}
+
+    def set(self, device, values):
+
+        self.config[device] = values
+
+    def update(self, device, values):
+
+        self.config.setdefault(device, {})
+        self.config[device].update(values)
+
+    def get(self, device):
+
+        return self.config.get(device, {})
+
+    def remove(self, device):
+
+        self.config.pop(device, None)
+
+    def clear(self):
+
+        self.config.clear()
+
+
+device_configuration = DeviceConfiguration()
