5d820b7bacd9d32b2742ba8cbb0b921eb20a9e05
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 07:32:23 -0300

    Update compiler_executor.py

--- backend/app/modules/simulator/programming/compiler/compiler_executor.py
+++ backend/app/modules/simulator/programming/compiler/compiler_executor.py
@@ -1,1 +1,34 @@
+from app.modules.simulator.programming.compiler.compiler_dispatcher import (
+    compiler_dispatcher,
+)
+from app.modules.simulator.programming.compiler.compiler_node_factory import (
+    compiler_node_factory,
+)
 
+
+class CompilerExecutor:
+
+    def execute(
+
+        self,
+
+        nodes,
+
+        context,
+
+    ):
+
+        for item in nodes:
+
+            node = compiler_node_factory.create(item)
+
+            compiler_dispatcher.dispatch(
+
+                node,
+
+                context,
+
+            )
+
+
+compiler_executor = CompilerExecutor()
