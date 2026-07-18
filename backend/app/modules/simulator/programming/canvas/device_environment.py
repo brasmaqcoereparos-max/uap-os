d5e9a17e7cc0d7e32a62ef2af5b1d73502292190
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:16:31 -0300

    Update device_environment.py

--- backend/app/modules/simulator/programming/canvas/device_environment.py
+++ backend/app/modules/simulator/programming/canvas/device_environment.py
@@ -1,1 +1,22 @@
+import platform
+import sys
 
+
+class DeviceEnvironment:
+
+    def info(self):
+
+        return {
+
+            "python": sys.version,
+
+            "platform": platform.system(),
+
+            "release": platform.release(),
+
+            "machine": platform.machine(),
+
+        }
+
+
+device_environment = DeviceEnvironment()
