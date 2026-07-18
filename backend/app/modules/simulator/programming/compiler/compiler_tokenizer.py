d8cd8d084ba7768e54122181b61f686faf6e22a5
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:15:17 -0300

    Update compiler_tokenizer.py

--- backend/app/modules/simulator/programming/compiler/compiler_tokenizer.py
+++ backend/app/modules/simulator/programming/compiler/compiler_tokenizer.py
@@ -1,1 +1,45 @@
+from app.modules.simulator.programming.compiler.compiler_token import (
+    CompilerToken,
+)
 
+
+class CompilerTokenizer:
+
+    def tokenize(
+
+        self,
+
+        text,
+
+    ):
+
+        tokens = []
+
+        for line_number, line in enumerate(
+
+            text.splitlines(),
+
+            start=1,
+
+        ):
+
+            for value in line.split():
+
+                tokens.append(
+
+                    CompilerToken(
+
+                        "WORD",
+
+                        value,
+
+                        line_number,
+
+                    )
+
+                )
+
+        return tokens
+
+
+compiler_tokenizer = CompilerTokenizer()
