c67ae35fe8052a9ba7446b378ed45a6726586674
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 08:05:35 -0300

    Update compiler_statistics.py

--- backend/app/modules/simulator/programming/compiler/compiler_statistics.py
+++ backend/app/modules/simulator/programming/compiler/compiler_statistics.py
@@ -1,1 +1,54 @@
+class CompilerStatistics:
 
+    def __init__(self):
+
+        self.reset()
+
+    def reset(self):
+
+        self.compilations = 0
+
+        self.errors = 0
+
+    def success(self):
+
+        self.compilations += 1
+
+    def error(self):
+
+        self.compilations += 1
+
+        self.errors += 1
+
+    def report(self):
+
+        success = self.compilations - self.errors
+
+        return {
+
+            "compilations": self.compilations,
+
+            "errors": self.errors,
+
+            "success": success,
+
+            "success_rate": (
+
+                100
+
+                if self.compilations == 0
+
+                else round(
+
+                    success * 100 / self.compilations,
+
+                    2,
+
+                )
+
+            ),
+
+        }
+
+
+compiler_statistics = CompilerStatistics()
