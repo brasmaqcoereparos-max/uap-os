a1074d9d83471309d25189f6830540d6bbba4e11
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:29:15 -0300

    Update compiler_pass_manager.py

--- backend/app/modules/simulator/programming/compiler/compiler_pass_manager.py
+++ backend/app/modules/simulator/programming/compiler/compiler_pass_manager.py
@@ -1,1 +1,44 @@
+class CompilerPassManager:
 
+    def __init__(self):
+
+        self.passes = []
+
+    def register(
+
+        self,
+
+        compiler_pass,
+
+    ):
+
+        self.passes.append(
+
+            compiler_pass,
+
+        )
+
+    def execute(
+
+        self,
+
+        nodes,
+
+    ):
+
+        for compiler_pass in self.passes:
+
+            nodes = compiler_pass.run(
+
+                nodes,
+
+            )
+
+        return nodes
+
+    def clear(self):
+
+        self.passes.clear()
+
+
+compiler_pass_manager = CompilerPassManager()
