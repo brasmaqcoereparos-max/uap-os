c4f622c81626c4d56641ec24d03496e0accf8a00
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 05:17:14 -0300

    Update device_scheduler.py

--- backend/app/modules/simulator/programming/canvas/device_scheduler.py
+++ backend/app/modules/simulator/programming/canvas/device_scheduler.py
@@ -1,1 +1,56 @@
+from app.modules.simulator.programming.canvas.device_queue import (
+    device_queue,
+)
+from app.modules.simulator.programming.canvas.device_executor import (
+    device_executor,
+)
 
+
+class DeviceScheduler:
+
+    def execute(self):
+
+        while not device_queue.empty():
+
+            task = device_queue.pop()
+
+            device_executor.execute(
+
+                task["device"],
+
+                task["command"],
+
+                *task["args"],
+
+                **task["kwargs"],
+
+            )
+
+    def add(
+
+        self,
+
+        device,
+
+        command,
+
+        *args,
+
+        **kwargs,
+
+    ):
+
+        device_queue.push(
+
+            device,
+
+            command,
+
+            *args,
+
+            **kwargs,
+
+        )
+
+
+device_scheduler = DeviceScheduler()
