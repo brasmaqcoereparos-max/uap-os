a764deff0bcfc24d93cded5dcf30a6fe3eac9468
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:15:04 -0300

    Update device_project_api.py

--- backend/app/modules/simulator/programming/canvas/device_project_api.py
+++ backend/app/modules/simulator/programming/canvas/device_project_api.py
@@ -1,1 +1,56 @@
+from fastapi import APIRouter
 
+from app.modules.simulator.programming.canvas.device_project import (
+    device_project,
+)
+from app.modules.simulator.programming.canvas.device_workspace import (
+    device_workspace,
+)
+
+router = APIRouter(
+
+    prefix="/project",
+
+    tags=["Device Project"],
+
+)
+
+
+@router.get("/")
+def info():
+
+    return device_project.info()
+
+
+@router.post("/rename")
+def rename(
+    name: str,
+):
+
+    device_project.rename(name)
+
+    return device_project.info()
+
+
+@router.post("/save")
+def save():
+
+    device_workspace.save_workspace()
+
+    return {
+
+        "message": "Workspace saved",
+
+    }
+
+
+@router.post("/load")
+def load():
+
+    device_workspace.load_workspace()
+
+    return {
+
+        "message": "Workspace loaded",
+
+    }
