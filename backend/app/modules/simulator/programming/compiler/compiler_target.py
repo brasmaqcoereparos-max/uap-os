ec022902ee1a96995085138bb8043b56db7bf5f9
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:45:49 -0300

    Update compiler_target.py

--- backend/app/modules/simulator/programming/compiler/compiler_target.py
+++ backend/app/modules/simulator/programming/compiler/compiler_target.py
@@ -1,1 +1,24 @@
+from enum import Enum
 
+
+class CompilerTarget(Enum):
+
+    ARDUINO = "arduino"
+
+    ESP32 = "esp32"
+
+    RASPBERRY_PI_PICO = "raspberry_pi_pico"
+
+    STM32 = "stm32"
+
+    PYTHON = "python"
+
+    MICROPYTHON = "micropython"
+
+    CPP = "cpp"
+
+    LADDER = "ladder"
+
+    JSON = "json"
+
+    FLOWCHART = "flowchart"
