ca00f6c2c31d5a14de3d5c2b46742771716eaeb5
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:55:10 -0300

    Update compiler_include_builder.py

--- backend/app/modules/simulator/programming/compiler/compiler_include_builder.py
+++ backend/app/modules/simulator/programming/compiler/compiler_include_builder.py
@@ -1,1 +1,21 @@
+from app.modules.simulator.programming.compiler.compiler_include_manager import (
+    compiler_include_manager,
+)
 
+
+class CompilerIncludeBuilder:
+
+    def build(self):
+
+        return [
+
+            f"#include {include}"
+
+            for include
+
+            in compiler_include_manager.all()
+
+        ]
+
+
+compiler_include_builder = CompilerIncludeBuilder()
