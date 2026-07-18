f9dbd201b066f33cb8301e2ec9b3842465e5570f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:51:49 -0300

    Update compiler_validator.py

--- backend/app/modules/simulator/programming/compiler/compiler_validator.py
+++ backend/app/modules/simulator/programming/compiler/compiler_validator.py
@@ -1,1 +1,20 @@
+class CompilerValidator:
 
+    def validate(self, canvas):
+
+        errors = []
+
+        nodes = canvas.all_nodes()
+
+        if len(nodes) == 0:
+
+            errors.append(
+
+                "Canvas vazio."
+
+            )
+
+        return errors
+
+
+compiler_validator = CompilerValidator()
