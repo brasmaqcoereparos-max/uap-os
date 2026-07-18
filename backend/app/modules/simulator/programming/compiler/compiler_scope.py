7daae26118d4c67acb7722b41ea5fb702075b9ef
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 18:57:21 -0300

    Update compiler_scope.py

--- backend/app/modules/simulator/programming/compiler/compiler_scope.py
+++ backend/app/modules/simulator/programming/compiler/compiler_scope.py
@@ -1,1 +1,45 @@
 
+class CompilerScope:
+
+    def __init__(self):
+
+        self.stack = [{}]
+
+    def push(self):
+
+        self.stack.append({})
+
+    def pop(self):
+
+        if len(self.stack) > 1:
+
+            self.stack.pop()
+
+    def set(
+        self,
+        name,
+        value,
+    ):
+
+        self.stack[-1][name] = value
+
+    def get(
+        self,
+        name,
+        default=None,
+    ):
+
+        for scope in reversed(self.stack):
+
+            if name in scope:
+
+                return scope[name]
+
+        return default
+
+    def clear(self):
+
+        self.stack = [{}]
+
+
+compiler_scope = CompilerScope()
