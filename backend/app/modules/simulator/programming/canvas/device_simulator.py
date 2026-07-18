95ec7d2a3ec0592b7b7831b84f086430167e27d1
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 21:12:34 -0300

    Update device_simulator.py

--- backend/app/modules/simulator/programming/canvas/device_simulator.py
+++ backend/app/modules/simulator/programming/canvas/device_simulator.py
@@ -1,1 +1,28 @@
+class DeviceSimulator:
 
+    def __init__(self):
+
+        self.running = False
+
+    def start(self):
+
+        self.running = True
+
+    def stop(self):
+
+        self.running = False
+
+    def reset(self):
+
+        self.running = False
+
+    def status(self):
+
+        return {
+
+            "running": self.running,
+
+        }
+
+
+device_simulator = DeviceSimulator()
