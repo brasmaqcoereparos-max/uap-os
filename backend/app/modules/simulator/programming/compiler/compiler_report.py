aaa5ce3a9f77a1016cec730ae28266e17e3b1cf2
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 07:39:32 -0300

    Update compiler_report.py

--- backend/app/modules/simulator/programming/compiler/compiler_report.py
+++ backend/app/modules/simulator/programming/compiler/compiler_report.py
@@ -1,1 +1,54 @@
+class CompilerReport:
 
+    def __init__(self):
+
+        self.messages = []
+
+    def info(self, message):
+
+        self.messages.append(
+
+            {
+
+                "type": "info",
+
+                "message": message,
+
+            }
+
+        )
+
+    def warning(self, message):
+
+        self.messages.append(
+
+            {
+
+                "type": "warning",
+
+                "message": message,
+
+            }
+
+        )
+
+    def error(self, message):
+
+        self.messages.append(
+
+            {
+
+                "type": "error",
+
+                "message": message,
+
+            }
+
+        )
+
+    def all(self):
+
+        return self.messages.copy()
+
+
+compiler_report = CompilerReport()
