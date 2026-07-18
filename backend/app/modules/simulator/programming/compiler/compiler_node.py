3ac1eea6313eb0a09dd72d0b465075b0f689590a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 07:22:59 -0300

    Update compiler_node.py

--- backend/app/modules/simulator/programming/compiler/compiler_node.py
+++ backend/app/modules/simulator/programming/compiler/compiler_node.py
@@ -1,1 +1,29 @@
+class CompilerNode:
 
+    def __init__(self, node):
+
+        self.node = node
+
+    @property
+    def id(self):
+
+        return self.node.get("id")
+
+    @property
+    def name(self):
+
+        return self.node.get("name")
+
+    @property
+    def block_type(self):
+
+        return self.node.get("block_type")
+
+    @property
+    def config(self):
+
+        return self.node.get("config", {})
+
+    def value(self, key, default=None):
+
+        return self.config.get(key, default)
