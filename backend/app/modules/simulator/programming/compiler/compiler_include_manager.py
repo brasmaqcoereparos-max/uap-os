df2dbb93327dfeaad1eab40fbfa10982461df548
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:51:42 -0300

    Update compiler_include_manager.py

--- backend/app/modules/simulator/programming/compiler/compiler_include_manager.py
+++ backend/app/modules/simulator/programming/compiler/compiler_include_manager.py
@@ -1,1 +1,37 @@
+class CompilerIncludeManager:
 
+    def __init__(self):
+
+        self.includes = set()
+
+    def add(
+        self,
+        include,
+    ):
+
+        self.includes.add(include)
+
+    def remove(
+        self,
+        include,
+    ):
+
+        self.includes.discard(include)
+
+    def exists(
+        self,
+        include,
+    ):
+
+        return include in self.includes
+
+    def all(self):
+
+        return sorted(self.includes)
+
+    def clear(self):
+
+        self.includes.clear()
+
+
+compiler_include_manager = CompilerIncludeManager()
