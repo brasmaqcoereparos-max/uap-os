2ad056cd86872cb8761d21656e6c49a295220b59
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 07:02:32 -0300

    Update compiler_factory.py

--- backend/app/modules/simulator/programming/compiler/compiler_factory.py
+++ backend/app/modules/simulator/programming/compiler/compiler_factory.py
@@ -1,1 +1,31 @@
+from app.modules.simulator.programming.compiler.compiler_registry import (
+    compiler_registry,
+)
 
+
+class CompilerFactory:
+
+    def register(
+
+        self,
+
+        target,
+
+        compiler,
+
+    ):
+
+        compiler_registry.register(
+
+            target,
+
+            compiler,
+
+        )
+
+    def get(self, target):
+
+        return compiler_registry.get(target)
+
+
+compiler_factory = CompilerFactory()
