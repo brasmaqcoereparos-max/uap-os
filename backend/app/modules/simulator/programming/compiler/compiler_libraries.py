1194877aaef6b7c622675a70c0ee52f2b9218870
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 18:55:37 -0300

    Update compiler_libraries.py

--- backend/app/modules/simulator/programming/compiler/compiler_libraries.py
+++ backend/app/modules/simulator/programming/compiler/compiler_libraries.py
@@ -1,1 +1,37 @@
+class CompilerLibraries:
 
+    def __init__(self):
+
+        self.libraries = set()
+
+    def add(
+        self,
+        library,
+    ):
+
+        self.libraries.add(library)
+
+    def remove(
+        self,
+        library,
+    ):
+
+        self.libraries.discard(library)
+
+    def exists(
+        self,
+        library,
+    ):
+
+        return library in self.libraries
+
+    def all(self):
+
+        return sorted(self.libraries)
+
+    def clear(self):
+
+        self.libraries.clear()
+
+
+compiler_libraries = CompilerLibraries()
