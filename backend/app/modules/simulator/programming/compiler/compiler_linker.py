5d3b24b0a0556e609a9382ca48cfa62227a21769
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:44:16 -0300

    Update compiler_linker.py

--- backend/app/modules/simulator/programming/compiler/compiler_linker.py
+++ backend/app/modules/simulator/programming/compiler/compiler_linker.py
@@ -1,1 +1,19 @@
+class CompilerLinker:
 
+    def link(
+        self,
+        sections,
+    ):
+
+        output = []
+
+        for section in sections:
+
+            if section:
+
+                output.append(section)
+
+        return "\n\n".join(output)
+
+
+compiler_linker = CompilerLinker()
