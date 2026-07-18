40f7c3ac5a14f813d52f36a8355c24d5d62938ff
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:13:30 -0300

    Update device_project.py

--- backend/app/modules/simulator/programming/canvas/device_project.py
+++ backend/app/modules/simulator/programming/canvas/device_project.py
@@ -1,1 +1,36 @@
+from datetime import datetime
 
+
+class DeviceProject:
+
+    def __init__(self):
+
+        self.name = "New Project"
+
+        self.created = datetime.now()
+
+        self.modified = datetime.now()
+
+    def rename(
+        self,
+        name,
+    ):
+
+        self.name = name
+
+        self.modified = datetime.now()
+
+    def info(self):
+
+        return {
+
+            "name": self.name,
+
+            "created": self.created.isoformat(),
+
+            "modified": self.modified.isoformat(),
+
+        }
+
+
+device_project = DeviceProject()
