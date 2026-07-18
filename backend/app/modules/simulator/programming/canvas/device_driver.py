a04bb20a84b637b8b4aaac0b6c840eaab9f623f6
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 21:55:10 -0300

    Update device_driver.py

--- backend/app/modules/simulator/programming/canvas/device_driver.py
+++ backend/app/modules/simulator/programming/canvas/device_driver.py
@@ -1,1 +1,41 @@
+class DeviceDriver:
 
+    def __init__(self):
+
+        self.drivers = {}
+
+    def register(
+        self,
+        name,
+        driver,
+    ):
+
+        self.drivers[name] = driver
+
+    def get(
+        self,
+        name,
+    ):
+
+        return self.drivers.get(name)
+
+    def remove(
+        self,
+        name,
+    ):
+
+        self.drivers.pop(
+            name,
+            None,
+        )
+
+    def clear(self):
+
+        self.drivers.clear()
+
+    def all(self):
+
+        return list(self.drivers.keys())
+
+
+device_driver = DeviceDriver()
