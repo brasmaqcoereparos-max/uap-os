01390f08b27c6da389949993d2c2c520fbd3abe7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 20:18:25 -0300

    Update compiler_codegen.py

--- backend/app/modules/simulator/programming/compiler/compiler_codegen.py
+++ backend/app/modules/simulator/programming/compiler/compiler_codegen.py
@@ -1,1 +1,18 @@
+from app.modules.simulator.programming.compiler.compiler_source_builder import (
+    compiler_source_builder,
+)
 
+
+class CompilerCodeGenerator:
+
+    def generate(
+        self,
+        context,
+    ):
+
+        return compiler_source_builder.build(
+            context
+        )
+
+
+compiler_codegen = CompilerCodeGenerator()
