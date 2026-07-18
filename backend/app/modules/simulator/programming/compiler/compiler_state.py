9ae67462113e70ea80050c16a3c42e6c941c5957
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:09:23 -0300

    Update compiler_state.py

--- backend/app/modules/simulator/programming/compiler/compiler_state.py
+++ backend/app/modules/simulator/programming/compiler/compiler_state.py
@@ -1,1 +1,62 @@
+class CompilerState:
 
+    def __init__(self):
+
+        self.state = {}
+
+    def set(
+
+        self,
+
+        key,
+
+        value,
+
+    ):
+
+        self.state[key] = value
+
+    def get(
+
+        self,
+
+        key,
+
+        default=None,
+
+    ):
+
+        return self.state.get(
+
+            key,
+
+            default,
+
+        )
+
+    def remove(
+
+        self,
+
+        key,
+
+    ):
+
+        self.state.pop(
+
+            key,
+
+            None,
+
+        )
+
+    def clear(self):
+
+        self.state.clear()
+
+    def all(self):
+
+        return self.state.copy()
+
+
+compiler_state = CompilerState()
