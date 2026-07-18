156f8c9f3f111013a5229a852d6599638b84007f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:06:00 -0300

    Update compiler_dependency.py

--- backend/app/modules/simulator/programming/compiler/compiler_dependency.py
+++ backend/app/modules/simulator/programming/compiler/compiler_dependency.py
@@ -1,1 +1,55 @@
+class CompilerDependency:
 
+    def __init__(self):
+
+        self.dependencies = {}
+
+    def add(
+        self,
+        source,
+        dependency,
+    ):
+
+        self.dependencies.setdefault(
+
+            source,
+
+            set(),
+
+        ).add(dependency)
+
+    def get(
+        self,
+        source,
+    ):
+
+        return sorted(
+
+            self.dependencies.get(
+
+                source,
+
+                set(),
+
+            )
+
+        )
+
+    def clear(self):
+
+        self.dependencies.clear()
+
+    def all(self):
+
+        return {
+
+            key: sorted(value)
+
+            for key, value
+
+            in self.dependencies.items()
+
+        }
+
+
+compiler_dependency = CompilerDependency()
