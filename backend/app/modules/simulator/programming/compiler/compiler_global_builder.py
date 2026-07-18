56efde1b7b8ae848d5ed002bfef0473ab6a38700
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 20:12:05 -0300

    Update compiler_global_builder.py

--- backend/app/modules/simulator/programming/compiler/compiler_global_builder.py
+++ backend/app/modules/simulator/programming/compiler/compiler_global_builder.py
@@ -1,1 +1,11 @@
+class CompilerGlobalBuilder:
 
+    def build(
+        self,
+        globals_list,
+    ):
+
+        return list(globals_list)
+
+
+compiler_global_builder = CompilerGlobalBuilder()
