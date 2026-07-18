a2a9a9dd59886d1c7150d2f897a733edcac2ee82
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:42:46 -0300

    Update compiler_postprocessor.py

--- backend/app/modules/simulator/programming/compiler/compiler_postprocessor.py
+++ backend/app/modules/simulator/programming/compiler/compiler_postprocessor.py
@@ -1,1 +1,21 @@
+class CompilerPostprocessor:
 
+    def process(
+        self,
+        text,
+    ):
+
+        lines = []
+
+        for line in text.splitlines():
+
+            lines.append(
+
+                line.rstrip()
+
+            )
+
+        return "\n".join(lines)
+
+
+compiler_postprocessor = CompilerPostprocessor()
