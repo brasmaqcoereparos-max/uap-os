5697c4b727cbcc652c7957e03751e01bf4effb8c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:40:23 -0300

    Update compiler_registry.py

--- backend/app/modules/simulator/programming/compiler/compiler_registry.py
+++ backend/app/modules/simulator/programming/compiler/compiler_registry.py
@@ -1,1 +1,32 @@
+class CompilerRegistry:
 
+    def __init__(self):
+
+        self.compilers = {}
+
+    def register(
+
+        self,
+
+        platform,
+
+        compiler,
+
+    ):
+
+        self.compilers[platform] = compiler
+
+    def get(self, platform):
+
+        return self.compilers.get(platform)
+
+    def platforms(self):
+
+        return sorted(
+
+            self.compilers.keys()
+
+        )
+
+
+compiler_registry = CompilerRegistry()
