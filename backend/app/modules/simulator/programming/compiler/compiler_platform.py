f581259f8eee1387d6bd05c43d38df6ea43f700e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 18:41:36 -0300

    Update compiler_platform.py

--- backend/app/modules/simulator/programming/compiler/compiler_platform.py
+++ backend/app/modules/simulator/programming/compiler/compiler_platform.py
@@ -1,1 +1,20 @@
+class CompilerPlatform:
 
+    def __init__(self):
+
+        self.platform = "arduino"
+
+    def set(self, platform):
+
+        self.platform = platform
+
+    def get(self):
+
+        return self.platform
+
+    def is_target(self, target):
+
+        return self.platform == target
+
+
+compiler_platform = CompilerPlatform()
