3f5e85c6cd2c64b1f78a3e87222ac7e6ac554bc7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 21:43:59 -0300

    Update device_factory.py

--- backend/app/modules/simulator/programming/canvas/device_factory.py
+++ backend/app/modules/simulator/programming/canvas/device_factory.py
@@ -1,1 +1,16 @@
+from app.modules.simulator.programming.canvas.device_base import (
+    DeviceBase,
+)
 
+
+class DeviceFactory:
+
+    def create(
+        self,
+        name,
+    ):
+
+        return DeviceBase(name)
+
+
+device_factory = DeviceFactory()
