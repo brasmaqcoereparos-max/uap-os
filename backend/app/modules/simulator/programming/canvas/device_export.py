9d29c75e3e53271e5fb6dcdd24d078ef51401990
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:05:12 -0300

    Update device_export.py

--- backend/app/modules/simulator/programming/canvas/device_export.py
+++ backend/app/modules/simulator/programming/canvas/device_export.py
@@ -1,1 +1,25 @@
+import json
 
+from app.modules.simulator.programming.canvas.device_manager import (
+    device_manager,
+)
+
+
+class DeviceExport:
+
+    def export(self):
+
+        return json.dumps(
+
+            device_manager.all(),
+
+            indent=4,
+
+        )
+
+    def export_dict(self):
+
+        return device_manager.all()
+
+
+device_export = DeviceExport()
