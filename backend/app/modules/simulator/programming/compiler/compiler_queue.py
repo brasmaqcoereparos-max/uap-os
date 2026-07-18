e52132012a8b3570260cbe6cc70d25182d5d56e2
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:00:18 -0300

    Update compiler_queue.py

--- backend/app/modules/simulator/programming/compiler/compiler_queue.py
+++ backend/app/modules/simulator/programming/compiler/compiler_queue.py
@@ -1,1 +1,42 @@
+from collections import deque
 
+
+class CompilerQueue:
+
+    def __init__(self):
+
+        self.queue = deque()
+
+    def enqueue(
+        self,
+        item,
+    ):
+
+        self.queue.append(item)
+
+    def dequeue(self):
+
+        if not self.queue:
+
+            return None
+
+        return self.queue.popleft()
+
+    def peek(self):
+
+        if not self.queue:
+
+            return None
+
+        return self.queue[0]
+
+    def size(self):
+
+        return len(self.queue)
+
+    def clear(self):
+
+        self.queue.clear()
+
+
+compiler_queue = CompilerQueue()
