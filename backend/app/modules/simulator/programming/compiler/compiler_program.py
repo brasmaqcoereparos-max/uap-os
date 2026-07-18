7ee1591d3fdae74cf31e4dcdcbdfc0e4a8b30ed1
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 07:34:29 -0300

    Update compiler_program.py

--- backend/app/modules/simulator/programming/compiler/compiler_program.py
+++ backend/app/modules/simulator/programming/compiler/compiler_program.py
@@ -1,1 +1,45 @@
+from app.modules.simulator.programming.compiler.compiler_pipeline import (
+    compiler_pipeline,
+)
+from app.modules.simulator.programming.compiler.compiler_executor import (
+    compiler_executor,
+)
+from app.modules.simulator.programming.compiler.compiler_generator import (
+    compiler_generator,
+)
 
+
+class CompilerProgram:
+
+    def compile(
+
+        self,
+
+        canvas,
+
+        context,
+
+    ):
+
+        nodes = compiler_pipeline.process(
+
+            canvas,
+
+        )
+
+        compiler_executor.execute(
+
+            nodes,
+
+            context,
+
+        )
+
+        return compiler_generator.generate(
+
+            context.build()
+
+        )
+
+
+compiler_program = CompilerProgram()
