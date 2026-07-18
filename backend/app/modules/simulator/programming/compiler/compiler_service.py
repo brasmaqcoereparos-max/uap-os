ac38243d3e7dbcfb4262a84d2acb0c411883ea30
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:42:00 -0300

    Update compiler_service.py

--- backend/app/modules/simulator/programming/compiler/compiler_service.py
+++ backend/app/modules/simulator/programming/compiler/compiler_service.py
@@ -1,1 +1,39 @@
+from fastapi import HTTPException
 
+from app.modules.simulator.programming.compiler.compiler_registry import (
+    compiler_registry,
+)
+
+
+class CompilerService:
+
+    def compile(
+
+        self,
+
+        platform,
+
+        canvas,
+
+    ):
+
+        compiler = compiler_registry.get(platform)
+
+        if compiler is None:
+
+            raise HTTPException(
+
+                status_code=404,
+
+                detail="Compiler not found",
+
+            )
+
+        compiler.reset()
+
+        compiler.compile(canvas)
+
+        return compiler.context().build()
+
+
+compiler_service = CompilerService()
