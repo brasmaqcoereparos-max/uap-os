474aa5f4de2a0e897ac69184063ca83897940d10
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:53:33 -0300

    Update compiler_engine.py

--- backend/app/modules/simulator/programming/compiler/compiler_engine.py
+++ backend/app/modules/simulator/programming/compiler/compiler_engine.py
@@ -1,1 +1,50 @@
+from app.modules.simulator.programming.compiler.compiler_result import (
+    CompilerResult,
+)
+from app.modules.simulator.programming.compiler.compiler_validator import (
+    compiler_validator,
+)
 
+
+class CompilerEngine:
+
+    def compile(
+
+        self,
+
+        compiler,
+
+        canvas,
+
+    ):
+
+        result = CompilerResult()
+
+        errors = compiler_validator.validate(
+
+            canvas
+
+        )
+
+        if errors:
+
+            for error in errors:
+
+                result.add_error(error)
+
+            return result
+
+        compiler.reset()
+
+        compiler.compile(canvas)
+
+        result.set_output(
+
+            compiler.context().build()
+
+        )
+
+        return result
+
+
+compiler_engine = CompilerEngine()
