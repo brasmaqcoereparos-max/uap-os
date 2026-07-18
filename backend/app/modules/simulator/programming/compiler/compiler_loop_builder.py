97ce7678a497734d8a6fc0d47a6ccf2b049f4098
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 20:15:17 -0300

    Update compiler_loop_builder.py

--- backend/app/modules/simulator/programming/compiler/compiler_loop_builder.py
+++ backend/app/modules/simulator/programming/compiler/compiler_loop_builder.py
@@ -1,1 +1,21 @@
+class CompilerLoopBuilder:
 
+    def build(
+        self,
+        loop,
+    ):
+
+        lines = []
+
+        lines.append("void loop() {")
+
+        for line in loop:
+
+            lines.append(f"    {line}")
+
+        lines.append("}")
+
+        return lines
+
+
+compiler_loop_builder = CompilerLoopBuilder()
