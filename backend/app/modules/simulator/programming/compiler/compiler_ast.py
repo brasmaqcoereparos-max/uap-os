7f8354cdc7f637f02f952881bcc3cd762232d5a8
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:11:23 -0300

    Update compiler_ast.py

--- backend/app/modules/simulator/programming/compiler/compiler_ast.py
+++ backend/app/modules/simulator/programming/compiler/compiler_ast.py
@@ -1,1 +1,38 @@
+class CompilerAST:
 
+    def __init__(self):
+
+        self.nodes = []
+
+    def add(
+
+        self,
+
+        node,
+
+    ):
+
+        self.nodes.append(node)
+
+    def remove(
+
+        self,
+
+        node,
+
+    ):
+
+        if node in self.nodes:
+
+            self.nodes.remove(node)
+
+    def all(self):
+
+        return self.nodes.copy()
+
+    def clear(self):
+
+        self.nodes.clear()
+
+
+compiler_ast = CompilerAST()
