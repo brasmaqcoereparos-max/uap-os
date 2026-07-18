4ea923fcd0168b30e9b9cf45b87da3c4ca2b0c56
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 07:25:08 -0300

    Update compiler_node_factory.py

--- backend/app/modules/simulator/programming/compiler/compiler_node_factory.py
+++ backend/app/modules/simulator/programming/compiler/compiler_node_factory.py
@@ -1,1 +1,13 @@
+from app.modules.simulator.programming.compiler.compiler_node import (
+    CompilerNode,
+)
 
+
+class CompilerNodeFactory:
+
+    def create(self, node):
+
+        return CompilerNode(node)
+
+
+compiler_node_factory = CompilerNodeFactory()
