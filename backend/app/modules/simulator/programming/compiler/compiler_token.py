0c48b4942e4ff4eaa09b7e115cd3e10026a09420
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:13:18 -0300

    Update compiler_token.py

--- backend/app/modules/simulator/programming/compiler/compiler_token.py
+++ backend/app/modules/simulator/programming/compiler/compiler_token.py
@@ -1,1 +1,37 @@
+class CompilerToken:
 
+    def __init__(
+
+        self,
+
+        token_type,
+
+        value,
+
+        line=0,
+
+        column=0,
+
+    ):
+
+        self.type = token_type
+
+        self.value = value
+
+        self.line = line
+
+        self.column = column
+
+    def to_dict(self):
+
+        return {
+
+            "type": self.type,
+
+            "value": self.value,
+
+            "line": self.line,
+
+            "column": self.column,
+
+        }
