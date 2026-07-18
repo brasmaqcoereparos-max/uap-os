dd369bdfa3662b24c8b0279a5a68a7f0dfd5a783
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 05:47:24 -0300

    Update device_cache.py

--- backend/app/modules/simulator/programming/canvas/device_cache.py
+++ backend/app/modules/simulator/programming/canvas/device_cache.py
@@ -1,1 +1,56 @@
+class DeviceCache:
 
+    def __init__(self):
+
+        self.cache = {}
+
+    def put(
+
+        self,
+
+        key,
+
+        value,
+
+    ):
+
+        self.cache[key] = value
+
+    def get(
+
+        self,
+
+        key,
+
+        default=None,
+
+    ):
+
+        return self.cache.get(
+
+            key,
+
+            default,
+
+        )
+
+    def remove(self, key):
+
+        self.cache.pop(
+
+            key,
+
+            None,
+
+        )
+
+    def clear(self):
+
+        self.cache.clear()
+
+    def size(self):
+
+        return len(self.cache)
+
+
+device_cache = DeviceCache()
