d3723ffad95a5a061db4921f0083ab1d9eac0e61
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:17:06 -0300

    Update compiler_symbol_table.py

--- backend/app/modules/simulator/programming/compiler/compiler_symbol_table.py
+++ backend/app/modules/simulator/programming/compiler/compiler_symbol_table.py
@@ -1,1 +1,48 @@
+class CompilerSymbolTable:
 
+    def __init__(self):
+
+        self.table = {}
+
+    def define(
+
+        self,
+
+        name,
+
+        value,
+
+    ):
+
+        self.table[name] = value
+
+    def resolve(
+
+        self,
+
+        name,
+
+    ):
+
+        return self.table.get(name)
+
+    def exists(
+
+        self,
+
+        name,
+
+    ):
+
+        return name in self.table
+
+    def clear(self):
+
+        self.table.clear()
+
+    def all(self):
+
+        return self.table.copy()
+
+
+compiler_symbol_table = CompilerSymbolTable()
