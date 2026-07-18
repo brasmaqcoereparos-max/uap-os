563bf3aa3bb5ece321379e17002355018a15c9fc
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 08:09:48 -0300

    Update compiler_logger.py

--- backend/app/modules/simulator/programming/compiler/compiler_logger.py
+++ backend/app/modules/simulator/programming/compiler/compiler_logger.py
@@ -1,1 +1,73 @@
+from datetime import datetime
 
+
+class CompilerLogger:
+
+    def __init__(self):
+
+        self.logs = []
+
+    def log(
+
+        self,
+
+        level,
+
+        message,
+
+    ):
+
+        self.logs.append(
+
+            {
+
+                "time": datetime.now().isoformat(),
+
+                "level": level,
+
+                "message": message,
+
+            }
+
+        )
+
+    def info(self, message):
+
+        self.log(
+
+            "INFO",
+
+            message,
+
+        )
+
+    def warning(self, message):
+
+        self.log(
+
+            "WARNING",
+
+            message,
+
+        )
+
+    def error(self, message):
+
+        self.log(
+
+            "ERROR",
+
+            message,
+
+        )
+
+    def all(self):
+
+        return self.logs.copy()
+
+    def clear(self):
+
+        self.logs.clear()
+
+
+compiler_logger = CompilerLogger()
