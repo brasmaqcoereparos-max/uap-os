485997bb076586a17f097222496e926be79f5f97
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 07:43:18 -0300

    Update compiler_session.py

--- backend/app/modules/simulator/programming/compiler/compiler_session.py
+++ backend/app/modules/simulator/programming/compiler/compiler_session.py
@@ -1,1 +1,40 @@
+from uuid import uuid4
+from datetime import datetime
 
+
+class CompilerSession:
+
+    def __init__(self):
+
+        self.id = str(uuid4())
+
+        self.started = datetime.now()
+
+        self.finished = None
+
+    def finish(self):
+
+        self.finished = datetime.now()
+
+    def to_dict(self):
+
+        return {
+
+            "id": self.id,
+
+            "started": self.started.isoformat(),
+
+            "finished": (
+
+                self.finished.isoformat()
+
+                if self.finished
+
+                else None
+
+            ),
+
+        }
+
+
+compiler_session = CompilerSession()
