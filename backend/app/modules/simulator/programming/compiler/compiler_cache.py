bf2da26fd467da0a77132095f8cdcce58f3c3cfd
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 08:07:54 -0300

    Update compiler_cache.py

--- backend/app/modules/simulator/programming/compiler/compiler_cache.py
+++ backend/app/modules/simulator/programming/compiler/compiler_cache.py
@@ -1,1 +1,60 @@
+class CompilerCache:
 
+    def __init__(self):
+
+        self.cache = {}
+
+    def put(
+
+        self,
+
+        key,
+
+        value,
+
+    ):
+
+        self.cache[key] = value
+
+    def get(
+
+        self,
+
+        key,
+
+    ):
+
+        return self.cache.get(key)
+
+    def exists(
+
+        self,
+
+        key,
+
+    ):
+
+        return key in self.cache
+
+    def remove(
+
+        self,
+
+        key,
+
+    ):
+
+        self.cache.pop(
+
+            key,
+
+            None,
+
+        )
+
+    def clear(self):
+
+        self.cache.clear()
+
+
+compiler_cache = CompilerCache()
