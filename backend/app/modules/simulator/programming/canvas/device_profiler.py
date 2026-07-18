6651b6ad4d4d57845f2933a30ef337144936eb71
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 05:23:42 -0300

    Update device_profiler.py

--- backend/app/modules/simulator/programming/canvas/device_profiler.py
+++ backend/app/modules/simulator/programming/canvas/device_profiler.py
@@ -1,1 +1,40 @@
+from datetime import datetime
 
+
+class DeviceProfiler:
+
+    def __init__(self):
+
+        self.data = {}
+
+    def begin(self, device):
+
+        self.data[device] = {
+            "start": datetime.now(),
+            "elapsed": 0.0,
+            "calls": self.data.get(device, {}).get("calls", 0),
+        }
+
+    def finish(self, device):
+
+        if device not in self.data:
+            return
+
+        elapsed = (
+            datetime.now()
+            - self.data[device]["start"]
+        ).total_seconds()
+
+        self.data[device]["elapsed"] = elapsed
+        self.data[device]["calls"] += 1
+
+    def get(self, device):
+
+        return self.data.get(device)
+
+    def clear(self):
+
+        self.data.clear()
+
+
+device_profiler = DeviceProfiler()
