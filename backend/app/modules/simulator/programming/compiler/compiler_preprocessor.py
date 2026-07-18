0fed03cefe48cb8d36843fa024786d4cff34677f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:40:54 -0300

    Update compiler_preprocessor.py

--- backend/app/modules/simulator/programming/compiler/compiler_preprocessor.py
+++ backend/app/modules/simulator/programming/compiler/compiler_preprocessor.py
@@ -1,1 +1,26 @@
+from app.modules.simulator.programming.compiler.compiler_macros import (
+    compiler_macros,
+)
 
+
+class CompilerPreprocessor:
+
+    def process(
+        self,
+        text,
+    ):
+
+        for name, value in compiler_macros.all().items():
+
+            text = text.replace(
+
+                f"${name}$",
+
+                str(value),
+
+            )
+
+        return text
+
+
+compiler_preprocessor = CompilerPreprocessor()
