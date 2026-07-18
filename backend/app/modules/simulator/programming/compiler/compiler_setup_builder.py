0a13f4caafe6883bf06430101a40b80b87ec34fa
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 20:13:36 -0300

    Update compiler_setup_builder.py

--- backend/app/modules/simulator/programming/compiler/compiler_setup_builder.py
+++ backend/app/modules/simulator/programming/compiler/compiler_setup_builder.py
@@ -1,1 +1,21 @@
+class CompilerSetupBuilder:
 
+    def build(
+        self,
+        setup,
+    ):
+
+        lines = []
+
+        lines.append("void setup() {")
+
+        for line in setup:
+
+            lines.append(f"    {line}")
+
+        lines.append("}")
+
+        return lines
+
+
+compiler_setup_builder = CompilerSetupBuilder()
