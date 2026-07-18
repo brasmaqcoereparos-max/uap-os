74cb904368de69ea73974b219f121c264da16332
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 18:45:21 -0300

    Update compiler_variables.py

--- backend/app/modules/simulator/programming/compiler/compiler_variables.py
+++ backend/app/modules/simulator/programming/compiler/compiler_variables.py
@@ -1,1 +1,39 @@
+class CompilerVariables:
 
+    def __init__(self):
+
+        self.variables = {}
+
+    def add(
+        self,
+        name,
+        value=None,
+    ):
+
+        self.variables[name] = value
+
+    def exists(self, name):
+
+        return name in self.variables
+
+    def get(
+        self,
+        name,
+        default=None,
+    ):
+
+        return self.variables.get(
+            name,
+            default,
+        )
+
+    def all(self):
+
+        return self.variables.copy()
+
+    def clear(self):
+
+        self.variables.clear()
+
+
+compiler_variables = CompilerVariables()
