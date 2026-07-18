45eeb8b8c6fe97d3b7ab6678658299ad7fcd600e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:31:00 -0300

    Update compiler_ir.py

--- backend/app/modules/simulator/programming/compiler/compiler_ir.py
+++ backend/app/modules/simulator/programming/compiler/compiler_ir.py
@@ -1,1 +1,42 @@
+class CompilerIR:
 
+    def __init__(self):
+
+        self.instructions = []
+
+    def add(
+
+        self,
+
+        opcode,
+
+        *operands,
+
+    ):
+
+        self.instructions.append(
+
+            {
+
+                "opcode": opcode,
+
+                "operands": list(
+
+                    operands,
+
+                ),
+
+            }
+
+        )
+
+    def all(self):
+
+        return self.instructions.copy()
+
+    def clear(self):
+
+        self.instructions.clear()
+
+
+compiler_ir = CompilerIR()
