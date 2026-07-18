e2417bd6a19238c891461f714a8d180363dd44ef
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 07:00:48 -0300

    Update compiler_pipeline.py

--- backend/app/modules/simulator/programming/compiler/compiler_pipeline.py
+++ backend/app/modules/simulator/programming/compiler/compiler_pipeline.py
@@ -1,1 +1,20 @@
+from app.modules.simulator.programming.compiler.compiler_parser import (
+    compiler_parser,
+)
+from app.modules.simulator.programming.compiler.compiler_optimizer import (
+    compiler_optimizer,
+)
 
+
+class CompilerPipeline:
+
+    def process(self, canvas):
+
+        nodes = compiler_parser.parse(canvas)
+
+        nodes = compiler_optimizer.optimize(nodes)
+
+        return nodes
+
+
+compiler_pipeline = CompilerPipeline()
