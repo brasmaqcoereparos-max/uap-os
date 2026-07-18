388d81d68d87b0cce077cfc628611279e9b2bc23
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 20:51:04 -0300

    Update compiler_ir_serializer.py

--- backend/app/modules/simulator/programming/compiler/compiler_ir_serializer.py
+++ backend/app/modules/simulator/programming/compiler/compiler_ir_serializer.py
@@ -1,1 +1,25 @@
+import json
 
+from app.modules.simulator.programming.compiler.compiler_ir import (
+    compiler_ir,
+)
+
+
+class CompilerIRSerializer:
+
+    def dumps(self):
+
+        return json.dumps(
+
+            compiler_ir.all(),
+
+            indent=4,
+
+        )
+
+    def loads(self, content):
+
+        return json.loads(content)
+
+
+compiler_ir_serializer = CompilerIRSerializer()
