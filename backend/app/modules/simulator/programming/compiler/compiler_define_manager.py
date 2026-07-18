a4999ae15e04d5b30f27758c4801dbc3d17dbb6c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:53:36 -0300

    Update compiler_define_manager.py

--- backend/app/modules/simulator/programming/compiler/compiler_define_manager.py
+++ backend/app/modules/simulator/programming/compiler/compiler_define_manager.py
@@ -1,1 +1,38 @@
+class CompilerDefineManager:
 
+    def __init__(self):
+
+        self.defines = {}
+
+    def add(
+        self,
+        name,
+        value="",
+    ):
+
+        self.defines[name] = value
+
+    def remove(
+        self,
+        name,
+    ):
+
+        self.defines.pop(name, None)
+
+    def exists(
+        self,
+        name,
+    ):
+
+        return name in self.defines
+
+    def all(self):
+
+        return self.defines.copy()
+
+    def clear(self):
+
+        self.defines.clear()
+
+
+compiler_define_manager = CompilerDefineManager()
