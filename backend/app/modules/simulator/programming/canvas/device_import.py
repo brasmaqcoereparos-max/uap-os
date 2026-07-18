491ae1a81da4bac35bba2272aeb41329334fdce2
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:06:42 -0300

    Update device_import.py

--- backend/app/modules/simulator/programming/canvas/device_import.py
+++ backend/app/modules/simulator/programming/canvas/device_import.py
@@ -1,1 +1,38 @@
+import json
 
+from app.modules.simulator.programming.canvas.device_serializer import (
+    device_serializer,
+)
+from app.modules.simulator.programming.canvas.device_manager import (
+    device_manager,
+)
+
+
+class DeviceImport:
+
+    def import_json(
+        self,
+        content,
+    ):
+
+        devices = json.loads(content)
+
+        for _, data in devices.items():
+
+            device = device_serializer.deserialize(data)
+
+            device_manager.register(device)
+
+    def import_dict(
+        self,
+        devices,
+    ):
+
+        for _, data in devices.items():
+
+            device = device_serializer.deserialize(data)
+
+            device_manager.register(device)
+
+
+device_import = DeviceImport()
