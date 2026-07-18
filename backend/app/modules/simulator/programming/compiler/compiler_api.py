c3e40b677811f23875c4029f182eb88fe2e7705c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:43:44 -0300

    Update compiler_api.py

--- backend/app/modules/simulator/programming/compiler/compiler_api.py
+++ backend/app/modules/simulator/programming/compiler/compiler_api.py
@@ -1,1 +1,26 @@
+from fastapi import APIRouter
 
+from app.modules.simulator.programming.canvas.canvas import canvas
+from app.modules.simulator.programming.compiler.compiler_service import (
+    compiler_service,
+)
+
+router = APIRouter(
+
+    prefix="/compiler",
+
+    tags=["Compiler"],
+
+)
+
+
+@router.post("/{platform}")
+def compile(platform: str):
+
+    return compiler_service.compile(
+
+        platform,
+
+        canvas,
+
+    )
