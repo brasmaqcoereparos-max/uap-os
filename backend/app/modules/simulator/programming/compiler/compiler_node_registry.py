08f641d091f676e0de5efcac3035de670f4606ea
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 07:27:41 -0300

    Update compiler_node_registry.py

--- backend/app/modules/simulator/programming/compiler/compiler_node_registry.py
+++ backend/app/modules/simulator/programming/compiler/compiler_node_registry.py
@@ -1,1 +1,32 @@
+class CompilerNodeRegistry:
 
+    def __init__(self):
+
+        self.registry = {}
+
+    def register(
+
+        self,
+
+        block_type,
+
+        handler,
+
+    ):
+
+        self.registry[block_type] = handler
+
+    def get(self, block_type):
+
+        return self.registry.get(block_type)
+
+    def exists(self, block_type):
+
+        return block_type in self.registry
+
+    def clear(self):
+
+        self.registry.clear()
+
+
+compiler_node_registry = CompilerNodeRegistry()
