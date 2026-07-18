15e1a7803791ea92363c52237cd620571188c4d7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:57:39 -0300

    Update compiler_parser.py

--- backend/app/modules/simulator/programming/compiler/compiler_parser.py
+++ backend/app/modules/simulator/programming/compiler/compiler_parser.py
@@ -1,1 +1,14 @@
+class CompilerParser:
 
+    def parse(self, canvas):
+
+        nodes = []
+
+        for node in canvas.all_nodes():
+
+            nodes.append(node.to_dict())
+
+        return nodes
+
+
+compiler_parser = CompilerParser()
