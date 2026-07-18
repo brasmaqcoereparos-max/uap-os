bb9b11fcd0c116ddb37bffc6a0882b50a7f65025
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 18:58:47 -0300

    Update compiler_stack.py

--- backend/app/modules/simulator/programming/compiler/compiler_stack.py
+++ backend/app/modules/simulator/programming/compiler/compiler_stack.py
@@ -1,1 +1,39 @@
+class CompilerStack:
 
+    def __init__(self):
+
+        self.stack = []
+
+    def push(
+        self,
+        value,
+    ):
+
+        self.stack.append(value)
+
+    def pop(self):
+
+        if not self.stack:
+
+            return None
+
+        return self.stack.pop()
+
+    def peek(self):
+
+        if not self.stack:
+
+            return None
+
+        return self.stack[-1]
+
+    def size(self):
+
+        return len(self.stack)
+
+    def clear(self):
+
+        self.stack.clear()
+
+
+compiler_stack = CompilerStack()
