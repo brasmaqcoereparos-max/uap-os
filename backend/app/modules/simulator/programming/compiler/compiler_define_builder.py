63d5fc459c6e4c2898fc8cde1d43dc00fb7980bb
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 20:05:45 -0300

    Update compiler_define_builder.py

--- backend/app/modules/simulator/programming/compiler/compiler_define_builder.py
+++ backend/app/modules/simulator/programming/compiler/compiler_define_builder.py
@@ -1,1 +1,33 @@
+from app.modules.simulator.programming.compiler.compiler_define_manager import (
+    compiler_define_manager,
+)
 
+
+class CompilerDefineBuilder:
+
+    def build(self):
+
+        lines = []
+
+        for name, value in compiler_define_manager.all().items():
+
+            if value == "":
+
+                lines.append(
+
+                    f"#define {name}"
+
+                )
+
+            else:
+
+                lines.append(
+
+                    f"#define {name} {value}"
+
+                )
+
+        return lines
+
+
+compiler_define_builder = CompilerDefineBuilder()
