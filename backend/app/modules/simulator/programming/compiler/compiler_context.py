dca2badd270e031c03ce755007fde83a9fe4d788
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:36:52 -0300

    Update compiler_context.py

--- backend/app/modules/simulator/programming/compiler/compiler_context.py
+++ backend/app/modules/simulator/programming/compiler/compiler_context.py
@@ -1,1 +1,62 @@
+class CompilerContext:
 
+    def __init__(self):
+
+        self.variables = {}
+
+        self.includes = set()
+
+        self.definitions = set()
+
+        self.globals = []
+
+        self.setup = []
+
+        self.loop = []
+
+    def add_include(self, include):
+
+        self.includes.add(include)
+
+    def add_definition(self, definition):
+
+        self.definitions.add(definition)
+
+    def add_global(self, code):
+
+        self.globals.append(code)
+
+    def add_setup(self, code):
+
+        self.setup.append(code)
+
+    def add_loop(self, code):
+
+        self.loop.append(code)
+
+    def set_variable(self, name, value):
+
+        self.variables[name] = value
+
+    def get_variable(self, name, default=None):
+
+        return self.variables.get(name, default)
+
+    def build(self):
+
+        return {
+
+            "includes": sorted(self.includes),
+
+            "definitions": sorted(self.definitions),
+
+            "globals": self.globals,
+
+            "setup": self.setup,
+
+            "loop": self.loop,
+
+        }
+
+
+compiler_context = CompilerContext()
