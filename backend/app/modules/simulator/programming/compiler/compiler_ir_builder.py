28da0aa7a01667dcc359cfd79a29e329e7fe8cdf
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:32:52 -0300

    Update compiler_ir_builder.py

--- backend/app/modules/simulator/programming/compiler/compiler_ir_builder.py
+++ backend/app/modules/simulator/programming/compiler/compiler_ir_builder.py
@@ -1,1 +1,41 @@
+from app.modules.simulator.programming.compiler.compiler_ir import (
+    compiler_ir,
+)
 
+
+class CompilerIRBuilder:
+
+    def build(
+
+        self,
+
+        nodes,
+
+    ):
+
+        compiler_ir.clear()
+
+        for node in nodes:
+
+            compiler_ir.add(
+
+                node.get(
+
+                    "block_type",
+
+                    "UNKNOWN",
+
+                ),
+
+                node.get(
+
+                    "id",
+
+                ),
+
+            )
+
+        return compiler_ir.all()
+
+
+compiler_ir_builder = CompilerIRBuilder()
