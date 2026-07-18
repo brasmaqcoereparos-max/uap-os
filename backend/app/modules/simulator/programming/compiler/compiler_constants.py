0b40670892b75853a9bdecabfb214e589446f752
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 18:46:53 -0300

    Update compiler_constants.py

--- backend/app/modules/simulator/programming/compiler/compiler_constants.py
+++ backend/app/modules/simulator/programming/compiler/compiler_constants.py
@@ -1,1 +1,35 @@
+class CompilerConstants:
 
+    def __init__(self):
+
+        self.constants = {}
+
+    def add(
+        self,
+        name,
+        value,
+    ):
+
+        self.constants[name] = value
+
+    def get(
+        self,
+        name,
+    ):
+
+        return self.constants.get(name)
+
+    def exists(self, name):
+
+        return name in self.constants
+
+    def all(self):
+
+        return self.constants.copy()
+
+    def clear(self):
+
+        self.constants.clear()
+
+
+compiler_constants = CompilerConstants()
