a8d5bf8684996e951e983bc91a815a7042339a82
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 06:38:39 -0300

    Update compiler_base.py

--- backend/app/modules/simulator/programming/compiler/compiler_base.py
+++ backend/app/modules/simulator/programming/compiler/compiler_base.py
@@ -1,1 +1,28 @@
+from app.modules.simulator.programming.compiler.compiler_context import (
+    compiler_context,
+)
 
+
+class CompilerBase:
+
+    def compile(self, canvas):
+
+        raise NotImplementedError()
+
+    def context(self):
+
+        return compiler_context
+
+    def reset(self):
+
+        compiler_context.includes.clear()
+
+        compiler_context.definitions.clear()
+
+        compiler_context.globals.clear()
+
+        compiler_context.setup.clear()
+
+        compiler_context.loop.clear()
+
+        compiler_context.variables.clear()
