7847e6b21ecba453de2643e8e8428bae0a648f1e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 19:07:47 -0300

    Update compiler_resolver.py

--- backend/app/modules/simulator/programming/compiler/compiler_resolver.py
+++ backend/app/modules/simulator/programming/compiler/compiler_resolver.py
@@ -1,1 +1,61 @@
+from app.modules.simulator.programming.compiler.compiler_dependency import (
+    compiler_dependency,
+)
 
+
+class CompilerResolver:
+
+    def resolve(self):
+
+        resolved = []
+
+        visited = set()
+
+        for item in compiler_dependency.all():
+
+            self._visit(
+
+                item,
+
+                visited,
+
+                resolved,
+
+            )
+
+        return resolved
+
+    def _visit(
+
+        self,
+
+        item,
+
+        visited,
+
+        resolved,
+
+    ):
+
+        if item in visited:
+
+            return
+
+        visited.add(item)
+
+        for dependency in compiler_dependency.get(item):
+
+            self._visit(
+
+                dependency,
+
+                visited,
+
+                resolved,
+
+            )
+
+        resolved.append(item)
+
+
+compiler_resolver = CompilerResolver()
