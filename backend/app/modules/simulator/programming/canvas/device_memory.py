9dd61b916e00a3c0a02ef28a032fe9cdc4ea48a2
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 05:18:46 -0300

    Update device_memory.py

--- backend/app/modules/simulator/programming/canvas/device_memory.py
+++ backend/app/modules/simulator/programming/canvas/device_memory.py
@@ -1,1 +1,62 @@
+class DeviceMemory:
 
+    def __init__(self):
+
+        self.memory = {}
+
+    def write(
+
+        self,
+
+        device,
+
+        key,
+
+        value,
+
+    ):
+
+        self.memory.setdefault(
+
+            device,
+
+            {},
+
+        )[key] = value
+
+    def read(
+
+        self,
+
+        device,
+
+        key,
+
+        default=None,
+
+    ):
+
+        return self.memory.get(
+
+            device,
+
+            {},
+
+        ).get(
+
+            key,
+
+            default,
+
+        )
+
+    def clear(self):
+
+        self.memory.clear()
+
+    def dump(self):
+
+        return self.memory.copy()
+
+
+device_memory = DeviceMemory()
