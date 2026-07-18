ded1299a03e36768a8396496647fcf8592beee6a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-17 21:42:37 -0300

    Update device_monitor.py

--- backend/app/modules/simulator/programming/canvas/device_monitor.py
+++ backend/app/modules/simulator/programming/canvas/device_monitor.py
@@ -1,1 +1,41 @@
+from app.modules.simulator.programming.canvas.device_manager import (
+    device_manager,
+)
 
+
+class DeviceMonitor:
+
+    def status(self):
+
+        return device_manager.all()
+
+    def connected(self):
+
+        return {
+
+            name: data
+
+            for name, data
+
+            in device_manager.all().items()
+
+            if data["connected"]
+
+        }
+
+    def disconnected(self):
+
+        return {
+
+            name: data
+
+            for name, data
+
+            in device_manager.all().items()
+
+            if not data["connected"]
+
+        }
+
+
+device_monitor = DeviceMonitor()
