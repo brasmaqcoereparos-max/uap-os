ed017c09015cdf46eff73ac323715d1731d26e44
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-18 07:36:20 -0300

    Update compiler_template.py

--- backend/app/modules/simulator/programming/compiler/compiler_template.py
+++ backend/app/modules/simulator/programming/compiler/compiler_template.py
@@ -1,1 +1,26 @@
+class CompilerTemplate:
 
+    def render(
+
+        self,
+
+        text,
+
+        values,
+
+    ):
+
+        for key, value in values.items():
+
+            text = text.replace(
+
+                "{{" + key + "}}",
+
+                str(value),
+
+            )
+
+        return text
+
+
+compiler_template = CompilerTemplate()
