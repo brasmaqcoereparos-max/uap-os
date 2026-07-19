fa9f8e3549f72120051ee0c5ccbc3a75b352f574
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 01:41:30 -0300

    Update backend_initializer.py

--- backend/app/modules/simulator/programming/compiler/backends/backend_initializer.py
+++ backend/app/modules/simulator/programming/compiler/backends/backend_initializer.py
@@ -1,1 +1,27 @@
+from app.modules.simulator.programming.compiler.compiler_backend_manager import (
+    compiler_backend_manager,
+)
 
+from app.modules.simulator.programming.compiler.backends.arduino_backend import (
+    arduino_backend,
+)
+
+
+class BackendInitializer:
+
+    initialized = False
+
+    @classmethod
+    def initialize(cls):
+
+        if cls.initialized:
+
+            return
+
+        compiler_backend_manager.register(
+
+            arduino_backend,
+
+        )
+
+        cls.initialized = True
