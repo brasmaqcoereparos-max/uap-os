8198142501b2468b33ca815bf42ef4b46ea4bd7d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 09:25:29 -0300

    Update runtime_variable.py

--- backend/app/modules/simulator/programming/simulator/runtime/runtime_variable.py
+++ backend/app/modules/simulator/programming/simulator/runtime/runtime_variable.py
@@ -1,1 +1,29 @@
+class RuntimeVariable:
 
+    def __init__(
+
+        self,
+
+        name,
+
+        value=None,
+
+    ):
+
+        self.name = name
+
+        self.value = value
+
+    def set(
+
+        self,
+
+        value,
+
+    ):
+
+        self.value = value
+
+    def get(self):
+
+        return self.value
