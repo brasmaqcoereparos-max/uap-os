40b1bb8be3ec44cb470f610c367df769aab000fe
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 09:01:55 -0300

    Update raspberry_backend.py

--- backend/app/modules/simulator/programming/compiler/backends/raspberry_backend.py
+++ backend/app/modules/simulator/programming/compiler/backends/raspberry_backend.py
@@ -1,1 +1,11 @@
+from app.modules.simulator.programming.compiler.backends.python_backend import (
+    PythonBackend,
+)
 
+
+class RaspberryBackend(PythonBackend):
+
+    name = "raspberry"
+
+
+raspberry_backend = RaspberryBackend()
