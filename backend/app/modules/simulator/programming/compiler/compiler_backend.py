ccd4a69dc87b872a6b2ecb6b84d56bcbfdc5e2f1
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 01:20:32 -0300

    Update compiler_backend.py

--- backend/app/modules/simulator/programming/compiler/compiler_backend.py
+++ backend/app/modules/simulator/programming/compiler/compiler_backend.py
@@ -1,1 +1,14 @@
+from abc import ABC, abstractmethod
 
+
+class CompilerBackend(ABC):
+
+    name = "generic"
+
+    @abstractmethod
+    def generate(
+        self,
+        ir,
+    ):
+
+        pass
