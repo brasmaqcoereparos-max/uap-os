f3afd9a4c6d349d6dbd59c2ed67b1335705ac92f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 01:17:07 -0300

    Update compiler_ir_duplicate_pass.py

--- backend/app/modules/simulator/programming/compiler/compiler_ir_duplicate_pass.py
+++ backend/app/modules/simulator/programming/compiler/compiler_ir_duplicate_pass.py
@@ -1,1 +1,34 @@
+from app.modules.simulator.programming.compiler.compiler_ir_pass import (
+    CompilerIRPass,
+)
 
+
+class DuplicateInstructionPass(CompilerIRPass):
+
+    name = "duplicate"
+
+    def run(
+        self,
+        ir,
+    ):
+
+        result = []
+
+        previous = None
+
+        for instruction in ir:
+
+            if instruction == previous:
+
+                continue
+
+            result.append(
+                instruction
+            )
+
+            previous = instruction
+
+        return result
+
+
+duplicate_instruction_pass = DuplicateInstructionPass()
