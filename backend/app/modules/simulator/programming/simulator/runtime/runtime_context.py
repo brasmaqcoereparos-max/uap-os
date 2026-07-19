6be29e7ae0a22e10672e7652b5cdc89a32c1e794
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 09:23:26 -0300

    Update runtime_context.py

--- backend/app/modules/simulator/programming/simulator/runtime/runtime_context.py
+++ backend/app/modules/simulator/programming/simulator/runtime/runtime_context.py
@@ -1,1 +1,24 @@
+class RuntimeContext:
 
+    def __init__(self):
+
+        self.variables = {}
+
+        self.digital = {}
+
+        self.analog = {}
+
+        self.memory = {}
+
+    def reset(self):
+
+        self.variables.clear()
+
+        self.digital.clear()
+
+        self.analog.clear()
+
+        self.memory.clear()
+
+
+runtime_context = RuntimeContext()
