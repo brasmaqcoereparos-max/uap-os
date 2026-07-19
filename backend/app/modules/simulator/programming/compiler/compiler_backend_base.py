04a39fe67f00daf0b17c06ad93bb6eafaf120aaf
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 01:25:31 -0300

    Update compiler_backend_base.py

--- backend/app/modules/simulator/programming/compiler/compiler_backend_base.py
+++ backend/app/modules/simulator/programming/compiler/compiler_backend_base.py
@@ -1,1 +1,28 @@
+from app.modules.simulator.programming.compiler.compiler_backend import (
+    CompilerBackend,
+)
 
+
+class CompilerBackendBase(CompilerBackend):
+
+    def line(
+        self,
+        text,
+        output,
+    ):
+
+        output.append(text)
+
+    def blank(
+        self,
+        output,
+    ):
+
+        output.append("")
+
+    def build(
+        self,
+        output,
+    ):
+
+        return "\n".join(output)
