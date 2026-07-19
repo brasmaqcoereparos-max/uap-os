d83dcfd80d2e6c0cb77edb869b14c9719d6886bd
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 01:47:25 -0300

    Update compiler_codegen_service.py

--- backend/app/modules/simulator/programming/compiler/compiler_codegen_service.py
+++ backend/app/modules/simulator/programming/compiler/compiler_codegen_service.py
@@ -1,1 +1,30 @@
+from app.modules.simulator.programming.compiler.compiler_ir import (
+    compiler_ir,
+)
+from app.modules.simulator.programming.compiler.compiler_backend_service import (
+    compiler_backend_service,
+)
 
+import app.modules.simulator.programming.compiler.backends
+
+
+class CompilerCodegenService:
+
+    def generate(
+
+        self,
+
+        target,
+
+    ):
+
+        return compiler_backend_service.generate(
+
+            target,
+
+            compiler_ir.all(),
+
+        )
+
+
+compiler_codegen_service = CompilerCodegenService()
