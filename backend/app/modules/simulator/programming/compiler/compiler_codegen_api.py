b006a140805ad19284bb555afc8fd923612c1b3d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 01:49:46 -0300

    Update compiler_codegen_api.py

--- backend/app/modules/simulator/programming/compiler/compiler_codegen_api.py
+++ backend/app/modules/simulator/programming/compiler/compiler_codegen_api.py
@@ -1,1 +1,33 @@
+from fastapi import APIRouter
 
+from app.modules.simulator.programming.compiler.compiler_codegen_service import (
+    compiler_codegen_service,
+)
+
+router = APIRouter(
+
+    prefix="/codegen",
+
+    tags=["Code Generator"],
+
+)
+
+
+@router.get("/{target}")
+def generate(
+
+    target: str,
+
+):
+
+    return {
+
+        "target": target,
+
+        "source": compiler_codegen_service.generate(
+
+            target,
+
+        ),
+
+    }
