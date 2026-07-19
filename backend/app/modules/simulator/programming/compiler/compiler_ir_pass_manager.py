06fe7d4ae09e560ec355f08fa4b41ebd1484b9fa
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 01:13:51 -0300

    Update compiler_ir_pass_manager.py

--- backend/app/modules/simulator/programming/compiler/compiler_ir_pass_manager.py
+++ backend/app/modules/simulator/programming/compiler/compiler_ir_pass_manager.py
@@ -1,1 +1,36 @@
+class CompilerIRPassManager:
 
+    def __init__(self):
+
+        self.passes = []
+
+    def register(
+        self,
+        compiler_pass,
+    ):
+
+        self.passes.append(
+            compiler_pass
+        )
+
+    def execute(
+        self,
+        ir,
+    ):
+
+        for compiler_pass in self.passes:
+
+            ir = compiler_pass.run(ir)
+
+        return ir
+
+    def clear(self):
+
+        self.passes.clear()
+
+    def all(self):
+
+        return self.passes.copy()
+
+
+compiler_ir_pass_manager = CompilerIRPassManager()
