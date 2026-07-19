eb35a4ede0d7888b9a27c75f1a1fdec473a17609
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 01:33:06 -0300

    Update compiler_backend_selector.py

--- backend/app/modules/simulator/programming/compiler/compiler_backend_selector.py
+++ backend/app/modules/simulator/programming/compiler/compiler_backend_selector.py
@@ -1,1 +1,23 @@
+from app.modules.simulator.programming.compiler.compiler_backend_factory import (
+    compiler_backend_factory,
+)
 
+
+class CompilerBackendSelector:
+
+    def select(
+
+        self,
+
+        target,
+
+    ):
+
+        return compiler_backend_factory.create(
+
+            target,
+
+        )
+
+
+compiler_backend_selector = CompilerBackendSelector()
