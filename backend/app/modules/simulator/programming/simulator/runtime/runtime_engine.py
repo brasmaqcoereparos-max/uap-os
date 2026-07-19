170d0f59a1238764fa517ce4fbc7963d7ba79045
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 09:33:31 -0300

    Update runtime_engine.py

--- backend/app/modules/simulator/programming/simulator/runtime/runtime_engine.py
+++ backend/app/modules/simulator/programming/simulator/runtime/runtime_engine.py
@@ -1,1 +1,31 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_context import (
+    runtime_context,
+)
 
+
+class RuntimeEngine:
+
+    def initialize(self):
+
+        runtime_context.reset()
+
+    def execute(
+
+        self,
+
+        instructions,
+
+    ):
+
+        for instruction in instructions:
+
+            print(
+
+                instruction,
+
+            )
+
+        return True
+
+
+runtime_engine = RuntimeEngine()
