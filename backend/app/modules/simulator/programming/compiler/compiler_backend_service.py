758ded72d2ef04eca84db4c07b58f1b48e60917f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 01:34:48 -0300

    Update compiler_backend_service.py

--- backend/app/modules/simulator/programming/compiler/compiler_backend_service.py
+++ backend/app/modules/simulator/programming/compiler/compiler_backend_service.py
@@ -1,1 +1,27 @@
+from app.modules.simulator.programming.compiler.compiler_backend_selector import (
+    compiler_backend_selector,
+)
 
+
+class CompilerBackendService:
+
+    def generate(
+
+        self,
+
+        target,
+
+        ir,
+
+    ):
+
+        backend = compiler_backend_selector.select(
+
+            target,
+
+        )
+
+        return backend.generate(ir)
+
+
+compiler_backend_service = CompilerBackendService()
