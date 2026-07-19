e8d6fd406fa678fffebde1c05b6bb59d506af1da
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 08:07:30 -0300

    Update stm32_backend.py

--- backend/app/modules/simulator/programming/compiler/backends/stm32_backend.py
+++ backend/app/modules/simulator/programming/compiler/backends/stm32_backend.py
@@ -1,1 +1,18 @@
+from app.modules.simulator.programming.compiler.backends.arduino_backend import (
+    ArduinoBackend,
+)
 
+
+class STM32Backend(ArduinoBackend):
+
+    name = "stm32"
+
+    def generate(
+        self,
+        ir,
+    ):
+
+        return super().generate(ir)
+
+
+stm32_backend = STM32Backend()
