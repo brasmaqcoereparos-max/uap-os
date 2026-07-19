f9820ec920e2461b7c41035472925339c297f261
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 09:09:08 -0300

    Update backend_initializer.py

--- backend/app/modules/simulator/programming/compiler/backends/backend_initializer.py
+++ backend/app/modules/simulator/programming/compiler/backends/backend_initializer.py
@@ -22,6 +22,22 @@ from app.modules.simulator.programming.compiler.backends.python_backend import (
     python_backend,
 )
 
+from app.modules.simulator.programming.compiler.backends.micropython_backend import (
+    micropython_backend,
+)
+
+from app.modules.simulator.programming.compiler.backends.raspberry_backend import (
+    raspberry_backend,
+)
+
+from app.modules.simulator.programming.compiler.backends.json_backend import (
+    json_backend,
+)
+
+from app.modules.simulator.programming.compiler.backends.text_backend import (
+    text_backend,
+)
+
 
 class BackendInitializer:
 
@@ -34,24 +50,14 @@ class BackendInitializer:
 
             return
 
-        compiler_backend_manager.register(
-            arduino_backend,
-        )
-
-        compiler_backend_manager.register(
-            esp32_backend,
-        )
-
-        compiler_backend_manager.register(
-            rp2040_backend,
-        )
-
-        compiler_backend_manager.register(
-            stm32_backend,
-        )
-
-        compiler_backend_manager.register(
-            python_backend,
-        )
+        compiler_backend_manager.register(arduino_backend)
+        compiler_backend_manager.register(esp32_backend)
+        compiler_backend_manager.register(rp2040_backend)
+        compiler_backend_manager.register(stm32_backend)
+        compiler_backend_manager.register(python_backend)
+        compiler_backend_manager.register(micropython_backend)
+        compiler_backend_manager.register(raspberry_backend)
+        compiler_backend_manager.register(json_backend)
+        compiler_backend_manager.register(text_backend)
 
         cls.initialized = True
