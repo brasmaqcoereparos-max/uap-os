dd984d1761f9fff2e75e0c57a168c08b83421bb3
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 01:28:19 -0300

    Update compiler_backend_context.py

--- backend/app/modules/simulator/programming/compiler/compiler_backend_context.py
+++ backend/app/modules/simulator/programming/compiler/compiler_backend_context.py
@@ -1,1 +1,40 @@
+class CompilerBackendContext:
 
+    def __init__(self):
+
+        self.reset()
+
+    def reset(self):
+
+        self.headers = []
+
+        self.globals = []
+
+        self.setup = []
+
+        self.loop = []
+
+        self.functions = []
+
+    def add_header(self, line):
+
+        self.headers.append(line)
+
+    def add_global(self, line):
+
+        self.globals.append(line)
+
+    def add_setup(self, line):
+
+        self.setup.append(line)
+
+    def add_loop(self, line):
+
+        self.loop.append(line)
+
+    def add_function(self, line):
+
+        self.functions.append(line)
+
+
+compiler_backend_context = CompilerBackendContext()
