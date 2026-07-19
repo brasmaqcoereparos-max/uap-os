1f14c971b445210f0af379c1daf710ed1aa52b2a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 09:28:04 -0300

    Update runtime_memory.py

--- backend/app/modules/simulator/programming/simulator/runtime/runtime_memory.py
+++ backend/app/modules/simulator/programming/simulator/runtime/runtime_memory.py
@@ -1,1 +1,42 @@
+class RuntimeMemory:
 
+    def __init__(self):
+
+        self.data = {}
+
+    def write(
+
+        self,
+
+        address,
+
+        value,
+
+    ):
+
+        self.data[address] = value
+
+    def read(
+
+        self,
+
+        address,
+
+        default=None,
+
+    ):
+
+        return self.data.get(
+
+            address,
+
+            default,
+
+        )
+
+    def clear(self):
+
+        self.data.clear()
+
+
+runtime_memory = RuntimeMemory()
