37b98b52c7af512d83b2dd80911b80a787dea7e6
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 08:06:00 -0300

    Update rp2040_backend.py

--- backend/app/modules/simulator/programming/compiler/backends/rp2040_backend.py
+++ backend/app/modules/simulator/programming/compiler/backends/rp2040_backend.py
@@ -1,1 +1,18 @@
+from app.modules.simulator.programming.compiler.backends.arduino_backend import (
+    ArduinoBackend,
+)
 
+
+class RP2040Backend(ArduinoBackend):
+
+    name = "rp2040"
+
+    def generate(
+        self,
+        ir,
+    ):
+
+        return super().generate(ir)
+
+
+rp2040_backend = RP2040Backend()
