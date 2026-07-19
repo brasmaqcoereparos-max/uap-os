3202551a2e12e187baad1a27e23773a275288ece
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 09:30:18 -0300

    Update runtime_clock.py

--- backend/app/modules/simulator/programming/simulator/runtime/runtime_clock.py
+++ backend/app/modules/simulator/programming/simulator/runtime/runtime_clock.py
@@ -1,1 +1,19 @@
+import time
 
+
+class RuntimeClock:
+
+    def now(self):
+
+        return time.time()
+
+    def millis(self):
+
+        return int(
+
+            time.time() * 1000
+
+        )
+
+
+runtime_clock = RuntimeClock()
