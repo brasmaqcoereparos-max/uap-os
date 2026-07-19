7a4ff114419274d76eaf220060d3ada4dc7984cc
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 09:03:50 -0300

    Update json_backend.py

--- backend/app/modules/simulator/programming/compiler/backends/json_backend.py
+++ backend/app/modules/simulator/programming/compiler/backends/json_backend.py
@@ -1,1 +1,26 @@
+import json
 
+from app.modules.simulator.programming.compiler.compiler_backend_base import (
+    CompilerBackendBase,
+)
+
+
+class JsonBackend(CompilerBackendBase):
+
+    name = "json"
+
+    def generate(
+        self,
+        ir,
+    ):
+
+        return json.dumps(
+
+            ir,
+
+            indent=4,
+
+        )
+
+
+json_backend = JsonBackend()
