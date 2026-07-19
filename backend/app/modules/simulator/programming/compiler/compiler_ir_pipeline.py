78ac3e8ea5d4e93d90cc242de3a470b20a1009f4
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-19 01:18:36 -0300

    Update compiler_ir_pipeline.py

--- backend/app/modules/simulator/programming/compiler/compiler_ir_pipeline.py
+++ backend/app/modules/simulator/programming/compiler/compiler_ir_pipeline.py
@@ -1,1 +1,34 @@
+from app.modules.simulator.programming.compiler.compiler_ir_pass_manager import (
+    compiler_ir_pass_manager,
+)
+from app.modules.simulator.programming.compiler.compiler_ir_dead_code_pass import (
+    dead_code_pass,
+)
+from app.modules.simulator.programming.compiler.compiler_ir_duplicate_pass import (
+    duplicate_instruction_pass,
+)
 
+
+class CompilerIRPipeline:
+
+    def __init__(self):
+
+        compiler_ir_pass_manager.register(
+            dead_code_pass
+        )
+
+        compiler_ir_pass_manager.register(
+            duplicate_instruction_pass
+        )
+
+    def optimize(
+        self,
+        ir,
+    ):
+
+        return compiler_ir_pass_manager.execute(
+            ir
+        )
+
+
+compiler_ir_pipeline = CompilerIRPipeline()
