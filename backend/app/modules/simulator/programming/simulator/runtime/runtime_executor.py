8f6a3e1e10aa7a543a4061f165ba65638f82c7bb
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-20 10:55:25 -0300

    Update runtime_executor.py

--- backend/app/modules/simulator/programming/simulator/runtime/runtime_executor.py
+++ backend/app/modules/simulator/programming/simulator/runtime/runtime_executor.py
@@ -1,1 +1,35 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_instruction_set import (
+    runtime_instruction_set,
+)
 
+
+class RuntimeExecutor:
+
+    def execute(
+
+        self,
+
+        instruction,
+
+    ):
+
+        callback = runtime_instruction_set.get(
+
+            instruction.opcode,
+
+        )
+
+        if callback is None:
+
+            return False
+
+        callback(
+
+            *instruction.operands,
+
+        )
+
+        return True
+
+
+runtime_executor = RuntimeExecutor()
