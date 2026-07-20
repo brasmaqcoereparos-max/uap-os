41eb64951cc9e38f990d1fa417a77312551dddef
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-20 10:59:42 -0300

    Update instruction_mov.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_mov.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_mov.py
@@ -1,1 +1,20 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_registers import (
+    runtime_registers,
+)
 
+
+def instruction_mov(
+
+    register,
+
+    value,
+
+):
+
+    runtime_registers.set(
+
+        register,
+
+        value,
+
+    )
