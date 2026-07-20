2ce5eee9852b1f7488e8b859ca1eb74a4c831b61
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-20 10:48:11 -0300

    Update runtime_instruction.py

--- backend/app/modules/simulator/programming/simulator/runtime/runtime_instruction.py
+++ backend/app/modules/simulator/programming/simulator/runtime/runtime_instruction.py
@@ -1,1 +1,26 @@
 
+class RuntimeInstruction:
+
+    def __init__(
+
+        self,
+
+        opcode,
+
+        operands=None,
+
+    ):
+
+        self.opcode = opcode
+
+        self.operands = operands or []
+
+    def to_dict(self):
+
+        return {
+
+            "opcode": self.opcode,
+
+            "operands": self.operands,
+
+        }
