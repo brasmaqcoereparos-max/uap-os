85a79324518aee45e7dfb31772890e35e65ec373
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-20 11:14:51 -0300

    Update instruction_set.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_set.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_set.py
@@ -1,1 +1,18 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_context import (
+    runtime_context,
+)
 
+
+def instruction_set(
+
+    variable,
+
+    value,
+
+):
+
+    runtime_context.variables[
+
+        variable
+
+    ] = value
