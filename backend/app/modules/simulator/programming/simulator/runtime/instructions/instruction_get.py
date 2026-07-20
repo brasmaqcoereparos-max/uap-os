5c6d8b1d70083ab13b582ff0020fae07e4c01e7d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-20 11:18:47 -0300

    Update instruction_get.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_get.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_get.py
@@ -1,1 +1,16 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_context import (
+    runtime_context,
+)
 
+
+def instruction_get(
+
+    variable,
+
+):
+
+    return runtime_context.variables.get(
+
+        variable,
+
+    )
