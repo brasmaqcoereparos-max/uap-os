7fe98d40096a35f84ec32ca417e074bc6c0c800d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 08:27:37 -0300

    Update instruction_pin_mode.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_pin_mode.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_pin_mode.py
@@ -1,1 +1,15 @@
 
+from app.modules.simulator.programming.simulator.runtime.runtime_gpio import (
+    runtime_gpio,
+)
+
+
+def instruction_pin_mode(
+    pin,
+    mode,
+):
+
+    runtime_gpio.mode(
+        pin,
+        mode,
+    )
