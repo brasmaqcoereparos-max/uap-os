fde03a62d02d6016676e80a8cd4c97c728aa4eed
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 08:14:56 -0300

    Update instruction_eq.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_eq.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_eq.py
@@ -1,1 +1,23 @@
 
+from app.modules.simulator.programming.simulator.runtime.runtime_registers import (
+    runtime_registers,
+)
+
+
+def instruction_eq(
+
+    register,
+
+    value,
+
+):
+
+    current = runtime_registers.get(
+
+        register,
+
+        None,
+
+    )
+
+    return current == value
