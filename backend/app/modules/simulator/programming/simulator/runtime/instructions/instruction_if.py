164297886dbb6126316d2fc269308e0552b480a5
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 07:49:04 -0300

    Update instruction_if.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_if.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_if.py
@@ -1,1 +1,20 @@
+from app.modules.simulator.programming.simulator.runtime.instructions.instruction_goto import (
+    RuntimeGoto,
+)
 
+
+def instruction_if(
+
+    condition,
+
+    label,
+
+):
+
+    if condition:
+
+        raise RuntimeGoto(
+
+            label,
+
+        )
