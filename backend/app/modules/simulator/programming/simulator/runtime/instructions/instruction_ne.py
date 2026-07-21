d4c729cbb75154fc76ea82f63b194e543fa751cd
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 08:16:38 -0300

    Update instruction_ne.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_ne.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_ne.py
@@ -1,1 +1,22 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_registers import (
+    runtime_registers,
+)
 
+
+def instruction_ne(
+
+    register,
+
+    value,
+
+):
+
+    current = runtime_registers.get(
+
+        register,
+
+        None,
+
+    )
+
+    return current != value
