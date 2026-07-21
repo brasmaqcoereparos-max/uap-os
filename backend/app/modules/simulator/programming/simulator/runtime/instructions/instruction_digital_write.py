359190d9b10ea4279f5f19df1d424316701c765f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 08:30:03 -0300

    Update instruction_digital_write.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_digital_write.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_digital_write.py
@@ -1,1 +1,14 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_gpio import (
+    runtime_gpio,
+)
 
+
+def instruction_digital_write(
+    pin,
+    value,
+):
+
+    runtime_gpio.write(
+        pin,
+        value,
+    )
