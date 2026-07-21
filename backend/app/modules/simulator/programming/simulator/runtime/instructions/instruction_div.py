5a984c48122eaa514f9f72ad9ab2489af3911648
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 08:13:01 -0300

    Update instruction_div.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_div.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_div.py
@@ -1,1 +1,33 @@
 
+from app.modules.simulator.programming.simulator.runtime.runtime_registers import (
+    runtime_registers,
+)
+
+
+def instruction_div(
+
+    register,
+
+    value,
+
+):
+
+    if value == 0:
+
+        return
+
+    current = runtime_registers.get(
+
+        register,
+
+        0,
+
+    )
+
+    runtime_registers.set(
+
+        register,
+
+        current / value,
+
+    )
