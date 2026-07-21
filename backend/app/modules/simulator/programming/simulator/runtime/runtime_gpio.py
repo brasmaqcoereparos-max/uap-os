ab824c8e7ef45769d771139194110e2692db5e0e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 08:24:16 -0300

    Update runtime_gpio.py

--- backend/app/modules/simulator/programming/simulator/runtime/runtime_gpio.py
+++ backend/app/modules/simulator/programming/simulator/runtime/runtime_gpio.py
@@ -1,1 +1,39 @@
+class RuntimeGPIO:
 
+    def __init__(self):
+
+        self.digital = {}
+
+        self.pin_mode = {}
+
+    def mode(
+        self,
+        pin,
+        mode,
+    ):
+
+        self.pin_mode[pin] = mode
+
+    def write(
+        self,
+        pin,
+        value,
+    ):
+
+        self.digital[pin] = bool(value)
+
+    def read(
+        self,
+        pin,
+    ):
+
+        return self.digital.get(pin, False)
+
+    def reset(self):
+
+        self.digital.clear()
+
+        self.pin_mode.clear()
+
+
+runtime_gpio = RuntimeGPIO()
