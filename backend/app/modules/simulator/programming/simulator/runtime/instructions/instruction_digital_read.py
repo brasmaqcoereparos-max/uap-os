c7290bca5fd1d9b6f3987f390fcc438ae6f5e78b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 08:34:11 -0300

    Update instruction_digital_read.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_digital_read.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_digital_read.py
@@ -1,1 +1,12 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_gpio import (
+    runtime_gpio,
+)
 
+
+def instruction_digital_read(
+    pin,
+):
+
+    return runtime_gpio.read(
+        pin,
+    )
