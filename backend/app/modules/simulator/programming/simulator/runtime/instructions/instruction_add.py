979304c3e2facc6ad3c84ee488fe63a6bf938879
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 08:02:01 -0300

    Update instruction_add.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_add.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_add.py
@@ -1,1 +1,28 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_registers import (
+    runtime_registers,
+)
 
+
+def instruction_add(
+
+    register,
+
+    value,
+
+):
+
+    current = runtime_registers.get(
+
+        register,
+
+        0,
+
+    )
+
+    runtime_registers.set(
+
+        register,
+
+        current + value,
+
+    )
