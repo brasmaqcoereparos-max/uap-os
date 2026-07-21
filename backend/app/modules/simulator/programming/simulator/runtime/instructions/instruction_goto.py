bf33432b81965d69143055b3342d6a4269ee040a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 07:47:06 -0300

    Update instruction_goto.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_goto.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_goto.py
@@ -1,1 +1,24 @@
+class RuntimeGoto(Exception):
 
+    def __init__(
+
+        self,
+
+        label,
+
+    ):
+
+        self.label = label
+
+
+def instruction_goto(
+
+    label,
+
+):
+
+    raise RuntimeGoto(
+
+        label,
+
+    )
