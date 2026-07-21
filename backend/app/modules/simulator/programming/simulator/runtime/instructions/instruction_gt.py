9cb0195e5314cf57ab6d9471b9e54fbec732dd66
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 08:19:50 -0300

    Update instruction_gt.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_gt.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_gt.py
@@ -1,1 +1,22 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_registers import (
+    runtime_registers,
+)
 
+
+def instruction_gt(
+
+    register,
+
+    value,
+
+):
+
+    current = runtime_registers.get(
+
+        register,
+
+        0,
+
+    )
+
+    return current > value
