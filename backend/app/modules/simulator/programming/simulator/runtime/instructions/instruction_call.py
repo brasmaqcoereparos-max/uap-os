baa57d779881d16e067a8e90376dc24f0128a5bf
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-21 07:50:41 -0300

    Update instruction_call.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_call.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_call.py
@@ -1,1 +1,28 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_stack import (
+    runtime_stack,
+)
 
+from app.modules.simulator.programming.simulator.runtime.instructions.instruction_goto import (
+    RuntimeGoto,
+)
+
+
+def instruction_call(
+
+    current,
+
+    label,
+
+):
+
+    runtime_stack.push(
+
+        current,
+
+    )
+
+    raise RuntimeGoto(
+
+        label,
+
+    )
