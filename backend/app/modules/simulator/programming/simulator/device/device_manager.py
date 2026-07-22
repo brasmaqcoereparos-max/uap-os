b3ebd47f2b7471d1786dfcbc5f6b5c082a56efb9
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 06:35:22 -0300

    Update device_manager.py

--- backend/app/modules/simulator/programming/simulator/device/device_manager.py
+++ backend/app/modules/simulator/programming/simulator/device/device_manager.py
@@ -1,1 +1,37 @@
+from app.modules.simulator.programming.simulator.devices.device_registry import (
+    device_registry,
+)
 
+
+class DeviceManager:
+
+    def add(
+
+        self,
+
+        device,
+
+    ):
+
+        device_registry.register(
+
+            device.name,
+
+            device,
+
+        )
+
+    def update_all(self):
+
+        for device in device_registry.all().values():
+
+            device.update()
+
+    def reset_all(self):
+
+        for device in device_registry.all().values():
+
+            device.reset()
+
+
+device_manager = DeviceManager()
