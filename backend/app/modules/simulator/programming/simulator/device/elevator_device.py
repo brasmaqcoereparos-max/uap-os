e19cc2bb460594d40525e25feda1b96769eab016
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 08:47:12 -0300

    Update elevator_device.py

--- backend/app/modules/simulator/programming/simulator/device/elevator_device.py
+++ backend/app/modules/simulator/programming/simulator/device/elevator_device.py
@@ -1,1 +1,29 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class ElevatorDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+
+        self.level = 0
+
+    def move_to(
+        self,
+        level,
+    ):
+
+        self.level = int(level)
+
+    def update(self):
+
+        pass
+
+    def reset(self):
+
+        self.level = 0
