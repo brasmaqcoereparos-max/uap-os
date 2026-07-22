9f3a364c9aaa2de6a7a79fb4f71b645bbebc2673
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 07:01:27 -0300

    Update seven_segment_device.py

--- backend/app/modules/simulator/programming/simulator/device/seven_segment_device.py
+++ backend/app/modules/simulator/programming/simulator/device/seven_segment_device.py
@@ -1,1 +1,36 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class SevenSegmentDevice(DeviceBase):
+
+    def __init__(
+
+        self,
+
+        name,
+
+    ):
+
+        super().__init__(name)
+
+        self.value = 0
+
+    def display(
+
+        self,
+
+        value,
+
+    ):
+
+        self.value = int(value)
+
+    def update(self):
+
+        pass
+
+    def reset(self):
+
+        self.value = 0
