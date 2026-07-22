70e7379503b2fa57d2816fb2c0d0eb1121b421aa
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 08:21:53 -0300

    Update ultrasonic_sensor_device.py

--- backend/app/modules/simulator/programming/simulator/device/ultrasonic_sensor_device.py
+++ backend/app/modules/simulator/programming/simulator/device/ultrasonic_sensor_device.py
@@ -1,1 +1,28 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class UltrasonicSensorDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+        self.distance = 100.0
+
+    def set_distance(
+        self,
+        distance,
+    ):
+        self.distance = float(distance)
+
+    def read(self):
+        return self.distance
+
+    def update(self):
+        pass
+
+    def reset(self):
+        self.distance = 100.0
