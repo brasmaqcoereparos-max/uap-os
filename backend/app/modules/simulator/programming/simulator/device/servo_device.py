f45d3843ef16cba0d1d45e9fcfbb89236e1e5863
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 06:52:00 -0300

    Update servo_device.py

--- backend/app/modules/simulator/programming/simulator/device/servo_device.py
+++ backend/app/modules/simulator/programming/simulator/device/servo_device.py
@@ -1,1 +1,44 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+from app.modules.simulator.programming.simulator.runtime.runtime_pwm import (
+    runtime_pwm,
+)
+
+
+class ServoDevice(DeviceBase):
+
+    def __init__(
+
+        self,
+
+        name,
+
+        pin,
+
+    ):
+
+        super().__init__(name)
+
+        self.pin = pin
+
+        self.angle = 0
+
+    def update(self):
+
+        pwm = runtime_pwm.read(
+
+            self.pin,
+
+        )
+
+        self.angle = int(
+
+            pwm * 180 / 255,
+
+        )
+
+    def reset(self):
+
+        self.angle = 0
