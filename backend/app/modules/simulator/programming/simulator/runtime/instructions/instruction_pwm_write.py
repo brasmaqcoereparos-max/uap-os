5c15172a5faf99a151054e351461ede96f05f26e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 06:21:04 -0300

    Update instruction_pwm_write.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_pwm_write.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_pwm_write.py
@@ -1,1 +1,20 @@
+from app.modules.simulator.programming.simulator.runtime.runtime_pwm import (
+    runtime_pwm,
+)
 
+
+def instruction_pwm_write(
+
+    pin,
+
+    value,
+
+):
+
+    runtime_pwm.write(
+
+        pin,
+
+        value,
+
+    )
