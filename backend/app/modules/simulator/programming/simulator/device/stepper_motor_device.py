d60ddc8fa47085eafa500f51d7e1c04d25fdf59c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 08:42:58 -0300

    Update stepper_motor_device.py

--- backend/app/modules/simulator/programming/simulator/device/stepper_motor_device.py
+++ backend/app/modules/simulator/programming/simulator/device/stepper_motor_device.py
@@ -1,1 +1,29 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class StepperMotorDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+
+        self.position = 0
+
+    def move(
+        self,
+        steps,
+    ):
+
+        self.position += int(steps)
+
+    def update(self):
+
+        pass
+
+    def reset(self):
+
+        self.position = 0
