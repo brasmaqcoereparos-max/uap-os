2b64d4f9496efcccc00ee0fa46a99121e1a3bfe8
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 08:28:08 -0300

    Update light_sensor_device.py

--- backend/app/modules/simulator/programming/simulator/device/light_sensor_device.py
+++ backend/app/modules/simulator/programming/simulator/device/light_sensor_device.py
@@ -1,1 +1,28 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class LightSensorDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+        self.lux = 500
+
+    def set_lux(
+        self,
+        value,
+    ):
+        self.lux = int(value)
+
+    def read(self):
+        return self.lux
+
+    def update(self):
+        pass
+
+    def reset(self):
+        self.lux = 500
