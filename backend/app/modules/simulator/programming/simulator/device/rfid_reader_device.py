2e890257a9976e836c9e06cc80cdbff8a41f91e0
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 08:51:36 -0300

    Update rfid_reader_device.py

--- backend/app/modules/simulator/programming/simulator/device/rfid_reader_device.py
+++ backend/app/modules/simulator/programming/simulator/device/rfid_reader_device.py
@@ -1,1 +1,37 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class RFIDReaderDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+
+        self.tag = None
+
+    def set_tag(
+        self,
+        tag,
+    ):
+
+        self.tag = tag
+
+    def read(self):
+
+        return self.tag
+
+    def clear(self):
+
+        self.tag = None
+
+    def update(self):
+
+        pass
+
+    def reset(self):
+
+        self.clear()
