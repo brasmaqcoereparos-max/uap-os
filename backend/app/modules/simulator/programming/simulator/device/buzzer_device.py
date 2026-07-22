ae237bd7c696d5f2e2d8149e1dc52c0d696c2213
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 06:50:09 -0300

    Update buzzer_device.py

--- backend/app/modules/simulator/programming/simulator/device/buzzer_device.py
+++ backend/app/modules/simulator/programming/simulator/device/buzzer_device.py
@@ -1,1 +1,38 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+from app.modules.simulator.programming.simulator.runtime.runtime_pwm import (
+    runtime_pwm,
+)
+
+
+class BuzzerDevice(DeviceBase):
+
+    def __init__(
+
+        self,
+
+        name,
+
+        pin,
+
+    ):
+
+        super().__init__(name)
+
+        self.pin = pin
+
+        self.level = 0
+
+    def update(self):
+
+        self.level = runtime_pwm.read(
+
+            self.pin,
+
+        )
+
+    def reset(self):
+
+        self.level = 0
