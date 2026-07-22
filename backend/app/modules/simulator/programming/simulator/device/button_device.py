41792b2ffb00b12e4fecaa2d547ae4426477d9dd
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 06:41:43 -0300

    Update button_device.py

--- backend/app/modules/simulator/programming/simulator/device/button_device.py
+++ backend/app/modules/simulator/programming/simulator/device/button_device.py
@@ -1,1 +1,52 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+from app.modules.simulator.programming.simulator.runtime.runtime_gpio import (
+    runtime_gpio,
+)
+
+
+class ButtonDevice(DeviceBase):
+
+    def __init__(
+
+        self,
+
+        name,
+
+        pin,
+
+    ):
+
+        super().__init__(name)
+
+        self.pin = pin
+
+    def press(self):
+
+        runtime_gpio.write(
+
+            self.pin,
+
+            True,
+
+        )
+
+    def release(self):
+
+        runtime_gpio.write(
+
+            self.pin,
+
+            False,
+
+        )
+
+    def update(self):
+
+        pass
+
+    def reset(self):
+
+        self.release()
