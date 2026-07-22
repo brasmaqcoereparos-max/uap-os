ca5afd7765710d67a6ca32f437108a787d2b39a7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 08:59:51 -0300

    Update load_cell_device.py

--- backend/app/modules/simulator/programming/simulator/device/load_cell_device.py
+++ backend/app/modules/simulator/programming/simulator/device/load_cell_device.py
@@ -1,1 +1,33 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class LoadCellDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+
+        self.weight = 0.0
+
+    def set_weight(
+        self,
+        weight,
+    ):
+
+        self.weight = float(weight)
+
+    def read(self):
+
+        return self.weight
+
+    def update(self):
+
+        pass
+
+    def reset(self):
+
+        self.weight = 0.0
