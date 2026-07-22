8874d8a943dce5398d7c6d9b7df57e6b337b993a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 08:54:05 -0300

    Update barcode_scanner_device.py

--- backend/app/modules/simulator/programming/simulator/device/barcode_scanner_device.py
+++ backend/app/modules/simulator/programming/simulator/device/barcode_scanner_device.py
@@ -1,1 +1,37 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class BarcodeScannerDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+
+        self.code = None
+
+    def scan(
+        self,
+        code,
+    ):
+
+        self.code = code
+
+    def read(self):
+
+        return self.code
+
+    def clear(self):
+
+        self.code = None
+
+    def update(self):
+
+        pass
+
+    def reset(self):
+
+        self.clear()
