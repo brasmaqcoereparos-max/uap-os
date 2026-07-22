9ad63226c5116d463faad73a94fb3660566b30f3
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 08:40:39 -0300

    Update dc_motor_device.py

--- backend/app/modules/simulator/programming/simulator/device/dc_motor_device.py
+++ backend/app/modules/simulator/programming/simulator/device/dc_motor_device.py
@@ -1,1 +1,35 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+from app.modules.simulator.programming.simulator.runtime.runtime_pwm import (
+    runtime_pwm,
+)
+
+
+class DCMotorDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+        pin,
+    ):
+        super().__init__(name)
+
+        self.pin = pin
+
+        self.speed = 0
+
+    def update(self):
+
+        self.speed = runtime_pwm.read(
+            self.pin,
+        )
+
+    def stop(self):
+
+        self.speed = 0
+
+    def reset(self):
+
+        self.stop()
