9370a7b37db324d22f039146836e972c34651215
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 06:32:06 -0300

    Update device_base.py

--- backend/app/modules/simulator/programming/simulator/device/device_base.py
+++ backend/app/modules/simulator/programming/simulator/device/device_base.py
@@ -1,1 +1,21 @@
+class DeviceBase:
 
+    def __init__(
+
+        self,
+
+        name,
+
+    ):
+
+        self.name = name
+
+        self.enabled = True
+
+    def update(self):
+
+        pass
+
+    def reset(self):
+
+        pass
