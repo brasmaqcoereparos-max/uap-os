99e7b95ca5f16996764e0aca1c0e9f22042dc76d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 06:27:01 -0300

    Update instruction_analog_read.py

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_analog_read.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_analog_read.py
@@ -1,1 +1,17 @@
 
+from app.modules.simulator.programming.simulator.runtime.runtime_adc import (
+    runtime_adc,
+)
+
+
+def instruction_analog_read(
+
+    pin,
+
+):
+
+    return runtime_adc.read(
+
+        pin,
+
+    )
