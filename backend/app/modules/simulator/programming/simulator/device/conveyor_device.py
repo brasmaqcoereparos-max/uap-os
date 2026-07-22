18ef0a5802ea6bfbd32b9c17dd8157d63268d645
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 08:45:03 -0300

    Update conveyor_device.py

--- backend/app/modules/simulator/programming/simulator/device/conveyor_device.py
+++ backend/app/modules/simulator/programming/simulator/device/conveyor_device.py
@@ -1,1 +1,39 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class ConveyorDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+
+        self.running = False
+
+        self.speed = 0
+
+    def start(
+        self,
+        speed=100,
+    ):
+
+        self.running = True
+
+        self.speed = speed
+
+    def stop(self):
+
+        self.running = False
+
+        self.speed = 0
+
+    def update(self):
+
+        pass
+
+    def reset(self):
+
+        self.stop()
