baa75fe0aef2b351fb3ab464a57d559c959447a6
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-22 06:39:59 -0300

    Update led_device.py

--- backend/app/modules/simulator/programming/simulator/device/led_device.py
+++ backend/app/modules/simulator/programming/simulator/device/led_device.py
@@ -1,1 +1,38 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+from app.modules.simulator.programming.simulator.runtime.runtime_gpio import (
+    runtime_gpio,
+)
+
+
+class LEDDevice(DeviceBase):
+
+    def __init__(
+
+        self,
+
+        name,
+
+        pin,
+
+    ):
+
+        super().__init__(name)
+
+        self.pin = pin
+
+        self.state = False
+
+    def update(self):
+
+        self.state = runtime_gpio.read(
+
+            self.pin,
+
+        )
+
+    def reset(self):
+
+        self.state = False
