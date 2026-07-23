5506868c7b9ef2d58542b528988d79cb6daae360
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-23 08:15:19 -0300

    Update bluetooth_device.py

--- backend/app/modules/simulator/programming/simulator/device/bluetooth_device.py
+++ backend/app/modules/simulator/programming/simulator/device/bluetooth_device.py
@@ -1,1 +1,33 @@
 
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
+
+
+class BluetoothDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+
+        self.connected = False
+        self.device = None
+
+    def connect(
+        self,
+        device,
+    ):
+        self.device = device
+        self.connected = True
+
+    def disconnect(self):
+        self.connected = False
+        self.device = None
+
+    def update(self):
+        pass
+
+    def reset(self):
+        self.disconnect()
