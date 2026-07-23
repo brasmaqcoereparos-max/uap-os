7593e6bf687fd2e0283435e5334e8c126e620a7a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-23 08:28:25 -0300

    Update i2c_device.py

--- backend/app/modules/simulator/programming/simulator/device/i2c_device.py
+++ backend/app/modules/simulator/programming/simulator/device/i2c_device.py
@@ -1,1 +1,33 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class I2CDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+
+        self.devices = {}
+
+    def register(
+        self,
+        address,
+        device,
+    ):
+        self.devices[address] = device
+
+    def read(
+        self,
+        address,
+    ):
+        return self.devices.get(address)
+
+    def update(self):
+        pass
+
+    def reset(self):
+        self.devices.clear()
