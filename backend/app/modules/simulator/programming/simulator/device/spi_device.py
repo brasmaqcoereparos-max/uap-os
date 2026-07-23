d00d477876b25ba7fbeca559b4858cd44d469683
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-23 08:30:20 -0300

    Update spi_device.py

--- backend/app/modules/simulator/programming/simulator/device/spi_device.py
+++ backend/app/modules/simulator/programming/simulator/device/spi_device.py
@@ -1,1 +1,32 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class SPIDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+
+        self.selected = None
+
+    def select(
+        self,
+        device,
+    ):
+        self.selected = device
+
+    def transfer(
+        self,
+        data,
+    ):
+        return data
+
+    def update(self):
+        pass
+
+    def reset(self):
+        self.selected = None
