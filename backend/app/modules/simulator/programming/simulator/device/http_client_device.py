a704d2a3f7c2463e73122c0bb4c0b3ac7873652c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-07-23 08:19:12 -0300

    Update http_client_device.py

--- backend/app/modules/simulator/programming/simulator/device/http_client_device.py
+++ backend/app/modules/simulator/programming/simulator/device/http_client_device.py
@@ -1,1 +1,38 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class HTTPClientDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+
+    def get(
+        self,
+        url,
+    ):
+        return {
+            "status": 200,
+            "url": url,
+        }
+
+    def post(
+        self,
+        url,
+        payload,
+    ):
+        return {
+            "status": 200,
+            "url": url,
+            "payload": payload,
+        }
+
+    def update(self):
+        pass
+
+    def reset(self):
+        pass
