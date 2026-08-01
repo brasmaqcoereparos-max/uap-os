3e0bf81a6a6b1f5ee74fbd64c4338f062e6512bf
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-01 08:50:16 -0300

    Implement ModbusTCPDevice class with basic methods

--- backend/app/modules/simulator/programming/simulator/device/modbus_tcp_device.py
+++ backend/app/modules/simulator/programming/simulator/device/modbus_tcp_device.py
@@ -1,1 +1,30 @@
+from app.modules.simulator.programming.simulator.devices.device_base import (
+    DeviceBase,
+)
 
+
+class ModbusTCPDevice(DeviceBase):
+
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(name)
+
+        self.connected = False
+
+    def connect(self):
+
+        self.connected = True
+
+    def disconnect(self):
+
+        self.connected = False
+
+    def update(self):
+
+        pass
+
+    def reset(self):
+
+        self.disconnect()
