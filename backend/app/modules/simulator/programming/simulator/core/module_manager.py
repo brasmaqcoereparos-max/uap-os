bb67e169b3fd1dbd2b7a41e72b7721e7179bf27b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-02 17:02:24 -0300

    Add ModuleManager class for module management
    Implemented ModuleManager class to manage modules.

--- backend/app/modules/simulator/programming/simulator/core/module_manager.py
+++ backend/app/modules/simulator/programming/simulator/core/module_manager.py
@@ -1,1 +1,30 @@
+class ModuleManager:
 
+    def __init__(self):
+
+        self.modules = []
+
+    def register(
+
+        self,
+
+        module,
+
+    ):
+
+        module.register()
+
+        self.modules.append(module)
+
+    def boot(self):
+
+        for module in self.modules:
+
+            module.boot()
+
+    def all(self):
+
+        return self.modules.copy()
+
+
+module_manager = ModuleManager()
