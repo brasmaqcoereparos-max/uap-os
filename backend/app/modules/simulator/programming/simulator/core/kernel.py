6e88d8e9d6d21a1e6aa0cc01103f6711b8cc48d5
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-02 16:57:26 -0300

    Update kernel.py

--- backend/app/modules/simulator/programming/simulator/core/kernel.py
+++ backend/app/modules/simulator/programming/simulator/core/kernel.py
@@ -1,1 +1,18 @@
+from app.modules.simulator.programming.simulator.plugins.plugin_loader import (
+    PluginLoader,
+)
 
+
+class Kernel:
+
+    initialized = False
+
+    @classmethod
+    def boot(cls):
+
+        if cls.initialized:
+            return
+
+        PluginLoader.load()
+
+        cls.initialized = True
