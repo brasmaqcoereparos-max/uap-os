fe6e0d23d21a264e83736f6a868cd15731cf1385
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-02 18:13:51 -0300

    Add NetworkHAL class with connection methods

--- backend/app/modules/simulator/programming/simulator/hal/network_hal.py
+++ backend/app/modules/simulator/programming/simulator/hal/network_hal.py
@@ -1,1 +1,24 @@
 
+class NetworkHAL:
+
+    def connect(self):
+
+        pass
+
+    def disconnect(self):
+
+        pass
+
+    def send(
+
+        self,
+
+        payload,
+
+    ):
+
+        pass
+
+    def receive(self):
+
+        return None
