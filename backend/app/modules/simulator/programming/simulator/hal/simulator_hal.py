03101a9638834d9a4f494d61722191bb73f8e7cd
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-02 17:35:13 -0300

    Add SimulatorHAL class with initialize and shutdown methods

--- backend/app/modules/simulator/programming/simulator/hal/simulator_hal.py
+++ backend/app/modules/simulator/programming/simulator/hal/simulator_hal.py
@@ -1,1 +1,14 @@
+from app.modules.simulator.programming.simulator.hal.hal_base import (
+    HALBase,
+)
 
+
+class SimulatorHAL(HALBase):
+
+    def initialize(self):
+
+        pass
+
+    def shutdown(self):
+
+        pass
