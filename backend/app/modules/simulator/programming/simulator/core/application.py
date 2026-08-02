eb9a1d811c6364fbe62de45ed2e8f1437f206d6b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-02 17:19:20 -0300

    Add Application class with start metho

--- backend/app/modules/simulator/programming/simulator/core/application.py
+++ backend/app/modules/simulator/programming/simulator/core/application.py
@@ -1,1 +1,19 @@
+from app.modules.simulator.programming.simulator.core.kernel import (
+    Kernel,
+)
 
+
+class Application:
+
+    started = False
+
+    @classmethod
+    def start(cls):
+
+        if cls.started:
+
+            return
+
+        Kernel.boot()
+
+        cls.started = True
