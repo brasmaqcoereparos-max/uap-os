f1e48b783c469828faf50b14bb64d18d66e118b8
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-02 17:37:19 -0300

    Add HALLoader class for SimulatorHAL management
    Implement HALLoader class to manage loading of SimulatorHAL.

--- backend/app/modules/simulator/programming/simulator/hal/hal_loader.py
+++ backend/app/modules/simulator/programming/simulator/hal/hal_loader.py
@@ -1,1 +1,27 @@
+from app.modules.simulator.programming.simulator.hal.hal_manager import (
+    hal_manager,
+)
 
+from app.modules.simulator.programming.simulator.hal.simulator_hal import (
+    SimulatorHAL,
+)
+
+
+class HALLoader:
+
+    loaded = False
+
+    @classmethod
+    def load(cls):
+
+        if cls.loaded:
+
+            return
+
+        hal_manager.set_driver(
+
+            SimulatorHAL(),
+
+        )
+
+        cls.loaded = True
