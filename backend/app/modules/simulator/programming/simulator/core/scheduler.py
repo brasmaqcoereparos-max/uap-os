a62579d8140718fd7e1b2799596afbdf7cdd02e9
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-02 10:19:54 -0300

    Implement Scheduler class for task management

--- backend/app/modules/simulator/programming/simulator/core/scheduler.py
+++ backend/app/modules/simulator/programming/simulator/core/scheduler.py
@@ -1,1 +1,28 @@
+class Scheduler:
 
+    def __init__(self):
+
+        self.tasks = []
+
+    def add(
+
+        self,
+
+        task,
+
+    ):
+
+        self.tasks.append(task)
+
+    def execute(self):
+
+        for task in self.tasks:
+
+            task()
+
+    def clear(self):
+
+        self.tasks.clear()
+
+
+scheduler = Scheduler()
