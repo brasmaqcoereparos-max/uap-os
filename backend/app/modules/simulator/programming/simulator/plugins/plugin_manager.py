a66492edfec76e93afbd7f587faba60f3d34bb77
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-02 09:58:04 -0300

    Add PluginManager class for plugin management
    Implement a PluginManager class to manage plugins, including registration, shutdown, and retrieval of all plugins.

--- backend/app/modules/simulator/programming/simulator/plugins/plugin_manager.py
+++ backend/app/modules/simulator/programming/simulator/plugins/plugin_manager.py
@@ -1,1 +1,30 @@
+class PluginManager:
 
+    def __init__(self):
+
+        self.plugins = []
+
+    def register(
+
+        self,
+
+        plugin,
+
+    ):
+
+        self.plugins.append(plugin)
+
+        plugin.initialize()
+
+    def shutdown(self):
+
+        for plugin in self.plugins:
+
+            plugin.shutdown()
+
+    def all(self):
+
+        return self.plugins.copy()
+
+
+plugin_manager = PluginManager()
