c6912251ea56938cd680ca18d35b25d938bc1b9f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-02 17:15:04 -0300

    Integrate ModuleLoader and module_manager in Kernel

--- backend/app/modules/simulator/programming/simulator/core/kernel.py
+++ backend/app/modules/simulator/programming/simulator/core/kernel.py
@@ -2,6 +2,14 @@ from app.modules.simulator.programming.simulator.plugins.plugin_loader import (
     PluginLoader,
 )
 
+from app.modules.simulator.programming.simulator.core.module_loader import (
+    ModuleLoader,
+)
+
+from app.modules.simulator.programming.simulator.core.module_manager import (
+    module_manager,
+)
+
 
 class Kernel:
 
@@ -11,8 +19,13 @@ class Kernel:
     def boot(cls):
 
         if cls.initialized:
+
             return
 
         PluginLoader.load()
+
+        ModuleLoader.load()
+
+        module_manager.boot()
 
         cls.initialized = True
