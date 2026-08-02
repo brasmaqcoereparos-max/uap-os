64cdc18ba00aa4b9d71aebf621b8466907daa787
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-02 18:10:41 -0300

    Update gpio_hal.py

--- backend/app/modules/simulator/programming/simulator/hal/gpio_hal.py
+++ backend/app/modules/simulator/programming/simulator/hal/gpio_hal.py
@@ -1,1 +1,45 @@
+class GPIOHAL:
 
+    def digital_write(
+
+        self,
+
+        pin,
+
+        value,
+
+    ):
+
+        pass
+
+    def digital_read(
+
+        self,
+
+        pin,
+
+    ):
+
+        return 0
+
+    def analog_read(
+
+        self,
+
+        pin,
+
+    ):
+
+        return 0
+
+    def pwm_write(
+
+        self,
+
+        pin,
+
+        duty,
+
+    ):
+
+        pass
