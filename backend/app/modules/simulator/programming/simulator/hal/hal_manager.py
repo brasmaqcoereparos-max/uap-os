2e5f9ba60e77e5302f98eb9601d533b7293e8d48
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-02 17:33:39 -0300

    Implement HALManager class with driver management

--- backend/app/modules/simulator/programming/simulator/hal/hal_manager.py
+++ backend/app/modules/simulator/programming/simulator/hal/hal_manager.py
@@ -1,1 +1,22 @@
+class HALManager:
 
+    def __init__(self):
+
+        self.driver = None
+
+    def set_driver(
+
+        self,
+
+        driver,
+
+    ):
+
+        self.driver = driver
+
+    def get_driver(self):
+
+        return self.driver
+
+
+hal_manager = HALManager()
