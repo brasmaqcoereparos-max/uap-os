90e984243c6c4091ebeeb4f27d53cd63ecd11d31
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-03 09:58:11 -0300

    Update statistics.py

--- backend/app/modules/simulator/programming/simulator/hardware_engine/statistics.py
+++ backend/app/modules/simulator/programming/simulator/hardware_engine/statistics.py
@@ -1,1 +1,16 @@
+class Statistics:
 
+    def __init__(self):
+
+        self.frames = 0
+
+    def frame(self):
+
+        self.frames += 1
+
+    def reset(self):
+
+        self.frames = 0
+
+
+statistics = Statistics()
