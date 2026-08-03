3a28a81566c78d807731898fe605df93d4d7fafe
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-03 13:13:53 -0300

    Implement TimerManager class for timer management

--- backend/app/modules/simulator/programming/simulator/hardware_engine/timer_manager.py
+++ backend/app/modules/simulator/programming/simulator/hardware_engine/timer_manager.py
@@ -1,1 +1,28 @@
+class TimerManager:
 
+    def __init__(self):
+
+        self.timers = []
+
+    def add(
+
+        self,
+
+        timer,
+
+    ):
+
+        self.timers.append(timer)
+
+    def update(self):
+
+        for timer in self.timers:
+
+            timer.update()
+
+    def clear(self):
+
+        self.timers.clear()
+
+
+timer_manager = TimerManager()
