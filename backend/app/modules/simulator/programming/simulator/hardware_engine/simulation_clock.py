aef1d9fcfa4a48c34dc009c560834e63b901ad42
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-03 09:50:36 -0300

    Update simulation_clock.py

--- backend/app/modules/simulator/programming/simulator/hardware_engine/simulation_clock.py
+++ backend/app/modules/simulator/programming/simulator/hardware_engine/simulation_clock.py
@@ -1,1 +1,27 @@
+import time
 
+
+class SimulationClock:
+
+    def __init__(self):
+
+        self.start_time = time.time()
+
+        self.current_time = 0.0
+
+    def update(self):
+
+        self.current_time = time.time() - self.start_time
+
+    def reset(self):
+
+        self.start_time = time.time()
+
+        self.current_time = 0.0
+
+    def seconds(self):
+
+        return self.current_time
+
+
+simulation_clock = SimulationClock()
