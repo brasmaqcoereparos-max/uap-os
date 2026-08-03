e5a880d013847bb5f15f7b56e01aa07f8e305f8e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-03 09:00:09 -0300

    Add VirtualDevice class with basic methods

--- backend/app/modules/simulator/programming/simulator/hve/virtual_device.py
+++ backend/app/modules/simulator/programming/simulator/hve/virtual_device.py
@@ -1,1 +1,21 @@
+class VirtualDevice:
 
+    def __init__(
+
+        self,
+
+        name,
+
+    ):
+
+        self.name = name
+
+        self.enabled = True
+
+    def update(self):
+
+        pass
+
+    def reset(self):
+
+        pass
