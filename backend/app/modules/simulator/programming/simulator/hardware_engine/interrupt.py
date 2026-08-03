a20fa607e99b80efda9db47aa5e58f3b8bb437c3
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-03 13:04:29 -0300

    Update interrupt.py

--- backend/app/modules/simulator/programming/simulator/hardware_engine/interrupt.py
+++ backend/app/modules/simulator/programming/simulator/hardware_engine/interrupt.py
@@ -1,1 +1,19 @@
+class Interrupt:
 
+    def __init__(
+
+        self,
+
+        pin,
+
+        callback,
+
+    ):
+
+        self.pin = pin
+
+        self.callback = callback
+
+    def trigger(self):
+
+        self.callback()
