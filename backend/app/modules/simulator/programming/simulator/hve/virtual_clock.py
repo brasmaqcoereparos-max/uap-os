9e6c36c94a040fb7f95d70b9c2907c87cfc133e0
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-03 09:05:53 -0300

    Implement VirtualClock class with tick management

--- backend/app/modules/simulator/programming/simulator/hve/virtual_clock.py
+++ backend/app/modules/simulator/programming/simulator/hve/virtual_clock.py
@@ -1,1 +1,16 @@
+class VirtualClock:
 
+    def __init__(self):
+
+        self.tick = 0
+
+    def next(self):
+
+        self.tick += 1
+
+    def reset(self):
+
+        self.tick = 0
+
+
+virtual_clock = VirtualClock()
