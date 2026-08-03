590b9a45fc67cbd730a4c72195aa38cda537d312
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-03 09:03:51 -0300

    Implement VirtualWorld class with device management

--- backend/app/modules/simulator/programming/simulator/hve/virtual_world.py
+++ backend/app/modules/simulator/programming/simulator/hve/virtual_world.py
@@ -1,1 +1,30 @@
+class VirtualWorld:
 
+    def __init__(self):
+
+        self.devices = []
+
+    def add(
+
+        self,
+
+        device,
+
+    ):
+
+        self.devices.append(device)
+
+    def update(self):
+
+        for device in self.devices:
+
+            device.update()
+
+    def reset(self):
+
+        for device in self.devices:
+
+            device.reset()
+
+
+virtual_world = VirtualWorld()
