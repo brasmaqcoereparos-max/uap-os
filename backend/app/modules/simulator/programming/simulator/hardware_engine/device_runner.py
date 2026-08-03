886e22d023dbdf099672f244409c0554361c2d5d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-03 09:18:23 -0300

    Add DeviceRunner class to manage device updates

--- backend/app/modules/simulator/programming/simulator/hardware_engine/device_runner.py
+++ backend/app/modules/simulator/programming/simulator/hardware_engine/device_runner.py
@@ -1,1 +1,18 @@
 
+from app.modules.simulator.programming.simulator.devices.device_manager import (
+    device_manager,
+)
+
+
+class DeviceRunner:
+
+    def update(self):
+
+        for device in device_manager.devices:
+
+            if hasattr(device, "update"):
+
+                device.update()
+
+
+device_runner = DeviceRunner()
