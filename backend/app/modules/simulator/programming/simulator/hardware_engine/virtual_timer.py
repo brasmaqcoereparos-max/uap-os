ef67e8ed3693d93a1adbbe94b53306aefa2743a4
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-03 13:11:27 -0300

    Implement VirtualTimer class with callback functionality

--- backend/app/modules/simulator/programming/simulator/hardware_engine/virtual_timer.py
+++ backend/app/modules/simulator/programming/simulator/hardware_engine/virtual_timer.py
@@ -1,1 +1,30 @@
+import time
 
+
+class VirtualTimer:
+
+    def __init__(
+
+        self,
+
+        interval,
+
+        callback,
+
+    ):
+
+        self.interval = interval
+
+        self.callback = callback
+
+        self.last = time.time()
+
+    def update(self):
+
+        now = time.time()
+
+        if now - self.last >= self.interval:
+
+            self.last = now
+
+            self.callback()
