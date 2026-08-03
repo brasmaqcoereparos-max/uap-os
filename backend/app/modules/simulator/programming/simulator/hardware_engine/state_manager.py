54bdbf3403e03c82418ea747068ac3e3419ca697
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-03 12:54:24 -0300

    Update state_manager.py

--- backend/app/modules/simulator/programming/simulator/hardware_engine/state_manager.py
+++ backend/app/modules/simulator/programming/simulator/hardware_engine/state_manager.py
@@ -1,1 +1,33 @@
+from app.modules.simulator.programming.simulator.hardware_engine.simulation_state import (
+    simulation_state,
+)
 
+
+class StateManager:
+
+    def __init__(self):
+
+        self.state = simulation_state.STOPPED
+
+    def start(self):
+
+        self.state = simulation_state.RUNNING
+
+    def pause(self):
+
+        self.state = simulation_state.PAUSED
+
+    def stop(self):
+
+        self.state = simulation_state.STOPPED
+
+    def is_running(self):
+
+        return self.state == simulation_state.RUNNING
+
+    def is_paused(self):
+
+        return self.state == simulation_state.PAUSED
+
+
+state_manager = StateManager()
