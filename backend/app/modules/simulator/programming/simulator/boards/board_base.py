b50c8fe3dbb9e37aaff12ace47087966160f5cbd
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 07:01:45 -0300

    Add BoardBase class with basic attributes

--- backend/app/modules/simulator/programming/simulator/boards/board_base.py
+++ backend/app/modules/simulator/programming/simulator/boards/board_base.py
@@ -1,1 +1,25 @@
+class BoardBase:
 
+    name = "Board"
+
+    manufacturer = ""
+
+    gpio_count = 0
+
+    pwm_count = 0
+
+    adc_count = 0
+
+    flash_size = 0
+
+    ram_size = 0
+
+    cpu = ""
+
+    frequency = 0
+
+    def initialize(self):
+        pass
+
+    def shutdown(self):
+        pass
