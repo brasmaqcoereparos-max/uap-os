07ef51916c46371a73e8b110c9de3a38d167243c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 20:42:26 -0300

    Add PinBank and PeripheralBank to BoardDescription

--- backend/app/modules/simulator/programming/simulator/board_sdk/board_description.py
+++ backend/app/modules/simulator/programming/simulator/board_sdk/board_description.py
@@ -1,1 +1,18 @@
+from app.modules.simulator.programming.simulator.board_sdk.pin_bank import (
+    PinBank,
+)
 
+from app.modules.simulator.programming.simulator.board_sdk.peripheral_bank import (
+    PeripheralBank,
+)
+
+
+class BoardDescription:
+
+    def __init__(self):
+
+        self.pins = PinBank()
+
+        self.peripherals = PeripheralBank()
+
+        self.properties = {}
