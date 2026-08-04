23f8dc655c8355b63b70c4b83411149474c2a66a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 20:40:51 -0300

    Implement PeripheralBank class for managing peripherals

--- backend/app/modules/simulator/programming/simulator/board_sdk/peripheral_bank.py
+++ backend/app/modules/simulator/programming/simulator/board_sdk/peripheral_bank.py
@@ -1,1 +1,22 @@
+class PeripheralBank:
 
+    def __init__(self):
+
+        self.peripherals = []
+
+    def add(
+
+        self,
+
+        peripheral,
+
+    ):
+
+        self.peripherals.append(peripheral)
+
+    def all(self):
+
+        return self.peripherals.copy()
+
+
+peripheral_bank = PeripheralBank()
