d8234c209afaae05625d6c99b1ba59fb8e0e9e67
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 20:49:15 -0300

    Add PeripheralRegistry class with registration methods
    Implement PeripheralRegistry class for managing peripherals.

--- backend/app/modules/simulator/programming/simulator/peripheral_sdk/peripheral_registry.py
+++ backend/app/modules/simulator/programming/simulator/peripheral_sdk/peripheral_registry.py
@@ -1,1 +1,32 @@
+class PeripheralRegistry:
 
+    def __init__(self):
+
+        self._registry = {}
+
+    def register(
+
+        self,
+
+        peripheral_class,
+
+    ):
+
+        self._registry[peripheral_class.name] = peripheral_class
+
+    def get(
+
+        self,
+
+        name,
+
+    ):
+
+        return self._registry.get(name)
+
+    def all(self):
+
+        return self._registry.copy()
+
+
+peripheral_registry = PeripheralRegistry()
