7b7f0f91af3eb60560b9c2c2c093418918e0f2c5
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 14:53:35 -0300

    Update esp32_devkit.py

--- backend/app/modules/simulator/programming/simulator/boards/esp32_devkit.py
+++ backend/app/modules/simulator/programming/simulator/boards/esp32_devkit.py
@@ -1,1 +1,24 @@
+from app.modules.simulator.programming.simulator.boards.board_base import (
+    BoardBase,
+)
 
+
+class ESP32DevKit(BoardBase):
+
+    name = "ESP32 DevKit"
+
+    manufacturer = "Espressif"
+
+    gpio_count = 39
+
+    pwm_count = 16
+
+    adc_count = 18
+
+    flash_size = 4194304
+
+    ram_size = 520192
+
+    cpu = "Xtensa LX6 Dual Core"
+
+    frequency = 240000000
