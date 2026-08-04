93495dd091d9b26fc3bed1d070e50ee5527ba9f3
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 07:08:10 -0300

    Add BoardLoader class for loading ArduinoUNO
    Implement BoardLoader class to manage board loading.

--- backend/app/modules/simulator/programming/simulator/boards/board_loader.py
+++ backend/app/modules/simulator/programming/simulator/boards/board_loader.py
@@ -1,1 +1,27 @@
+from app.modules.simulator.programming.simulator.boards.board_manager import (
+    board_manager,
+)
 
+from app.modules.simulator.programming.simulator.boards.arduino_uno import (
+    ArduinoUNO,
+)
+
+
+class BoardLoader:
+
+    loaded = False
+
+    @classmethod
+    def load(cls):
+
+        if cls.loaded:
+
+            return
+
+        board_manager.set_board(
+
+            ArduinoUNO(),
+
+        )
+
+        cls.loaded = True
