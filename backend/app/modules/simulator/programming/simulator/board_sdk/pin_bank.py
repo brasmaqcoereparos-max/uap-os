b4d3a6c29a6a8652ad5bae4747a86af52be3a740
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 20:37:22 -0300

    Implement PinBank class for managing pins

--- backend/app/modules/simulator/programming/simulator/board_sdk/pin_bank.py
+++ backend/app/modules/simulator/programming/simulator/board_sdk/pin_bank.py
@@ -1,1 +1,32 @@
+class PinBank:
 
+    def __init__(self):
+
+        self.pins = {}
+
+    def add(
+
+        self,
+
+        pin,
+
+    ):
+
+        self.pins[pin.number] = pin
+
+    def get(
+
+        self,
+
+        number,
+
+    ):
+
+        return self.pins.get(number)
+
+    def all(self):
+
+        return list(self.pins.values())
+
+
+pin_bank = PinBank()
