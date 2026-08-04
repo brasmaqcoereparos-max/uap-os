40033594ea9474015a559e5ed615507e3f1e6c0f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 20:59:12 -0300

    Create Interface class with constructor
    Add Interface class with initialization for name and type.

--- backend/app/modules/simulator/programming/simulator/peripheral_sdk/interface.py
+++ backend/app/modules/simulator/programming/simulator/peripheral_sdk/interface.py
@@ -1,1 +1,17 @@
+class Interface:
 
+    def __init__(
+
+        self,
+
+        name,
+
+        interface_type,
+
+    ):
+
+        self.name = name
+
+        self.type = interface_type
+
+        self.parameters = {}
