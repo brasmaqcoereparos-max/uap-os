d3b7ae38582300f91148cc4e6d4e5b03957279dc
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 20:57:31 -0300

    Add PeripheralDescription class with attributes
    Initialize PeripheralDescription class with default attributes.

--- backend/app/modules/simulator/programming/simulator/peripheral_sdk/peripheral_description.py
+++ backend/app/modules/simulator/programming/simulator/peripheral_sdk/peripheral_description.py
@@ -1,1 +1,17 @@
+class PeripheralDescription:
 
+    def __init__(self):
+
+        self.name = ""
+
+        self.manufacturer = ""
+
+        self.category = ""
+
+        self.version = "1.0"
+
+        self.properties = {}
+
+        self.interfaces = []
+
+        self.pins = []
