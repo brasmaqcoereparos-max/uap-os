99679aa12e24f389ee702b26cc9438fb86952a37
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 07:06:06 -0300

    Add ArduinoUNO class with specifications

--- backend/app/modules/simulator/programming/simulator/boards/arduino_uno.py
+++ backend/app/modules/simulator/programming/simulator/boards/arduino_uno.py
@@ -1,1 +1,24 @@
+from app.modules.simulator.programming.simulator.boards.board_base import (
+    BoardBase,
+)
 
+
+class ArduinoUNO(BoardBase):
+
+    name = "Arduino Uno"
+
+    manufacturer = "Arduino"
+
+    gpio_count = 20
+
+    pwm_count = 6
+
+    adc_count = 6
+
+    flash_size = 32768
+
+    ram_size = 2048
+
+    cpu = "ATmega328P"
+
+    frequency = 16000000
