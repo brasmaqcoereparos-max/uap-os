949db5c1a1ebfba47447f138b54c8a9640c3755c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 20:50:46 -0300

    Implement PeripheralFactory for peripheral creation
    Added PeripheralFactory class to create peripherals using a registry.

--- backend/app/modules/simulator/programming/simulator/peripheral_sdk/peripheral_factory.py
+++ backend/app/modules/simulator/programming/simulator/peripheral_sdk/peripheral_factory.py
@@ -1,1 +1,44 @@
 
+from app.modules.simulator.programming.simulator.peripheral_sdk.peripheral_registry import (
+    peripheral_registry,
+)
+
+
+class PeripheralFactory:
+
+    def create(
+
+        self,
+
+        name,
+
+        *args,
+
+        **kwargs,
+
+    ):
+
+        peripheral = peripheral_registry.get(
+
+            name,
+
+        )
+
+        if peripheral is None:
+
+            raise ValueError(
+
+                f"Periférico '{name}' não encontrado."
+
+            )
+
+        return peripheral(
+
+            *args,
+
+            **kwargs,
+
+        )
+
+
+peripheral_factory = PeripheralFactory()
