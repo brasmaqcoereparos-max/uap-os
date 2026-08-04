7ea16253ec0ccefc2d62326ea57da5c7cb533da4
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 09:08:53 -0300

    Update arduino_mega2560.py

--- backend/app/modules/simulator/programming/simulator/boards/arduino_mega2560.py
+++ backend/app/modules/simulator/programming/simulator/boards/arduino_mega2560.py
@@ -1,1 +1,24 @@
+from app.modules.simulator.programming.simulator.boards.board_base import (
+    BoardBase,
+)
 
+
+class ArduinoMega2560(BoardBase):
+
+    name = "Arduino Mega 2560"
+
+    manufacturer = "Arduino"
+
+    gpio_count = 70
+
+    pwm_count = 15
+
+    adc_count = 16
+
+    flash_size = 262144
+
+    ram_size = 8192
+
+    cpu = "ATmega2560"
+
+    frequency = 16000000
