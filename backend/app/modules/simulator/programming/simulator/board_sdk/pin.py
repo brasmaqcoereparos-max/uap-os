1c82cf75cf1253bf1b1a9bd7b4b7d00b0613dba3
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 20:35:57 -0300

    Update pin.py

--- backend/app/modules/simulator/programming/simulator/board_sdk/pin.py
+++ backend/app/modules/simulator/programming/simulator/board_sdk/pin.py
@@ -1,1 +1,21 @@
+class Pin:
 
+    def __init__(
+
+        self,
+
+        number,
+
+        name,
+
+        modes=None,
+
+    ):
+
+        self.number = number
+
+        self.name = name
+
+        self.modes = modes or []
+
+        self.value = 0
