54649d29b69718675924adf0b59e5310da8c3ccc
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 20:47:36 -0300

    Add PeripheralBase class with basic methods

--- backend/app/modules/simulator/programming/simulator/peripheral_sdk/peripheral_base.py
+++ backend/app/modules/simulator/programming/simulator/peripheral_sdk/peripheral_base.py
@@ -1,1 +1,19 @@
 
+class PeripheralBase:
+
+    name = "Peripheral"
+
+    manufacturer = ""
+
+    category = ""
+
+    version = "1.0"
+
+    def initialize(self):
+        pass
+
+    def update(self):
+        pass
+
+    def shutdown(self):
+        pass
