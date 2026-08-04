fd73fe94fb6cf0d20c298ca4e7c6a4d64b639d63
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 15:52:23 -0300

    Implement BoardGenerator class with registration method

--- backend/app/modules/simulator/programming/simulator/board_sdk/board_generator.py
+++ backend/app/modules/simulator/programming/simulator/board_sdk/board_generator.py
@@ -1,1 +1,33 @@
+from app.modules.simulator.programming.simulator.board_sdk.board_validator import (
+    board_validator,
+)
 
+from app.modules.simulator.programming.simulator.board_sdk.board_registry import (
+    board_registry,
+)
+
+
+class BoardGenerator:
+
+    def register(
+
+        self,
+
+        board_class,
+
+    ):
+
+        board_validator.validate(
+
+            board_class,
+
+        )
+
+        board_registry.register(
+
+            board_class,
+
+        )
+
+
+board_generator = BoardGenerator()
