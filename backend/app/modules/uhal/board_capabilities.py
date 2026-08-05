1f6308855a4050ea05afef67f6c1b18d8cdb52cd
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 20:09:57 -0300

    Update board_capabilities.py

--- backend/app/modules/uhal/board_capabilities.py
+++ backend/app/modules/uhal/board_capabilities.py
@@ -1,1 +1,25 @@
+class BoardCapabilities:
 
+    def __init__(self):
+
+        self.gpio = 0
+
+        self.pwm = 0
+
+        self.adc = 0
+
+        self.dac = 0
+
+        self.uart = 0
+
+        self.i2c = 0
+
+        self.spi = 0
+
+        self.can = False
+
+        self.ethernet = False
+
+        self.wifi = False
+
+        self.bluetooth = False
