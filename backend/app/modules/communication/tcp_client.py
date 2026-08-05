5c12baee6e5c76ce999c64497fb8f085ba330461
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 11:44:45 -0300

    Implement TCPClient class with basic methods

--- backend/app/modules/communication/tcp_client.py
+++ backend/app/modules/communication/tcp_client.py
@@ -1,1 +1,39 @@
+class TCPClient:
 
+    def __init__(
+
+        self,
+
+        host="127.0.0.1",
+
+        port=0,
+
+    ):
+
+        self.host = host
+
+        self.port = port
+
+        self.connected = False
+
+    def connect(self):
+
+        self.connected = True
+
+    def disconnect(self):
+
+        self.connected = False
+
+    def send(
+
+        self,
+
+        data,
+
+    ):
+
+        pass
+
+    def receive(self):
+
+        return None
