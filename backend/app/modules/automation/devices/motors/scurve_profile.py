56fb1b3b15d6a454d1a0d8a2c283b2ed92656282
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 08:49:30 -0300

    Add SCurveProfile class with configuration method

--- backend/app/modules/automation/devices/motors/scurve_profile.py
+++ backend/app/modules/automation/devices/motors/scurve_profile.py
@@ -1,1 +1,27 @@
+class SCurveProfile:
 
+    def __init__(self):
+
+        self.jerk = 0
+
+        self.acceleration = 100
+
+        self.max_speed = 1000
+
+    def configure(
+
+        self,
+
+        jerk,
+
+        acceleration,
+
+        max_speed,
+
+    ):
+
+        self.jerk = jerk
+
+        self.acceleration = acceleration
+
+        self.max_speed = max_speed
