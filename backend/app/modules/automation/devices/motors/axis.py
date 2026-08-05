6b63144715f42cdf1cd3bcc729fbae28bfd3c873
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 08:51:34 -0300

    Update axis.py

--- backend/app/modules/automation/devices/motors/axis.py
+++ backend/app/modules/automation/devices/motors/axis.py
@@ -1,1 +1,37 @@
+class Axis:
 
+    def __init__(
+
+        self,
+
+        name,
+
+        motor,
+
+    ):
+
+        self.name = name
+
+        self.motor = motor
+
+        self.position = 0
+
+    def move_to(
+
+        self,
+
+        position,
+
+    ):
+
+        self.position = position
+
+    def move_relative(
+
+        self,
+
+        delta,
+
+    ):
+
+        self.position += delta
