97cd548af7bc0f31f281134a17768964f4e16d29
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 20:43:03 -0300

    Implement hardware initialization in system_boot.py
    Add hardware initialization function to load and initialize drivers.

--- backend/app/modules/uhal/system_boot.py
+++ backend/app/modules/uhal/system_boot.py
@@ -1,1 +1,20 @@
+from app.modules.uhal.auto_loader import (
+    auto_loader,
+)
 
+from app.modules.uhal.register_builtin_drivers import (
+    register_builtin_drivers,
+)
+
+
+def initialize_hardware():
+
+    register_builtin_drivers()
+
+    driver = auto_loader.load()
+
+    if driver:
+
+        driver.initialize()
+
+    return driver
