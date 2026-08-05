44461a31f8ab9341ad986aadcffafebfe5c8c78d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 08:23:42 -0300

    Update stepper_motor.py

--- backend/app/modules/automation/devices/motors/stepper_motor.py
+++ backend/app/modules/automation/devices/motors/stepper_motor.py
@@ -1,1 +1,36 @@
+from app.modules.automation.devices.motors.motor_base import (
+    MotorBase,
+)
 
+
+class StepperMotor(MotorBase):
+
+    def __init__(
+
+        self,
+
+        device_id,
+
+        name,
+
+    ):
+
+        super().__init__(
+
+            device_id,
+
+            name,
+
+        )
+
+        self.position = 0
+
+    def move_steps(
+
+        self,
+
+        steps,
+
+    ):
+
+        self.position += steps
