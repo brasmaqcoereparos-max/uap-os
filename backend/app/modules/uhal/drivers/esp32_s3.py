8918e45ad2e4b8e67210d2c840d8492a5530353b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 20:28:30 -0300

    Implement ESP32S3Driver with USB and Bluetooth support

--- backend/app/modules/uhal/drivers/esp32_s3.py
+++ backend/app/modules/uhal/drivers/esp32_s3.py
@@ -1,1 +1,14 @@
+from app.modules.uhal.drivers.esp32 import ESP32Driver
 
+
+class ESP32S3Driver(ESP32Driver):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.board.name = "ESP32-S3"
+
+        self.board.capabilities.usb = True
+
+        self.board.capabilities.bluetooth = True
