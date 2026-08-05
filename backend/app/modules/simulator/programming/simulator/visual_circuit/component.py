040313fae9165424a02b7814409a94a80a4dff92
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:15:39 -0300

    Add Component class with initialization parameters

--- backend/app/modules/simulator/programming/simulator/visual_circuit/component.py
+++ backend/app/modules/simulator/programming/simulator/visual_circuit/component.py
@@ -1,1 +1,25 @@
+class Component:
 
+    def __init__(
+
+        self,
+
+        component_id,
+
+        name,
+
+        x=0,
+
+        y=0,
+
+    ):
+
+        self.id = component_id
+
+        self.name = name
+
+        self.x = x
+
+        self.y = y
+
+        self.rotation = 0
