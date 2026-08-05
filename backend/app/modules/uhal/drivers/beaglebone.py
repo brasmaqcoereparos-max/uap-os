e7b3cabb277f99d8fcbffa6d277b41f404203451
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 19:44:15 -0300

    Update beaglebone.py

--- backend/app/modules/uhal/drivers/beaglebone.py
+++ backend/app/modules/uhal/drivers/beaglebone.py
@@ -1,1 +1,18 @@
+from app.modules.uhal.drivers.simulator import (
+    SimulatorDriver,
+)
 
+
+class BeagleBoneDriver(SimulatorDriver):
+
+    BOARD_NAME = "BeagleBone Black"
+
+    GPIO_COUNT = 65
+
+    PWM_PINS = list(range(65))
+
+    ADC_CHANNELS = 7
+
+    DAC_CHANNELS = 0
+
+    HAS_CAN = True
