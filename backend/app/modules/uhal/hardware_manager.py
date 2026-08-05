e00908b6128855ee1e9f8f38c1f57b4f7b0ebb3f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 12:35:38 -0300

    Implement HardwareManager class with driver loading

--- backend/app/modules/uhal/hardware_manager.py
+++ backend/app/modules/uhal/hardware_manager.py
@@ -1,1 +1,29 @@
+from app.modules.uhal.hardware_registry import (
+    hardware_registry,
+)
 
+
+class HardwareManager:
+
+    def __init__(self):
+
+        self.current_driver = None
+
+    def load(
+
+        self,
+
+        name,
+
+    ):
+
+        self.current_driver = hardware_registry.get(name)
+
+        return self.current_driver
+
+    def current(self):
+
+        return self.current_driver
+
+
+hardware_manager = HardwareManager()
