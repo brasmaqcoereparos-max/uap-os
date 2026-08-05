5c4f0560ae343ce841391263ea03aa34519536bd
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 10:23:57 -0300

    Implement UARTInterface class
    Add UARTInterface class for UART communication with adjustable baud rate.

--- backend/app/modules/communication/uart.py
+++ backend/app/modules/communication/uart.py
@@ -1,1 +1,32 @@
+from app.modules.communication.interface_base import (
+    CommunicationInterface,
+)
 
+
+class UARTInterface(CommunicationInterface):
+
+    def __init__(
+
+        self,
+
+        baudrate=115200,
+
+    ):
+
+        super().__init__(
+
+            "UART",
+
+        )
+
+        self.baudrate = baudrate
+
+    def set_baudrate(
+
+        self,
+
+        baudrate,
+
+    ):
+
+        self.baudrate = baudrate
