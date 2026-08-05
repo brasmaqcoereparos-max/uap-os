e98882133a765522e2b8be5fe814b58767c5894a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 11:43:23 -0300

    Implement HTTPClient class with basic methods

--- backend/app/modules/communication/http_client.py
+++ backend/app/modules/communication/http_client.py
@@ -1,1 +1,45 @@
+class HTTPClient:
 
+    def get(
+
+        self,
+
+        url,
+
+    ):
+
+        return None
+
+    def post(
+
+        self,
+
+        url,
+
+        data,
+
+    ):
+
+        return None
+
+    def put(
+
+        self,
+
+        url,
+
+        data,
+
+    ):
+
+        return None
+
+    def delete(
+
+        self,
+
+        url,
+
+    ):
+
+        return None
