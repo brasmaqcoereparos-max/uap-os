8c9814e10dc86cf86c82cda25badfb904d6d8b65
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:21:25 -0300

    Add Connection class for circuit connections

--- backend/app/modules/simulator/programming/simulator/visual_circuit/connection.py
+++ backend/app/modules/simulator/programming/simulator/visual_circuit/connection.py
@@ -1,1 +1,24 @@
 
+class Connection:
+
+    def __init__(
+
+        self,
+
+        source,
+
+        source_pin,
+
+        target,
+
+        target_pin,
+
+    ):
+
+        self.source = source
+
+        self.source_pin = source_pin
+
+        self.target = target
+
+        self.target_pin = target_pin
