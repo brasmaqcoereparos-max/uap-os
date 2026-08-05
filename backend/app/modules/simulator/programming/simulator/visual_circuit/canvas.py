6eaa62c175a5e3f5777fc3cbda8b6294f93fb934
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:26:40 -0300

    Add Canvas class with zoom and move functionality

--- backend/app/modules/simulator/programming/simulator/visual_circuit/canvas.py
+++ backend/app/modules/simulator/programming/simulator/visual_circuit/canvas.py
@@ -1,1 +1,40 @@
+class Canvas:
 
+    def __init__(self):
+
+        self.width = 10000
+
+        self.height = 10000
+
+        self.zoom = 1.0
+
+        self.offset_x = 0
+
+        self.offset_y = 0
+
+    def set_zoom(
+
+        self,
+
+        zoom,
+
+    ):
+
+        self.zoom = zoom
+
+    def move(
+
+        self,
+
+        dx,
+
+        dy,
+
+    ):
+
+        self.offset_x += dx
+
+        self.offset_y += dy
+
+
+canvas = Canvas()
