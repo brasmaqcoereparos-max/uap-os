ec233a95765f8d08a221f1f59455b4a0dffe0394
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 09:41:32 -0300

    Update digital_input.py

--- backend/app/modules/automation/devices/inputs/digital_input.py
+++ backend/app/modules/automation/devices/inputs/digital_input.py
@@ -1,1 +1,30 @@
+from app.modules.automation.devices.inputs.input_base import (
+    InputBase,
+)
 
+
+class DigitalInput(InputBase):
+
+    def __init__(
+
+        self,
+
+        device_id,
+
+        name,
+
+    ):
+
+        super().__init__(
+
+            device_id,
+
+            name,
+
+        )
+
+        self.value = False
+
+    def is_active(self):
+
+        return self.value
