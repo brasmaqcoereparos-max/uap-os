741e4d9cda03917e9c6588ed967603de3d372d66
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:23:15 -0300

    Implement ConnectionManager class for managing connections

--- backend/app/modules/simulator/programming/simulator/visual_circuit/connection_manager.py
+++ backend/app/modules/simulator/programming/simulator/visual_circuit/connection_manager.py
@@ -1,1 +1,34 @@
+class ConnectionManager:
 
+    def __init__(self):
+
+        self.connections = []
+
+    def add(
+
+        self,
+
+        connection,
+
+    ):
+
+        self.connections.append(connection)
+
+    def remove(
+
+        self,
+
+        connection,
+
+    ):
+
+        if connection in self.connections:
+
+            self.connections.remove(connection)
+
+    def all(self):
+
+        return self.connections.copy()
+
+
+connection_manager = ConnectionManager()
