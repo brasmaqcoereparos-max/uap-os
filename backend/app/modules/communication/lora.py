dc2f5faca7b1ef490d24e3316ea29ae0ae6dd3c4
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 11:58:27 -0300

    Update lora.py

--- backend/app/modules/communication/lora.py
+++ backend/app/modules/communication/lora.py
@@ -1,1 +1,25 @@
+class LoRaInterface:
 
+    def __init__(
+
+        self,
+
+        frequency=915000000,
+
+    ):
+
+        self.frequency = frequency
+
+        self.connected = False
+
+    def connect(self):
+
+        self.connected = True
+
+    def disconnect(self):
+
+        self.connected = False
+
+    def status(self):
+
+        return self.connected
