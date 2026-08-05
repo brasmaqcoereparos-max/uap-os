19ee0cfc34225a540950b09ed0568907e7cba679
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 10:39:32 -0300

    Update spi.py

--- backend/app/modules/communication/spi.py
+++ backend/app/modules/communication/spi.py
@@ -1,1 +1,32 @@
+from app.modules.communication.interface_base import (
+    CommunicationInterface,
+)
 
+
+class SPIInterface(CommunicationInterface):
+
+    def __init__(
+
+        self,
+
+        frequency=1000000,
+
+    ):
+
+        super().__init__(
+
+            "SPI",
+
+        )
+
+        self.frequency = frequency
+
+    def set_frequency(
+
+        self,
+
+        frequency,
+
+    ):
+
+        self.frequency = frequency
