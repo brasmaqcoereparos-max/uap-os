acfe89917532dbd8c2e973c345a0f2fb6c2713c4
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 10:53:06 -0300

    Update can_bus.py

--- backend/app/modules/communication/can_bus.py
+++ backend/app/modules/communication/can_bus.py
@@ -1,1 +1,48 @@
+from app.modules.communication.interface_base import (
+    CommunicationInterface,
+)
 
+
+class CANBusInterface(CommunicationInterface):
+
+    def __init__(
+
+        self,
+
+        bitrate=500000,
+
+    ):
+
+        super().__init__(
+
+            "CAN",
+
+        )
+
+        self.bitrate = bitrate
+
+    def set_bitrate(
+
+        self,
+
+        bitrate,
+
+    ):
+
+        self.bitrate = bitrate
+
+    def send(
+
+        self,
+
+        can_id,
+
+        data,
+
+    ):
+
+        pass
+
+    def receive(self):
+
+        return None
