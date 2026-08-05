c372de7f07baea5513e4812b4a88849823a82959
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:53:24 -0300

    Update device.py

--- backend/app/modules/automation/device.py
+++ backend/app/modules/automation/device.py
@@ -1,1 +1,29 @@
+class Device:
 
+    def __init__(
+
+        self,
+
+        device_id,
+
+        name,
+
+    ):
+
+        self.id = device_id
+
+        self.name = name
+
+        self.enabled = True
+
+    def initialize(self):
+
+        pass
+
+    def update(self):
+
+        pass
+
+    def shutdown(self):
+
+        pass
