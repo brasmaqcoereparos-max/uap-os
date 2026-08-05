e0e41c6166d143025ef44839e2b5067acd3dbb5a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 12:18:07 -0300

    Implement UHALBase class with basic methods

--- backend/app/modules/uhal/hal_base.py
+++ backend/app/modules/uhal/hal_base.py
@@ -1,1 +1,25 @@
+class UHALBase:
 
+    def initialize(self):
+        pass
+
+    def shutdown(self):
+        pass
+
+    def digital_write(self, pin, value):
+        pass
+
+    def digital_read(self, pin):
+        return 0
+
+    def analog_read(self, pin):
+        return 0
+
+    def analog_write(self, pin, value):
+        pass
+
+    def pwm_write(self, pin, duty):
+        pass
+
+    def pwm_frequency(self, pin, frequency):
+        pass
