6fbc78b93f2f71ffb99960560d66cda7997ade2e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:57:00 -0300

    Implement DeviceGroup class with basic methods

--- backend/app/modules/automation/device_group.py
+++ backend/app/modules/automation/device_group.py
@@ -1,1 +1,39 @@
+class DeviceGroup:
 
+    def __init__(
+
+        self,
+
+        name,
+
+    ):
+
+        self.name = name
+
+        self.devices = []
+
+    def add(
+
+        self,
+
+        device,
+
+    ):
+
+        self.devices.append(device)
+
+    def remove(
+
+        self,
+
+        device,
+
+    ):
+
+        if device in self.devices:
+
+            self.devices.remove(device)
+
+    def all(self):
+
+        return self.devices.copy()
