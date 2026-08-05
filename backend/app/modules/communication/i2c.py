68d3f0f595ac0cd181ee91b804b26bc95dd3a462
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 10:25:39 -0300

    Update i2c.py

--- backend/app/modules/communication/i2c.py
+++ backend/app/modules/communication/i2c.py
@@ -1,1 +1,32 @@
+from app.modules.communication.interface_base import (
+    CommunicationInterface,
+)
 
+
+class I2CInterface(CommunicationInterface):
+
+    def __init__(
+
+        self,
+
+        frequency=100000,
+
+    ):
+
+        super().__init__(
+
+            "I2C",
+
+        )
+
+        self.frequency = frequency
+
+    def set_frequency(
+
+        self,
+
+        frequency,
+
+    ):
+
+        self.frequency = frequency
