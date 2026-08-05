259005d5b76b1c1fa846c23e6b3fabdd96123eec
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 08:42:50 -0300

    Update energy_monitor.py

--- backend/app/modules/automation/devices/motors/energy_monitor.py
+++ backend/app/modules/automation/devices/motors/energy_monitor.py
@@ -1,1 +1,31 @@
+class EnergyMonitor:
 
+    def __init__(self):
+
+        self.voltage = 0
+
+        self.current = 0
+
+    def power(self):
+
+        return self.voltage * self.current
+
+    def set_voltage(
+
+        self,
+
+        voltage,
+
+    ):
+
+        self.voltage = voltage
+
+    def set_current(
+
+        self,
+
+        current,
+
+    ):
+
+        self.current = current
