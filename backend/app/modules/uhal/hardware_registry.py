d5f3ef5f885d4dba5fbfb861f2f839f76ea0b903
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 12:33:09 -0300

    Update hardware_registry.py

--- backend/app/modules/uhal/hardware_registry.py
+++ backend/app/modules/uhal/hardware_registry.py
@@ -1,1 +1,34 @@
+class HardwareRegistry:
 
+    def __init__(self):
+
+        self.drivers = {}
+
+    def register(
+
+        self,
+
+        name,
+
+        driver,
+
+    ):
+
+        self.drivers[name] = driver
+
+    def get(
+
+        self,
+
+        name,
+
+    ):
+
+        return self.drivers.get(name)
+
+    def all(self):
+
+        return self.drivers.copy()
+
+
+hardware_registry = HardwareRegistry()
