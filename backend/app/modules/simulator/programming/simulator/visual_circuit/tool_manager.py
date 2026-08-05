59e5c0384db6fd95b83f58cafed621e0b7433e73
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:34:16 -0300

    Update tool_manager.py

--- backend/app/modules/simulator/programming/simulator/visual_circuit/tool_manager.py
+++ backend/app/modules/simulator/programming/simulator/visual_circuit/tool_manager.py
@@ -1,1 +1,22 @@
+class ToolManager:
 
+    def __init__(self):
+
+        self.current_tool = "select"
+
+    def set_tool(
+
+        self,
+
+        tool,
+
+    ):
+
+        self.current_tool = tool
+
+    def get_tool(self):
+
+        return self.current_tool
+
+
+tool_manager = ToolManager()
