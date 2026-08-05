afe5eea78c58363af02d196eae7ffb4052b51963
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 08:21:10 -0300

    Update dc_motor.py

--- backend/app/modules/automation/devices/motors/dc_motor.py
+++ backend/app/modules/automation/devices/motors/dc_motor.py
@@ -1,1 +1,24 @@
+from app.modules.automation.devices.motors.motor_base import (
+    MotorBase,
+)
 
+
+class DCMotor(MotorBase):
+
+    def __init__(
+
+        self,
+
+        device_id,
+
+        name,
+
+    ):
+
+        super().__init__(
+
+            device_id,
+
+            name,
+
+        )
