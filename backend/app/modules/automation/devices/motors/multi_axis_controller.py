d1d86beb619d1bf2e798d1f2ef7afecee3cbf4e6
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 08:53:35 -0300

    Implement MultiAxisController class for managing axes

--- backend/app/modules/automation/devices/motors/multi_axis_controller.py
+++ backend/app/modules/automation/devices/motors/multi_axis_controller.py
@@ -1,1 +1,32 @@
+class MultiAxisController:
 
+    def __init__(self):
+
+        self.axes = {}
+
+    def add_axis(
+
+        self,
+
+        axis,
+
+    ):
+
+        self.axes[axis.name] = axis
+
+    def get_axis(
+
+        self,
+
+        name,
+
+    ):
+
+        return self.axes.get(name)
+
+    def all_axes(self):
+
+        return list(self.axes.values())
+
+
+multi_axis_controller = MultiAxisController()
