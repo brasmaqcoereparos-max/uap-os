f0820c30ac97a1248f16a62645da1760c4ca1c3f
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:55:17 -0300

    Add DeviceRegistry class for device management
    Implemented DeviceRegistry class to manage devices.

--- backend/app/modules/automation/device_registry.py
+++ backend/app/modules/automation/device_registry.py
@@ -1,1 +1,48 @@
+class DeviceRegistry:
 
+    def __init__(self):
+
+        self.devices = {}
+
+    def register(
+
+        self,
+
+        device,
+
+    ):
+
+        self.devices[device.id] = device
+
+    def get(
+
+        self,
+
+        device_id,
+
+    ):
+
+        return self.devices.get(device_id)
+
+    def remove(
+
+        self,
+
+        device_id,
+
+    ):
+
+        self.devices.pop(
+
+            device_id,
+
+            None,
+
+        )
+
+    def all(self):
+
+        return list(self.devices.values())
+
+
+device_registry = DeviceRegistry()
