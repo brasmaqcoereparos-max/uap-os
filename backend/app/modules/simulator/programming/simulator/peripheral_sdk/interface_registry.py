02ef5003286a3861c2a74a7308fbba151f377b3e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:00:54 -0300

    Add InterfaceRegistry class for interface management
    Implemented InterfaceRegistry class to manage interfaces.

--- backend/app/modules/simulator/programming/simulator/peripheral_sdk/interface_registry.py
+++ backend/app/modules/simulator/programming/simulator/peripheral_sdk/interface_registry.py
@@ -1,1 +1,32 @@
+class InterfaceRegistry:
 
+    def __init__(self):
+
+        self.interfaces = {}
+
+    def register(
+
+        self,
+
+        interface,
+
+    ):
+
+        self.interfaces[interface.name] = interface
+
+    def get(
+
+        self,
+
+        name,
+
+    ):
+
+        return self.interfaces.get(name)
+
+    def all(self):
+
+        return self.interfaces.copy()
+
+
+interface_registry = InterfaceRegistry()
