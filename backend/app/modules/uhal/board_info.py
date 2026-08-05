5beb41ade320ee96ec5974aabea36d88c330c236
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 20:13:28 -0300

    Update board_info.py

--- backend/app/modules/uhal/board_info.py
+++ backend/app/modules/uhal/board_info.py
@@ -1,1 +1,24 @@
+from app.modules.uhal.board_capabilities import (
+    BoardCapabilities,
+)
 
+
+class BoardInfo:
+
+    def __init__(
+
+        self,
+
+        name,
+
+        manufacturer,
+
+    ):
+
+        self.name = name
+
+        self.manufacturer = manufacturer
+
+        self.version = ""
+
+        self.capabilities = BoardCapabilities()
