db2e88e4550c9049b986bca12beaa2f7051a6eff
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:42:57 -0300

    Implement rotation method for components
    Add a static method to rotate components by a given angle.

--- backend/app/modules/simulator/programming/simulator/visual_circuit/rotation.py
+++ backend/app/modules/simulator/programming/simulator/visual_circuit/rotation.py
@@ -1,1 +1,16 @@
+class Rotation:
 
+    @staticmethod
+    def rotate(
+
+        component,
+
+        angle,
+
+    ):
+
+        component.rotation = (
+
+            component.rotation + angle
+
+        ) % 360
