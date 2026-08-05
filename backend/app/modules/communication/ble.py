16eebf6dc593b9bf432071f20e46eec208db1276
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 11:56:21 -0300

    Implement BLEInterface class for Bluetooth communication

--- backend/app/modules/communication/ble.py
+++ backend/app/modules/communication/ble.py
@@ -1,1 +1,25 @@
+class BLEInterface:
 
+    def __init__(
+
+        self,
+
+        device_name="UAP BLE",
+
+    ):
+
+        self.device_name = device_name
+
+        self.connected = False
+
+    def connect(self):
+
+        self.connected = True
+
+    def disconnect(self):
+
+        self.connected = False
+
+    def status(self):
+
+        return self.connected
