a901fcb456dfe98359792b95e78e72759464bfbf
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 10:05:56 -0300

    Update photoelectric_sensor.py

--- backend/app/modules/automation/devices/inputs/photoelectric_sensor.py
+++ backend/app/modules/automation/devices/inputs/photoelectric_sensor.py
@@ -1,1 +1,14 @@
+from app.modules.automation.devices.inputs.digital_input import (
+    DigitalInput,
+)
 
+
+class PhotoelectricSensor(DigitalInput):
+
+    def detect(self):
+
+        self.value = True
+
+    def clear(self):
+
+        self.value = False
