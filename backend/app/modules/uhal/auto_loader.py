62bd185d6719420e9fe370260dee550b254b2841
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 20:39:48 -0300

    Update auto_loader.py

--- backend/app/modules/uhal/auto_loader.py
+++ backend/app/modules/uhal/auto_loader.py
@@ -1,1 +1,19 @@
+from app.modules.uhal.board_detector import (
+    board_detector,
+)
 
+from app.modules.uhal.hal_manager import (
+    hal_manager,
+)
+
+
+class AutoLoader:
+
+    def load(self):
+
+        board = board_detector.detect()
+
+        return hal_manager.load(board)
+
+
+auto_loader = AutoLoader()
