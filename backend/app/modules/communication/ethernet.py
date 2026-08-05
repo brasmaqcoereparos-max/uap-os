ab8a9c5a3e44aee4cf34b3d4f1eff1f048888cc7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 11:50:12 -0300

    Implement EthernetInterface class with basic methods

--- backend/app/modules/communication/ethernet.py
+++ backend/app/modules/communication/ethernet.py
@@ -1,1 +1,33 @@
+class EthernetInterface:
 
+    def __init__(
+
+        self,
+
+        ip="192.168.0.100",
+
+        mask="255.255.255.0",
+
+        gateway="192.168.0.1",
+
+    ):
+
+        self.ip = ip
+
+        self.mask = mask
+
+        self.gateway = gateway
+
+        self.connected = False
+
+    def connect(self):
+
+        self.connected = True
+
+    def disconnect(self):
+
+        self.connected = False
+
+    def status(self):
+
+        return self.connected
