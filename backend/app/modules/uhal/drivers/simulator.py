5d50b651b54b214a1a8b5df4789a9b138c23c863
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 18:40:43 -0300

    Add SimulatorDriver for pin management
    Implement SimulatorDriver class with pin control methods.

--- backend/app/modules/uhal/drivers/simulator.py
+++ backend/app/modules/uhal/drivers/simulator.py
@@ -1,1 +1,94 @@
+from app.modules.uhal.hal_base import HALBase
 
+
+class SimulatorDriver(HALBase):
+
+    def __init__(self):
+
+        self.pins = {}
+
+    def pin_mode(
+
+        self,
+
+        pin,
+
+        mode,
+
+    ):
+
+        self.pins[pin] = {
+
+            "mode": mode,
+
+            "value": 0,
+
+        }
+
+    def digital_write(
+
+        self,
+
+        pin,
+
+        value,
+
+    ):
+
+        if pin in self.pins:
+
+            self.pins[pin]["value"] = value
+
+    def digital_read(
+
+        self,
+
+        pin,
+
+    ):
+
+        return self.pins.get(
+
+            pin,
+
+            {},
+
+        ).get(
+
+            "value",
+
+            0,
+
+        )
+
+    def analog_write(
+
+        self,
+
+        pin,
+
+        value,
+
+    ):
+
+        self.digital_write(
+
+            pin,
+
+            value,
+
+        )
+
+    def analog_read(
+
+        self,
+
+        pin,
+
+    ):
+
+        return self.digital_read(
+
+            pin,
+
+      )
