fce0ad645c17f30c62c986a83e4972bdd98ff34a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:44:58 -0300

    Implement Clipboard class for copy and paste functionality

--- backend/app/modules/simulator/programming/simulator/visual_circuit/clipboard.py
+++ backend/app/modules/simulator/programming/simulator/visual_circuit/clipboard.py
@@ -1,1 +1,22 @@
+class Clipboard:
 
+    def __init__(self):
+
+        self.data = None
+
+    def copy(
+
+        self,
+
+        obj,
+
+    ):
+
+        self.data = obj
+
+    def paste(self):
+
+        return self.data
+
+
+clipboard = Clipboard()
