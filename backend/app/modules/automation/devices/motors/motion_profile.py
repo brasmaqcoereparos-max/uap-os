db8ad7ba86837d2202b4207626b130af5620d632
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 08:39:12 -0300

    Update motion_profile.py

--- backend/app/modules/automation/devices/motors/motion_profile.py
+++ backend/app/modules/automation/devices/motors/motion_profile.py
@@ -1,1 +1,27 @@
+class MotionProfile:
 
+    def __init__(self):
+
+        self.acceleration = 100
+
+        self.deceleration = 100
+
+        self.max_speed = 1000
+
+    def configure(
+
+        self,
+
+        acceleration,
+
+        deceleration,
+
+        max_speed,
+
+    ):
+
+        self.acceleration = acceleration
+
+        self.deceleration = deceleration
+
+        self.max_speed = max_speed
