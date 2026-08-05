4894b512dea1b0c5659e06be485d32b688700d7e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 19:47:22 -0300

    Create BoardInfo class with constructor
    Add BoardInfo class with initialization for name and manufacturer

--- backend/app/modules/uhal/drivers/board_info.py
+++ backend/app/modules/uhal/drivers/board_info.py
@@ -1,1 +1,19 @@
+class BoardInfo:
 
+    def __init__(
+
+        self,
+
+        name,
+
+        manufacturer,
+
+    ):
+
+        self.name = name
+
+        self.manufacturer = manufacturer
+
+        self.version = ""
+
+        self.capabilities = None
