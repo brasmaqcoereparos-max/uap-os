cfdfecc1685673ec0946fec2cc9fc7d37e04e81b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 09:21:42 -0300

    Add Solenoid class with voltage management

--- backend/app/modules/automation/devices/outputs/solenoid.py
+++ backend/app/modules/automation/devices/outputs/solenoid.py
@@ -1,1 +1,36 @@
+from app.modules.automation.devices.outputs.output_base import (
+    OutputBase,
+)
 
+
+class Solenoid(OutputBase):
+
+    def __init__(
+
+        self,
+
+        device_id,
+
+        name,
+
+    ):
+
+        super().__init__(
+
+            device_id,
+
+            name,
+
+        )
+
+        self.voltage = 24
+
+    def set_voltage(
+
+        self,
+
+        voltage,
+
+    ):
+
+        self.voltage = voltage
