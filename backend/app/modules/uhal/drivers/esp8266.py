7a1c7171af068a32386fd6c23bde6be2a8f4ee47
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 19:34:10 -0300

    Update esp8266.py

--- backend/app/modules/uhal/drivers/esp8266.py
+++ backend/app/modules/uhal/drivers/esp8266.py
@@ -1,1 +1,14 @@
+from app.modules.uhal.drivers.simulator import (
+    SimulatorDriver,
+)
 
+
+class ESP8266Driver(SimulatorDriver):
+
+    BOARD_NAME = "ESP8266"
+
+    GPIO_COUNT = 17
+
+    PWM_PINS = list(range(17))
+
+    ADC_CHANNELS = 1
