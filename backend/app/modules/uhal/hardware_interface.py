d748ee34bc8a56e296ba2c96c9e2155c9c5a76c9
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 12:31:25 -0300

    Define abstract HardwareInterface class methods

--- backend/app/modules/uhal/hardware_interface.py
+++ backend/app/modules/uhal/hardware_interface.py
@@ -1,1 +1,60 @@
+from abc import ABC, abstractmethod
 
+
+class HardwareInterface(ABC):
+
+    @abstractmethod
+    def pin_mode(
+
+        self,
+
+        pin,
+
+        mode,
+
+    ):
+        pass
+
+    @abstractmethod
+    def digital_write(
+
+        self,
+
+        pin,
+
+        value,
+
+    ):
+        pass
+
+    @abstractmethod
+    def digital_read(
+
+        self,
+
+        pin,
+
+    ):
+        pass
+
+    @abstractmethod
+    def analog_write(
+
+        self,
+
+        pin,
+
+        value,
+
+    ):
+        pass
+
+    @abstractmethod
+    def analog_read(
+
+        self,
+
+        pin,
+
+    ):
+        pass
