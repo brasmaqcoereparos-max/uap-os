6c98e5f29af3b43ea8055ee20f62500c98076951
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 11:47:59 -0300

    Update udp_client.py

--- backend/app/modules/communication/udp_client.py
+++ backend/app/modules/communication/udp_client.py
@@ -1,1 +1,29 @@
+class UDPClient:
 
+    def __init__(
+
+        self,
+
+        host="127.0.0.1",
+
+        port=0,
+
+    ):
+
+        self.host = host
+
+        self.port = port
+
+    def send(
+
+        self,
+
+        data,
+
+    ):
+
+        pass
+
+    def receive(self):
+
+        return None
