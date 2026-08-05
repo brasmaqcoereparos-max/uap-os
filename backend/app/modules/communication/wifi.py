50bac38d967c259166677dbf1d84e2ee2cc5c2e5
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 11:52:11 -0300

    Update wifi.py

--- backend/app/modules/communication/wifi.py
+++ backend/app/modules/communication/wifi.py
@@ -1,1 +1,29 @@
+class WiFiInterface:
 
+    def __init__(
+
+        self,
+
+        ssid="",
+
+        password="",
+
+    ):
+
+        self.ssid = ssid
+
+        self.password = password
+
+        self.connected = False
+
+    def connect(self):
+
+        self.connected = True
+
+    def disconnect(self):
+
+        self.connected = False
+
+    def status(self):
+
+        return self.connected
