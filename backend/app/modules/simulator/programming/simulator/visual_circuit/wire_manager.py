c95f2bc4146930a393db634e3bade69640dae1d6
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:41:03 -0300

    Implement WireManager class for wire management

--- backend/app/modules/simulator/programming/simulator/visual_circuit/wire_manager.py
+++ backend/app/modules/simulator/programming/simulator/visual_circuit/wire_manager.py
@@ -1,1 +1,34 @@
+class WireManager:
 
+    def __init__(self):
+
+        self.wires = []
+
+    def add(
+
+        self,
+
+        wire,
+
+    ):
+
+        self.wires.append(wire)
+
+    def remove(
+
+        self,
+
+        wire,
+
+    ):
+
+        if wire in self.wires:
+
+            self.wires.remove(wire)
+
+    def all(self):
+
+        return self.wires.copy()
+
+
+wire_manager = WireManager()
