194815fd864f535d542d1910f25f6f08412164fc
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 09:07:31 -0300

    Implement Relay class inheriting from OutputBase

--- backend/app/modules/automation/devices/outputs/relay.py
+++ backend/app/modules/automation/devices/outputs/relay.py
@@ -1,1 +1,24 @@
+from app.modules.automation.devices.outputs.output_base import (
+    OutputBase,
+)
 
+
+class Relay(OutputBase):
+
+    def __init__(
+
+        self,
+
+        device_id,
+
+        name,
+
+    ):
+
+        super().__init__(
+
+            device_id,
+
+            name,
+
+        )
