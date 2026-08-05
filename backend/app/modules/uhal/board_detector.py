8d46f91cc100aef2ff3af6339f5e5b744b929c56
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 20:38:16 -0300

    Update board_detector.py

--- backend/app/modules/uhal/board_detector.py
+++ backend/app/modules/uhal/board_detector.py
@@ -1,1 +1,13 @@
+class BoardDetector:
 
+    def detect(self):
+
+        """
+        Futuramente detectará automaticamente
+        a placa em execução.
+        """
+
+        return "simulator"
+
+
+board_detector = BoardDetector()
