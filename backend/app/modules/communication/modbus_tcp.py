acdcdeff77b3ba03075fd68a48ce2528f17acddc
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 11:02:00 -0300

    Implement ModbusTCP class with basic methods

--- backend/app/modules/communication/modbus_tcp.py
+++ backend/app/modules/communication/modbus_tcp.py
@@ -1,1 +1,47 @@
+class ModbusTCP:
 
+    def __init__(
+
+        self,
+
+        host="127.0.0.1",
+
+        port=502,
+
+    ):
+
+        self.host = host
+
+        self.port = port
+
+        self.connected = False
+
+    def connect(self):
+
+        self.connected = True
+
+    def disconnect(self):
+
+        self.connected = False
+
+    def read_register(
+
+        self,
+
+        address,
+
+    ):
+
+        return None
+
+    def write_register(
+
+        self,
+
+        address,
+
+        value,
+
+    ):
+
+        pass
