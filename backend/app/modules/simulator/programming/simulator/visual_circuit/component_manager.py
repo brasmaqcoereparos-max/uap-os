aa9a87c567bf615fd096749c13a1094141b8281e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:19:22 -0300

    Implement ComponentManager class for component management

--- backend/app/modules/simulator/programming/simulator/visual_circuit/component_manager.py
+++ backend/app/modules/simulator/programming/simulator/visual_circuit/component_manager.py
@@ -1,1 +1,48 @@
+class ComponentManager:
 
+    def __init__(self):
+
+        self.components = {}
+
+    def add(
+
+        self,
+
+        component,
+
+    ):
+
+        self.components[component.id] = component
+
+    def get(
+
+        self,
+
+        component_id,
+
+    ):
+
+        return self.components.get(component_id)
+
+    def remove(
+
+        self,
+
+        component_id,
+
+    ):
+
+        self.components.pop(
+
+            component_id,
+
+            None,
+
+        )
+
+    def all(self):
+
+        return list(self.components.values())
+
+
+component_manager = ComponentManager()
