2a1993290691773da86dc173797c6f51175df55d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-04 21:07:36 -0300

    Add PeripheralGenerator class with registration method

--- backend/app/modules/simulator/programming/simulator/peripheral_sdk/peripheral_generator.py
+++ backend/app/modules/simulator/programming/simulator/peripheral_sdk/peripheral_generator.py
@@ -1,1 +1,33 @@
+from app.modules.simulator.programming.simulator.peripheral_sdk.peripheral_registry import (
+    peripheral_registry,
+)
 
+from app.modules.simulator.programming.simulator.peripheral_sdk.peripheral_validator import (
+    peripheral_validator,
+)
+
+
+class PeripheralGenerator:
+
+    def register(
+
+        self,
+
+        peripheral_class,
+
+    ):
+
+        peripheral_validator.validate(
+
+            peripheral_class,
+
+        )
+
+        peripheral_registry.register(
+
+            peripheral_class,
+
+        )
+
+
+peripheral_generator = PeripheralGenerator()
