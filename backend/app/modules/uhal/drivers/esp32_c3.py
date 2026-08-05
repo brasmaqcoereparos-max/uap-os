4528c27179176c745169346a6fce80fd42cc5daa
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 20:30:37 -0300

    Implement ESP32C3Driver with specific capabilities

--- backend/app/modules/uhal/drivers/esp32_c3.py
+++ backend/app/modules/uhal/drivers/esp32_c3.py
@@ -1,1 +1,14 @@
+from app.modules.uhal.drivers.esp32 import ESP32Driver
 
+
+class ESP32C3Driver(ESP32Driver):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.board.name = "ESP32-C3"
+
+        self.board.capabilities.bluetooth = True
+
+        self.board.capabilities.can = False
