1f2cd8a0c329ed78cd99034eb4780cc66fceb84e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 09:54:03 -0300

    Add Button class with press and release methods

--- backend/app/modules/automation/devices/inputs/button.py
+++ backend/app/modules/automation/devices/inputs/button.py
@@ -1,1 +1,14 @@
+from app.modules.automation.devices.inputs.digital_input import (
+    DigitalInput,
+)
 
+
+class Button(DigitalInput):
+
+    def press(self):
+
+        self.value = True
+
+    def release(self):
+
+        self.value = False
