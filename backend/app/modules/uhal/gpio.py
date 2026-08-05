1541143842dc54b9a9658124d3430e3a37fc3275
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 12:22:03 -0300

    Update gpio.py

--- backend/app/modules/uhal/gpio.py
+++ backend/app/modules/uhal/gpio.py
@@ -1,1 +1,39 @@
+from app.modules.uhal.hal_manager import uhal_manager
 
+
+def digital_write(pin, value):
+    driver = uhal_manager.get_driver()
+    if driver:
+        driver.digital_write(pin, value)
+
+
+def digital_read(pin):
+    driver = uhal_manager.get_driver()
+    if driver:
+        return driver.digital_read(pin)
+    return 0
+
+
+def analog_read(pin):
+    driver = uhal_manager.get_driver()
+    if driver:
+        return driver.analog_read(pin)
+    return 0
+
+
+def analog_write(pin, value):
+    driver = uhal_manager.get_driver()
+    if driver:
+        driver.analog_write(pin, value)
+
+
+def pwm_write(pin, duty):
+    driver = uhal_manager.get_driver()
+    if driver:
+        driver.pwm_write(pin, duty)
+
+
+def pwm_frequency(pin, frequency):
+    driver = uhal_manager.get_driver()
+    if driver:
+        driver.pwm_frequency(pin, frequency)
