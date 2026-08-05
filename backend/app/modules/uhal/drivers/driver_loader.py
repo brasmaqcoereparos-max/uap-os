2fbabbe948f0bc9d0cec5005f172c7888dad665b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 19:49:13 -0300

    Implement DriverLoader to load drivers from registry

--- backend/app/modules/uhal/drivers/driver_loader.py
+++ backend/app/modules/uhal/drivers/driver_loader.py
@@ -1,1 +1,19 @@
+from app.modules.uhal.hardware_registry import (
+    hardware_registry,
+)
 
+
+class DriverLoader:
+
+    def load(
+
+        self,
+
+        name,
+
+    ):
+
+        return hardware_registry.get(name)
+
+
+driver_loader = DriverLoader()
