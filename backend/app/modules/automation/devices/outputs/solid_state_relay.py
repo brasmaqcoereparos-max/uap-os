021f45e055e9158f64e067c09c9c381506fe7165
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 09:09:13 -0300

    Update solid_state_relay.py

--- backend/app/modules/automation/devices/outputs/solid_state_relay.py
+++ backend/app/modules/automation/devices/outputs/solid_state_relay.py
@@ -1,1 +1,26 @@
+from app.modules.automation.devices.outputs.output_base import (
+    OutputBase,
+)
 
+
+class SolidStateRelay(OutputBase):
+
+    def __init__(
+
+        self,
+
+        device_id,
+
+        name,
+
+    ):
+
+        super().__init__(
+
+            device_id,
+
+            name,
+
+        )
+
+        self.zero_cross = True
