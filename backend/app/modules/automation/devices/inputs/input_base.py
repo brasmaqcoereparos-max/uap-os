c5ffe71afd409a45b69feb730162ce7fa3d95eb7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 09:40:05 -0300

    Implement InputBase class for device inputs

--- backend/app/modules/automation/devices/inputs/input_base.py
+++ backend/app/modules/automation/devices/inputs/input_base.py
@@ -1,1 +1,38 @@
+from app.modules.automation.device import Device
 
+
+class InputBase(Device):
+
+    def __init__(
+
+        self,
+
+        device_id,
+
+        name,
+
+    ):
+
+        super().__init__(
+
+            device_id,
+
+            name,
+
+        )
+
+        self.value = None
+
+    def read(self):
+
+        return self.value
+
+    def update_value(
+
+        self,
+
+        value,
+
+    ):
+
+        self.value = value
