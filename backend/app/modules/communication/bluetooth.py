e1bfc8c2199c4ce58ee28b46bc08886c8f018108
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 11:54:53 -0300

    Update bluetooth.py

--- backend/app/modules/communication/bluetooth.py
+++ backend/app/modules/communication/bluetooth.py
@@ -1,1 +1,25 @@
+class BluetoothInterface:
 
+    def __init__(
+
+        self,
+
+        device_name="UAP",
+
+    ):
+
+        self.device_name = device_name
+
+        self.connected = False
+
+    def connect(self):
+
+        self.connected = True
+
+    def disconnect(self):
+
+        self.connected = False
+
+    def status(self):
+
+        return self.connected
