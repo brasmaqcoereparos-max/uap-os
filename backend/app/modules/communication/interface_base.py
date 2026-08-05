1506dbeb2db660f4792dbdb9c55726d4557a0699
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-05 10:22:14 -0300

    Update interface_base.py

--- backend/app/modules/communication/interface_base.py
+++ backend/app/modules/communication/interface_base.py
@@ -1,1 +1,25 @@
+class CommunicationInterface:
 
+    def __init__(
+
+        self,
+
+        name,
+
+    ):
+
+        self.name = name
+
+        self.connected = False
+
+    def connect(self):
+
+        self.connected = True
+
+    def disconnect(self):
+
+        self.connected = False
+
+    def is_connected(self):
+
+        return self.connected
