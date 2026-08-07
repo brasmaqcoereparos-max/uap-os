adcbe7bc9cce9d1a4fd86b35c746296029a01c1b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 20:33:36 -0300

    Add Sensor class extending Device class

--- backend/app/modules/ude/sensor.py
+++ backend/app/modules/ude/sensor.py
@@ -1,1 +1,29 @@
+from app.modules.ude.device import Device
 
+
+class Sensor(Device):
+
+    def __init__(
+        self,
+        name,
+        sensor_type,
+    ):
+
+        super().__init__(
+            name,
+            "sensor",
+        )
+
+        self.sensor_type = sensor_type
+        self.value = None
+
+    def update(
+        self,
+        value,
+    ):
+
+        self.value = value
+
+    def read(self):
+
+        return self.value
