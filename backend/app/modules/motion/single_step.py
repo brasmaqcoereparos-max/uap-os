6032e4b44595c5f7a54e6326366acdb1deb1cb39
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 04:05:11 -0300

    Update single_step.py

--- backend/app/modules/motion/single_step.py
+++ backend/app/modules/motion/single_step.py
@@ -1,1 +1,16 @@
+class SingleStep:
 
+    def __init__(self):
+
+        self.enabled = False
+
+    def enable(self):
+
+        self.enabled = True
+
+    def disable(self):
+
+        self.enabled = False
+
+
+single_step = SingleStep()
