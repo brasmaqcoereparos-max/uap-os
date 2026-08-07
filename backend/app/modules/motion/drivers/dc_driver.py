b326ceff50d4a83b006801a0b6c7c9a13972e2eb
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 04:16:01 -0300

    Implement DCMotorDriver class with move and stop methods

--- backend/app/modules/motion/drivers/dc_driver.py
+++ backend/app/modules/motion/drivers/dc_driver.py
@@ -1,1 +1,24 @@
+from app.modules.motion.drivers.driver_base import MotionDriverBase
 
+
+class DCMotorDriver(MotionDriverBase):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.speed = 0
+
+    def move(
+
+        self,
+
+        speed,
+
+    ):
+
+        self.speed = speed
+
+    def stop(self):
+
+        self.speed = 0
