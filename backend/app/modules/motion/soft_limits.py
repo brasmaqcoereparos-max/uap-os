7b3932399abcb1cdeb769e5739f45abebd559d2d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 04:06:42 -0300

    Add SoftLimits class for motion control

--- backend/app/modules/motion/soft_limits.py
+++ backend/app/modules/motion/soft_limits.py
@@ -1,1 +1,16 @@
+class SoftLimits:
 
+    def __init__(self):
+
+        self.enabled = True
+
+    def enable(self):
+
+        self.enabled = True
+
+    def disable(self):
+
+        self.enabled = False
+
+
+soft_limits = SoftLimits()
