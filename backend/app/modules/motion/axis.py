ef93ef36ade6c368475f5d8f81aab6a45c107722
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 03:43:39 -0300

    Create MotionAxis class with constructor
    Added MotionAxis class with initialization parameters.

--- backend/app/modules/motion/axis.py
+++ backend/app/modules/motion/axis.py
@@ -1,1 +1,19 @@
+class MotionAxis:
 
+    def __init__(
+
+        self,
+
+        name,
+
+    ):
+
+        self.name = name
+
+        self.position = 0
+
+        self.enabled = True
+
+        self.limit_min = None
+
+        self.limit_max = None
