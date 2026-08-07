ca1a3e26144a5e4b37a7f0b8b478b4cf5c5f1895
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 17:52:11 -0300

    Implement DeviceCommand class with parameter management

--- backend/app/modules/ude/device_command.py
+++ backend/app/modules/ude/device_command.py
@@ -1,1 +1,33 @@
+class DeviceCommand:
 
+    def __init__(
+        self,
+        name,
+        action,
+        parameters=None,
+    ):
+
+        self.name = name
+
+        self.action = action
+
+        self.parameters = parameters or {}
+
+    def set_parameter(
+        self,
+        name,
+        value,
+    ):
+
+        self.parameters[name] = value
+
+    def get_parameter(
+        self,
+        name,
+        default=None,
+    ):
+
+        return self.parameters.get(
+            name,
+            default,
+        )
