fd3da1f2f2610e052941d54a7627ad1740a6aa16
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 03:46:25 -0300

    Add HomeManager class for position management

--- backend/app/modules/motion/home.py
+++ backend/app/modules/motion/home.py
@@ -1,1 +1,22 @@
+class HomeManager:
 
+    def __init__(self):
+
+        self.position = None
+
+    def set(
+
+        self,
+
+        position,
+
+    ):
+
+        self.position = position
+
+    def get(self):
+
+        return self.position
+
+
+home_manager = HomeManager()
