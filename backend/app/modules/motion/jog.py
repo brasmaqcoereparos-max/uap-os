932b5544fad7858be9ce6c05dd018850012ac952
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 03:48:47 -0300

    Implement JogController with movement methods

--- backend/app/modules/motion/jog.py
+++ backend/app/modules/motion/jog.py
@@ -1,1 +1,38 @@
+class JogController:
 
+    def __init__(self):
+
+        self.increment = 1.0
+
+    def set_increment(
+
+        self,
+
+        value,
+
+    ):
+
+        self.increment = value
+
+    def move_positive(
+
+        self,
+
+        axis,
+
+    ):
+
+        axis.position += self.increment
+
+    def move_negative(
+
+        self,
+
+        axis,
+
+    ):
+
+        axis.position -= self.increment
+
+
+jog_controller = JogController()
