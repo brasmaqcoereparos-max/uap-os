6a7c6c6bc7047cc20f17830e4f533c4f1e0efabe
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 06:26:22 -0300

    Add DeviceType enum for device categorization
    Define an enumeration for various device types in the Universal Device Engine.

--- backend/app/modules/ude/device_types.py
+++ backend/app/modules/ude/device_types.py
@@ -1,3 +1,36 @@
-"""
+from enum import Enum
+
+
+class DeviceType(Enum):
+
+    MOTOR = "motor"
+
+    SERVO = "servo"
+
+    STEPPER = "stepper"
+
+    ENCODER = "encoder"
+
+    SENSOR = "sensor"
+
+    CAMERA = "camera"
+
+    RELAY = "relay"
+
+    VALVE = "valve"
+
+    DISPLAY = "display"
+
+    PLC = "plc"
+
+    ROBOT = "robot"
+
+    CNC = "cnc"
+
+    IO = "io"
+
+    POWER = "power"
+
+    VIRTUAL = "virtual""""
 Universal Device Engine
 """
