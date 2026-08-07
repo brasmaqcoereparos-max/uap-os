aef3d1f34a99995141f15174eedaedbb5e365d70
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 17:35:36 -0300

    Update device_factory.py

--- backend/app/modules/ude/device_factory.py
+++ backend/app/modules/ude/device_factory.py
@@ -1,3 +1,30 @@
-"""
+from app.modules.ude.device import Device
+from app.modules.ude.device_registry import device_registry
+
+
+class DeviceFactory:
+
+    def create(
+        self,
+        device_type,
+        name,
+    ):
+
+        device = device_registry.create(
+            device_type,
+            name,
+            device_type,
+        )
+
+        if device is not None:
+            return device
+
+        return Device(
+            name,
+            device_type,
+        )
+
+
+device_factory = DeviceFactory()"""
 Universal Device Engine
 """
