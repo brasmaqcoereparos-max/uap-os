d164bf286b5e55472ab26279350063818cf22395
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 20:34:50 -0300

    Implement Actuator class inheriting from Device

--- backend/app/modules/ude/actuator.py
+++ backend/app/modules/ude/actuator.py
@@ -1,1 +1,29 @@
+from app.modules.ude.device import Device
 
+
+class Actuator(Device):
+
+    def __init__(
+        self,
+        name,
+        actuator_type,
+    ):
+
+        super().__init__(
+            name,
+            "actuator",
+        )
+
+        self.actuator_type = actuator_type
+        self.value = 0
+
+    def set(
+        self,
+        value,
+    ):
+
+        self.value = value
+
+    def get(self):
+
+        return self.value
