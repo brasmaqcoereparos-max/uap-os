b2915a95ff1b16d6856bda9b869c7b336946ff84
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 03:45:12 -0300

    Implement AxisManager for managing MotionAxis instances

--- backend/app/modules/motion/axis_manager.py
+++ backend/app/modules/motion/axis_manager.py
@@ -1,1 +1,39 @@
+from app.modules.motion.axis import MotionAxis
 
+
+class AxisManager:
+
+    def __init__(self):
+
+        self.axes = {}
+
+    def add(
+
+        self,
+
+        name,
+
+    ):
+
+        axis = MotionAxis(name)
+
+        self.axes[name] = axis
+
+        return axis
+
+    def get(
+
+        self,
+
+        name,
+
+    ):
+
+        return self.axes.get(name)
+
+    def list(self):
+
+        return list(self.axes.keys())
+
+
+axis_manager = AxisManager()
