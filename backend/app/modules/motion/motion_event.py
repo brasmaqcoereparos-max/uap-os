464ac4ff5ba09c3dd9887f595353ac7d1e0eb560
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 03:36:39 -0300

    Add MotionEvent class with initializer

--- backend/app/modules/motion/motion_event.py
+++ backend/app/modules/motion/motion_event.py
@@ -1,1 +1,19 @@
+class MotionEvent:
 
+    def __init__(
+
+        self,
+
+        name,
+
+        action,
+
+        parameter=None,
+
+    ):
+
+        self.name = name
+
+        self.action = action
+
+        self.parameter = parameter
