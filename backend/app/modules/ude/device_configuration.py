65c3aee2bd0da90d83f6c721238150c6bb09d386
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 17:57:16 -0300

    Implement DeviceConfiguration class with methods

--- backend/app/modules/ude/device_configuration.py
+++ backend/app/modules/ude/device_configuration.py
@@ -1,1 +1,30 @@
+class DeviceConfiguration:
 
+    def __init__(self):
+
+        self.parameters = {}
+
+    def set(
+        self,
+        name,
+        value,
+    ):
+
+        self.parameters[name] = value
+
+    def get(
+        self,
+        name,
+        default=None,
+    ):
+
+        return self.parameters.get(
+            name,
+            default,
+        )
+
+    def all(self):
+
+        return dict(
+            self.parameters
+        )
