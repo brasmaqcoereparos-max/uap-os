9cdab53ebf3125920f818ea4938a976c8a399533
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 17:42:48 -0300

    Add VirtualDevice class for managing virtual states
    Implement VirtualDevice class with state management

--- backend/app/modules/ude/virtual_device.py
+++ backend/app/modules/ude/virtual_device.py
@@ -1,3 +1,38 @@
-"""
+from app.modules.ude.device import Device
+from app.modules.ude.device_types import DeviceType
+
+
+class VirtualDevice(Device):
+
+    def __init__(
+        self,
+        name,
+    ):
+
+        super().__init__(
+            name,
+            DeviceType.VIRTUAL.value,
+        )
+
+        self.state = {}
+
+    def set(
+        self,
+        name,
+        value,
+    ):
+
+        self.state[name] = value
+
+    def get(
+        self,
+        name,
+        default=None,
+    ):
+
+        return self.state.get(
+            name,
+            default,
+        )"""
 Universal Device Engine
 """
