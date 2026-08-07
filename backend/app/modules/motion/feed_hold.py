6441a172b72b386d165b7c496d9bf8c47fa218d9
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 04:02:27 -0300

    Implement FeedHold class with enable/disable methods

--- backend/app/modules/motion/feed_hold.py
+++ backend/app/modules/motion/feed_hold.py
@@ -1,1 +1,16 @@
+class FeedHold:
 
+    def __init__(self):
+
+        self.active = False
+
+    def enable(self):
+
+        self.active = True
+
+    def disable(self):
+
+        self.active = False
+
+
+feed_hold = FeedHold()
