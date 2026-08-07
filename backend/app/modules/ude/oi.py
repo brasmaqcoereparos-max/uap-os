23a877ab9710177f2aab95a7bf803792087a9434
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 20:31:54 -0300

    Implement DeviceIO class for I/O operations

--- backend/app/modules/ude/oi.py
+++ backend/app/modules/ude/oi.py
@@ -1,1 +1,81 @@
+class DeviceIO:
 
+    def __init__(self):
+
+        self.digital_inputs = {}
+        self.digital_outputs = {}
+
+        self.analog_inputs = {}
+        self.analog_outputs = {}
+
+    def set_digital_input(
+        self,
+        channel,
+        value,
+    ):
+
+        self.digital_inputs[channel] = bool(value)
+
+    def read_digital_input(
+        self,
+        channel,
+    ):
+
+        return self.digital_inputs.get(
+            channel,
+            False,
+        )
+
+    def set_digital_output(
+        self,
+        channel,
+        value,
+    ):
+
+        self.digital_outputs[channel] = bool(value)
+
+    def read_digital_output(
+        self,
+        channel,
+    ):
+
+        return self.digital_outputs.get(
+            channel,
+            False,
+        )
+
+    def set_analog_input(
+        self,
+        channel,
+        value,
+    ):
+
+        self.analog_inputs[channel] = value
+
+    def read_analog_input(
+        self,
+        channel,
+    ):
+
+        return self.analog_inputs.get(
+            channel,
+            0,
+        )
+
+    def set_analog_output(
+        self,
+        channel,
+        value,
+    ):
+
+        self.analog_outputs[channel] = value
+
+    def read_analog_output(
+        self,
+        channel,
+    ):
+
+        return self.analog_outputs.get(
+            channel,
+            0,
+  )
