84c161de4fdc02332600e24f5244a83bc1ad5f3c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 03:55:39 -0300

    Refactor axis attributes to use float types
    Updated position and target attributes to use float values. Added velocity and acceleration attributes.

--- backend/app/modules/motion/axis.py
+++ backend/app/modules/motion/axis.py
@@ -10,10 +10,26 @@ class MotionAxis:
 
         self.name = name
 
-        self.position = 0
+        self.position = 0.0
+
+        self.target = 0.0
 
         self.enabled = True
+
+        self.homed = False
 
         self.limit_min = None
 
         self.limit_max = None
+
+        self.velocity = 100.0
+
+        self.acceleration = 100.0
+
+    def set_home(self):
+
+        self.position = 0.0
+
+        self.target = 0.0
+
+        self.homed = True
