e2ec517e87d59bb48cbbfa34bee08c67df7ad221
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-07 04:11:35 -0300

    Update driver_base.py

--- backend/app/modules/motion/drivers/driver_base.py
+++ backend/app/modules/motion/drivers/driver_base.py
@@ -1,1 +1,29 @@
+class MotionDriverBase:
 
+    def __init__(self):
+
+        self.connected = False
+
+    def connect(self):
+
+        self.connected = True
+
+    def disconnect(self):
+
+        self.connected = False
+
+    def enable(self):
+
+        pass
+
+    def disable(self):
+
+        pass
+
+    def move(self, target):
+
+        raise NotImplementedError
+
+    def stop(self):
+
+        raise NotImplementedError
