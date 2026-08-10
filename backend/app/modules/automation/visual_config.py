a32a95987def39b3d409021eb008bc87879ca646
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 18:11:41 -0300

    Implement VisualConfig class with basic properties

--- backend/app/modules/automation/visual_config.py
+++ backend/app/modules/automation/visual_config.py
@@ -1,1 +1,30 @@
+class VisualConfig:
 
+    def __init__(self):
+
+        self.label = ""
+        self.icon = ""
+        self.category = ""
+        self.color = ""
+        self.position = {
+            "x": 0,
+            "y": 0,
+        }
+
+    def set_position(
+        self,
+        x,
+        y,
+    ):
+
+        self.position = {
+            "x": x,
+            "y": y,
+        }
+
+    def set_label(
+        self,
+        label,
+    ):
+
+        self.label = label
