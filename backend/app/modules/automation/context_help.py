e866c71176cd4dc66a22be65700129a934148479
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 18:13:23 -0300

    Implement ContextHelp class for help item management

--- backend/app/modules/automation/context_help.py
+++ backend/app/modules/automation/context_help.py
@@ -1,1 +1,37 @@
+class ContextHelp:
 
+    def __init__(self):
+
+        self.help_items = {}
+
+    def register(
+        self,
+        key,
+        title,
+        description,
+        example="",
+    ):
+
+        self.help_items[key] = {
+            "title": title,
+            "description": description,
+            "example": example,
+        }
+
+    def get(
+        self,
+        key,
+    ):
+
+        return self.help_items.get(
+            key
+        )
+
+    def list(self):
+
+        return dict(
+            self.help_items
+        )
+
+
+context_help = ContextHelp()
