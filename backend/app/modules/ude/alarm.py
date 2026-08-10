a2274bfde30142e4afd9628ea81ec76c53c9009b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 07:26:03 -0300

    Add AlarmLevel enum and DeviceAlarm class

--- backend/app/modules/ude/alarm.py
+++ backend/app/modules/ude/alarm.py
@@ -1,1 +1,33 @@
+from enum import Enum
 
+
+class AlarmLevel(Enum):
+
+    INFO = "info"
+
+    WARNING = "warning"
+
+    CRITICAL = "critical"
+
+
+class DeviceAlarm:
+
+    def __init__(
+        self,
+        name,
+        level=AlarmLevel.WARNING,
+        message="",
+    ):
+
+        self.name = name
+        self.level = level
+        self.message = message
+        self.active = False
+
+    def activate(self):
+
+        self.active = True
+
+    def deactivate(self):
+
+        self.active = False
