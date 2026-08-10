f8a5441c9ca38f49db012b05bc81573167ebc494
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 20:58:47 -0300

    Rename DeviceRegistry to AutomationDeviceRegistry
    Renamed DeviceRegistry class to AutomationDeviceRegistry and adjusted its methods accordingly.

--- backend/app/modules/automation/device_registry.py
+++ backend/app/modules/automation/device_registry.py
@@ -1,4 +1,45 @@
-class DeviceRegistry:
+class AutomationDeviceRegistry:
+
+    def __init__(self):
+
+        self.devices = {}
+
+    def register(
+        self,
+        device,
+    ):
+
+        self.devices[
+            device.id
+        ] = device
+
+    def unregister(
+        self,
+        device_id,
+    ):
+
+        self.devices.pop(
+            device_id,
+            None,
+        )
+
+    def get(
+        self,
+        device_id,
+    ):
+
+        return self.devices.get(
+            device_id
+        )
+
+    def list(self):
+
+        return list(
+            self.devices.values()
+        )
+
+
+device_registry = AutomationDeviceRegistry()class DeviceRegistry:
 
     def __init__(self):
 
