d9a334b39d40118116ca5dc18a1179a3ffcc8830
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 07:14:07 -0300

    Update relay.py

--- backend/app/modules/ude/relay.py
+++ backend/app/modules/ude/relay.py
@@ -1,1 +1,37 @@
+from app.modules.ude.actuator import Actuator
 
+
+class Relay(Actuator):
+
+    def __init__(
+        self,
+        name,
+    ):
+
+        super().__init__(
+            name,
+            "relay",
+        )
+
+        self.state = False
+
+    def on(self):
+
+        self.state = True
+        self.set(True)
+
+    def off(self):
+
+        self.state = False
+        self.set(False)
+
+    def toggle(self):
+
+        if self.state:
+            self.off()
+        else:
+            self.on()
+
+    def is_on(self):
+
+        return self.state
