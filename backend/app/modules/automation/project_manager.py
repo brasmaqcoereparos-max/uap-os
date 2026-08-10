326cc1602852821d89b0d359a71fcde03424b981
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 14:07:51 -0300

    Implement AutomationProjectManager class

--- backend/app/modules/automation/project_manager.py
+++ backend/app/modules/automation/project_manager.py
@@ -1,1 +1,42 @@
+class AutomationProjectManager:
 
+    def __init__(self):
+
+        self.projects = {}
+
+    def add(
+        self,
+        project,
+    ):
+
+        self.projects[
+            project.name
+        ] = project
+
+    def remove(
+        self,
+        name,
+    ):
+
+        self.projects.pop(
+            name,
+            None,
+        )
+
+    def get(
+        self,
+        name,
+    ):
+
+        return self.projects.get(
+            name
+        )
+
+    def list(self):
+
+        return list(
+            self.projects.values()
+        )
+
+
+project_manager = AutomationProjectManager()
