c7f51972053b019c398ab998426a8beccf780fdc
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 07:30:47 -0300

    Add DeviceData class for managing device information
    Implemented DeviceData class with methods to manage tags, variables, and alarms.

--- backend/app/modules/ude/device_data.py
+++ backend/app/modules/ude/device_data.py
@@ -1,1 +1,60 @@
+class DeviceData:
 
+    def __init__(self):
+
+        self.tags = {}
+        self.variables = {}
+        self.alarms = {}
+
+    def set_tag(
+        self,
+        name,
+        value,
+    ):
+
+        self.tags[name] = value
+
+    def get_tag(
+        self,
+        name,
+        default=None,
+    ):
+
+        return self.tags.get(
+            name,
+            default,
+        )
+
+    def set_variable(
+        self,
+        name,
+        value,
+    ):
+
+        self.variables[name] = value
+
+    def get_variable(
+        self,
+        name,
+        default=None,
+    ):
+
+        return self.variables.get(
+            name,
+            default,
+        )
+
+    def add_alarm(
+        self,
+        alarm,
+    ):
+
+        self.alarms[alarm.name] = alarm
+
+    def active_alarms(self):
+
+        return [
+            alarm
+            for alarm in self.alarms.values()
+            if alarm.active
+          ]
