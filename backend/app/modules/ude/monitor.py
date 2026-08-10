f30d16e304ed5a324dd2e3809fb4c27112f3bbaa
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 07:28:58 -0300

    Implement DeviceMonitor class for device tracking

--- backend/app/modules/ude/monitor.py
+++ backend/app/modules/ude/monitor.py
@@ -1,1 +1,31 @@
 
+import time
+
+
+class DeviceMonitor:
+
+    def __init__(self):
+
+        self.values = {}
+
+    def update(
+        self,
+        device_id,
+        value,
+    ):
+
+        self.values[device_id] = {
+            "value": value,
+            "timestamp": time.time(),
+        }
+
+    def get(
+        self,
+        device_id,
+    ):
+
+        return self.values.get(device_id)
+
+    def all(self):
+
+        return dict(self.values)
