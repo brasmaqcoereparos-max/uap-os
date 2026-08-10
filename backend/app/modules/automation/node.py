d589c1592ac5fcd5ce3ee67e82f16ceda85884af
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 13:59:40 -0300

    Add AutomationNode class with input/output management

--- backend/app/modules/automation/node.py
+++ backend/app/modules/automation/node.py
@@ -1,1 +1,41 @@
-u
+class AutomationNode:
+
+    def __init__(
+        self,
+        node_id,
+        node_type,
+        name="",
+    ):
+
+        self.node_id = node_id
+        self.node_type = node_type
+        self.name = name
+
+        self.inputs = {}
+        self.outputs = {}
+
+        self.parameters = {}
+
+    def set_input(
+        self,
+        name,
+        value,
+    ):
+
+        self.inputs[name] = value
+
+    def set_output(
+        self,
+        name,
+        value,
+    ):
+
+        self.outputs[name] = value
+
+    def set_parameter(
+        self,
+        name,
+        value,
+    ):
+
+        self.parameters[name] = valueu
