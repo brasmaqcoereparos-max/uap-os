ba356d84618027a54992401e6c8d2a9ac4844495
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 10:19:53 -0300

    Implement SequenceExecutor class for executing steps

--- backend/app/modules/automation/sequence_executor.py
+++ backend/app/modules/automation/sequence_executor.py
@@ -1,1 +1,27 @@
+from app.modules.automation.action_executor import (
+    action_executor,
+)
 
+
+class SequenceExecutor:
+
+    def execute(
+        self,
+        sequence,
+        device=None,
+    ):
+
+        for step in sequence.steps:
+
+            if hasattr(
+                step,
+                "action",
+            ):
+
+                action_executor.execute(
+                    step.action,
+                    device,
+                )
+
+
+sequence_executor = SequenceExecutor()
