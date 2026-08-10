32401c4ff362bb199fab78a747bc377985165533
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 07:15:21 -0300

    Implement Valve class with open/close functionality

--- backend/app/modules/ude/valve.py
+++ backend/app/modules/ude/valve.py
@@ -1,1 +1,30 @@
+from app.modules.ude.actuator import Actuator
 
+
+class Valve(Actuator):
+
+    def __init__(
+        self,
+        name,
+    ):
+
+        super().__init__(
+            name,
+            "valve",
+        )
+
+        self.opened = False
+
+    def open(self):
+
+        self.opened = True
+        self.set(1)
+
+    def close(self):
+
+        self.opened = False
+        self.set(0)
+
+    def is_open(self):
+
+        return self.opened
