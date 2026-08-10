88b19424638efe2d50d33536e131a9faa276c1be
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 07:19:33 -0300

    Add IOType enum and IOChannel class

--- backend/app/modules/ude/io_channel.py
+++ backend/app/modules/ude/io_channel.py
@@ -1,1 +1,39 @@
+from enum import Enum
 
+
+class IOType(Enum):
+
+    DIGITAL_INPUT = "digital_input"
+
+    DIGITAL_OUTPUT = "digital_output"
+
+    ANALOG_INPUT = "analog_input"
+
+    ANALOG_OUTPUT = "analog_output"
+
+    PWM = "pwm"
+
+
+class IOChannel:
+
+    def __init__(
+        self,
+        channel,
+        io_type,
+    ):
+
+        self.channel = channel
+        self.io_type = io_type
+        self.value = 0
+        self.enabled = True
+
+    def write(
+        self,
+        value,
+    ):
+
+        self.value = value
+
+    def read(self):
+
+        return self.value
