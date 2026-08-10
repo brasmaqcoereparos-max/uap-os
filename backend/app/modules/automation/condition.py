89858f190625354a94f67411a89ec58f9fa8c6bf
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 10:06:12 -0300

    Add AutomationCondition class with evaluate method
    Implement AutomationCondition class for evaluating conditions.

--- backend/app/modules/automation/condition.py
+++ backend/app/modules/automation/condition.py
@@ -1,1 +1,34 @@
+class AutomationCondition:
 
+    def __init__(
+        self,
+        left,
+        operator,
+        right,
+    ):
+
+        self.left = left
+        self.operator = operator
+        self.right = right
+
+    def evaluate(self):
+
+        if self.operator == "==":
+            return self.left == self.right
+
+        if self.operator == "!=":
+            return self.left != self.right
+
+        if self.operator == ">":
+            return self.left > self.right
+
+        if self.operator == "<":
+            return self.left < self.right
+
+        if self.operator == ">=":
+            return self.left >= self.right
+
+        if self.operator == "<=":
+            return self.left <= self.right
+
+        return False
