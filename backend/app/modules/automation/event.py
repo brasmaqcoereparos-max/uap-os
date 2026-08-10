c5eeca4128b84ff48d58a41ed865ae0ab84def65
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 10:26:17 -0300

    Implement AutomationEvent and AutomationEventBus classes

--- backend/app/modules/automation/event.py
+++ backend/app/modules/automation/event.py
@@ -1,1 +1,44 @@
+class AutomationEvent:
 
+    def __init__(
+        self,
+        name,
+        data=None,
+    ):
+
+        self.name = name
+
+        self.data = data or {}
+
+
+class AutomationEventBus:
+
+    def __init__(self):
+
+        self.listeners = {}
+
+    def subscribe(
+        self,
+        event_name,
+        callback,
+    ):
+
+        self.listeners.setdefault(
+            event_name,
+            [],
+        ).append(callback)
+
+    def emit(
+        self,
+        event,
+    ):
+
+        for callback in self.listeners.get(
+            event.name,
+            [],
+        ):
+
+            callback(event)
+
+
+event_bus = AutomationEventBus()
