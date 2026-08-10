e9ddaf9114046285dd1a1401b356e88054ca25a0
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 10:23:35 -0300

    Add AutomationDecision class with evaluate method

--- backend/app/modules/automation/decision.py
+++ backend/app/modules/automation/decision.py
@@ -1,1 +1,14 @@
+class AutomationDecision:
 
+    def __init__(self):
+
+        self.result = False
+
+    def evaluate(
+        self,
+        condition,
+    ):
+
+        self.result = condition.evaluate()
+
+        return self.result
