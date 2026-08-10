3c348447501c0bcb2f077501a69fa901ec88bd5c
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 17:12:51 -0300

    Update block.py

--- backend/app/modules/automation/block.py
+++ backend/app/modules/automation/block.py
@@ -1,1 +1,38 @@
+class AutomationBlock:
 
+    def __init__(
+        self,
+        block_type,
+        name,
+        description="",
+    ):
+
+        self.block_type = block_type
+        self.name = name
+        self.description = description
+
+        self.inputs = []
+        self.outputs = []
+        self.parameters = {}
+
+    def add_input(
+        self,
+        name,
+    ):
+
+        self.inputs.append(name)
+
+    def add_output(
+        self,
+        name,
+    ):
+
+        self.outputs.append(name)
+
+    def set_parameter(
+        self,
+        name,
+        value,
+    ):
+
+        self.parameters[name] = value
