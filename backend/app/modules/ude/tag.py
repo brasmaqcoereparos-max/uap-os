e2f4f19b8da5d89af28e849c294c34f56c7b9ea7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-10 07:22:24 -0300

    Add DeviceTag class with basic methods

--- backend/app/modules/ude/tag.py
+++ backend/app/modules/ude/tag.py
@@ -1,1 +1,20 @@
+class DeviceTag:
 
+    def __init__(
+        self,
+        name,
+        value=None,
+        description="",
+    ):
+
+        self.name = name
+        self.value = value
+        self.description = description
+
+    def set(self, value):
+
+        self.value = value
+
+    def get(self):
+
+        return self.value
