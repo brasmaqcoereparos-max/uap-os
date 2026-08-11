7f53a9a343030e5d9b3ac9c9b5ffc127c10db5cc
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-11 11:34:28 -0300

    Implement LimitSwitch class with basic functionality

--- backend/app/modules/automation/limit_switch.py
+++ backend/app/modules/automation/limit_switch.py
@@ -1,1 +1,18 @@
+class LimitSwitch:
 
+    def __init__(self, name):
+
+        self.name = name
+        self.triggered = False
+
+    def trigger(self):
+
+        self.triggered = True
+
+    def reset(self):
+
+        self.triggered = False
+
+    def is_triggered(self):
+
+        return self.triggered
