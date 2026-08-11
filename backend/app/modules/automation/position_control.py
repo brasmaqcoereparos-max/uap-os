042165d66e3b600da09f575df2a82c195a71ed84
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-11 11:25:55 -0300

    Add PositionController class for device position management

--- backend/app/modules/automation/position_control.py
+++ backend/app/modules/automation/position_control.py
@@ -1,1 +1,17 @@
+class PositionController:
 
+    def __init__(self, device):
+
+        self.device = device
+
+    def move_to(self, position):
+
+        self.device.position = position
+
+    def move_by(self, distance):
+
+        self.device.position += distance
+
+    def get_position(self):
+
+        return self.device.position
