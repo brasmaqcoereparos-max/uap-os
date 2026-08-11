dcee0ecf6d74321dba501d557b1dcc43428fd1a3
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-11 11:30:57 -0300

    Implement ServoController class for angle control

--- backend/app/modules/automation/servo_control.py
+++ backend/app/modules/automation/servo_control.py
@@ -1,1 +1,21 @@
+class ServoController:
 
+    def __init__(self, device):
+
+        self.device = device
+        self.angle = 0
+
+    def set_angle(self, angle):
+
+        if angle < 0:
+            angle = 0
+
+        if angle > 180:
+            angle = 180
+
+        self.angle = angle
+        self.device.position = angle
+
+    def get_angle(self):
+
+        return self.angle
