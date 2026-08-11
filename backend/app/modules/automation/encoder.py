01a0a68e4ccf672dfc8d2d2c82bb9703983674c2
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-11 11:33:15 -0300

    Update encoder.py

--- backend/app/modules/automation/encoder.py
+++ backend/app/modules/automation/encoder.py
@@ -1,1 +1,23 @@
+class Encoder:
 
+    def __init__(self):
+
+        self.position = 0
+        self.velocity = 0
+
+    def update(
+        self,
+        position,
+        velocity=0,
+    ):
+
+        self.position = position
+        self.velocity = velocity
+
+    def get_position(self):
+
+        return self.position
+
+    def get_velocity(self):
+
+        return self.velocity
