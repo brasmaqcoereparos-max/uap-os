73ac463a8ae1d806d131defa932d4ad9db864974
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-11 14:32:47 -0300

    Implement RobotArm class with axis management

--- backend/app/modules/automation/robot_arm.py
+++ backend/app/modules/automation/robot_arm.py
@@ -1,1 +1,50 @@
+class RobotArm:
 
+    def __init__(self, name="Robot Arm"):
+
+        self.name = name
+        self.axes = {}
+        self.enabled = False
+
+    def add_axis(
+        self,
+        axis_id,
+        position=0,
+    ):
+
+        self.axes[axis_id] = {
+            "position": position,
+            "speed": 0,
+        }
+
+    def set_position(
+        self,
+        axis_id,
+        position,
+    ):
+
+        if axis_id not in self.axes:
+            return False
+
+        self.axes[axis_id]["position"] = position
+
+        return True
+
+    def get_position(
+        self,
+        axis_id,
+    ):
+
+        axis = self.axes.get(axis_id)
+
+        if axis is None:
+            return None
+
+        return axis["position"]
+
+    def get_positions(self):
+
+        return {
+            axis_id: data["position"]
+            for axis_id, data in self.axes.items()
+        }
