5c13739bfe31c864b1b28219b1a14799632622e8
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-11 19:30:07 -0300

    Implement RobotExecution class with control methods

--- backend/app/modules/automation/robot_execution.py
+++ backend/app/modules/automation/robot_execution.py
@@ -1,1 +1,34 @@
+class RobotExecution:
 
+    def __init__(self):
+
+        self.running = False
+        self.paused = False
+
+    def start(self):
+
+        self.running = True
+        self.paused = False
+
+    def pause(self):
+
+        if self.running:
+            self.paused = True
+
+    def resume(self):
+
+        if self.running:
+            self.paused = False
+
+    def stop(self):
+
+        self.running = False
+        self.paused = False
+
+    def is_running(self):
+
+        return self.running
+
+    def is_paused(self):
+
+        return self.paused
