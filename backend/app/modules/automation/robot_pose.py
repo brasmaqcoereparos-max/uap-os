3f54cbd4247d340def32ab91466c92cfb2ed5cb6
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-11 14:34:14 -0300

    Implement RobotPose class with axis management

--- backend/app/modules/automation/robot_pose.py
+++ backend/app/modules/automation/robot_pose.py
@@ -1,1 +1,34 @@
+class RobotPose:
 
+    def __init__(
+        self,
+        name="Position",
+    ):
+
+        self.name = name
+        self.axes = {}
+        self.speed = 0
+        self.wait = 0
+
+    def set_axis(
+        self,
+        axis_id,
+        position,
+    ):
+
+        self.axes[axis_id] = position
+
+    def get_axis(
+        self,
+        axis_id,
+        default=None,
+    ):
+
+        return self.axes.get(
+            axis_id,
+            default,
+        )
+
+    def get_all(self):
+
+        return dict(self.axes)
