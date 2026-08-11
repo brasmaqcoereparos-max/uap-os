8a71ea09ed6b9d4656206291ba26637827b1815b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-11 11:35:59 -0300

    Add HomingController class for device homing

--- backend/app/modules/automation/homing.py
+++ backend/app/modules/automation/homing.py
@@ -1,1 +1,19 @@
+class HomingController:
 
+    def __init__(self, device):
+
+        self.device = device
+        self.homed = False
+
+    def home(self):
+
+        self.device.position = 0
+        self.homed = True
+
+    def is_homed(self):
+
+        return self.homed
+
+    def reset(self):
+
+        self.homed = False
