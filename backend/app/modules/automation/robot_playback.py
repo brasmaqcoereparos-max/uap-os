f4a0c6621f702f459a73a99fb33bec220a572d61
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-11 14:45:54 -0300

    Implement RobotPlayback class for automation control

--- backend/app/modules/automation/robot_playback.py
+++ backend/app/modules/automation/robot_playback.py
@@ -1,1 +1,41 @@
+class RobotPlayback:
 
+    def __init__(self):
+
+        self.running = False
+        self.index = 0
+
+    def start(self):
+
+        self.running = True
+        self.index = 0
+
+    def stop(self):
+
+        self.running = False
+
+    def next(
+        self,
+        segments,
+    ):
+
+        if not self.running:
+            return None
+
+        if self.index >= len(segments):
+
+            self.running = False
+
+            return None
+
+        segment = segments[
+            self.index
+        ]
+
+        self.index += 1
+
+        return segment
+
+    def is_running(self):
+
+        return self.running
