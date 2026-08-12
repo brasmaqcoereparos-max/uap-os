2197cd10d4a6099aca12ca76bd95c14f5bf4d721
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-12 01:06:46 -0300

    Create teach_commands.py

---
+++ backend/app/modules/automation/teach_commands.py
@@ -0,0 +1,10 @@
+class TeachCommands:
+
+    JOG = "jog"
+    RECORD = "record"
+    DELETE = "delete"
+    EDIT = "edit"
+    PLAY = "play"
+    PAUSE = "pause"
+    STOP = "stop"
+    SAVE = "save"
