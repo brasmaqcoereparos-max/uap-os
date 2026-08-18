f5774873b0a9ece04d855363154c5df1abff85c9
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-18 10:56:48 -0300

    Update __init__.py

--- backend/app/modules/uhal/__init__.py
+++ backend/app/modules/uhal/__init__.py
@@ -1,3 +1,15 @@
-"""
-Universal Hardware Abstraction Layer (UHAL)
-"""
+from .hardware_abstraction import (
+    DeviceState,
+    DeviceType,
+    HardwareAbstractionLayer,
+    HardwareDevice,
+    HardwarePort,
+)
+
+__all__ = [
+    "DeviceState",
+    "DeviceType",
+    "HardwareAbstractionLayer",
+    "HardwareDevice",
+    "HardwarePort",
+]
