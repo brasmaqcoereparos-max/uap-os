9f316ce7c91584f981f0e8a0626ae6467c38bba4
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-18 21:16:18 -0300

    Update __init__.py to include new service
    Added create_default_discovery_service to exports.

--- backend/app/modules/devices/discovery/__init__.py
+++ backend/app/modules/devices/discovery/__init__.py
@@ -1,3 +1,11 @@
+from .providers import create_default_discovery_service
 from .service import DeviceDiscoveryService
+from .network_provider import NetworkDiscoveryProvider
+
+__all__ = [
+    "DeviceDiscoveryService",
+    "NetworkDiscoveryProvider",
+    "create_default_discovery_service",
+]from .service import DeviceDiscoveryService
 
 __all__ = ["DeviceDiscoveryService"]
