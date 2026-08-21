73a6d5b7a1b16953e9846cec673b63a5ba612006
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-21 15:30:42 -0300

    Add enable/disable methods to DeviceBase class

--- backend/app/modules/simulator/programming/simulator/device/device_base.py
+++ backend/app/modules/simulator/programming/simulator/device/device_base.py
@@ -1,21 +1,32 @@
+"""
+Classe base para dispositivos do simulador UAP.
+"""
+
+
 class DeviceBase:
 
     def __init__(
-
         self,
-
         name,
-
     ):
 
         self.name = name
+        self.enabled = True
+
+    def enable(self):
 
         self.enabled = True
 
-    def update(self):
+    def disable(self):
 
+        self.enabled = False
+
+    def is_enabled(self):
+
+        return self.enabled
+
+    def update(self):
         pass
 
     def reset(self):
-
         pass
