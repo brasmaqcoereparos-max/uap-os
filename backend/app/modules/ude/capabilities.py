dc8aa64a0301cc5e6b55cf355acb30b78628b7b7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-21 06:44:38 -0300

    Enhance DeviceCapabilities with new methods
    Added methods to remove communication protocols and convert device capabilities to a dictionary format.

--- backend/app/modules/ude/capabilities.py
+++ backend/app/modules/ude/capabilities.py
@@ -1,14 +1,21 @@
+"""
+Universal Device Engine
+Capacidades dos dispositivos.
+"""
+
+
 class DeviceCapabilities:
 
     def __init__(self):
-
         self.inputs = 0
         self.outputs = 0
+
         self.analog_inputs = 0
         self.analog_outputs = 0
 
         self.pwm = False
         self.encoder = False
+
         self.communication = []
 
         self.motion = False
@@ -19,21 +26,47 @@ class DeviceCapabilities:
         self,
         protocol,
     ):
-
         if protocol not in self.communication:
-            self.communication.append(protocol)
+            self.communication.append(
+                protocol
+            )
+
+    def remove_communication(
+        self,
+        protocol,
+    ):
+        if protocol in self.communication:
+            self.communication.remove(
+                protocol
+            )
+            return True
+
+        return False
 
     def supports(
         self,
         feature,
     ):
-
         return bool(
             getattr(
                 self,
                 feature,
                 False,
             )
-        )"""
-Universal Device Engine
-"""
+        )
+
+    def to_dict(self):
+        return {
+            "inputs": self.inputs,
+            "outputs": self.outputs,
+            "analog_inputs": self.analog_inputs,
+            "analog_outputs": self.analog_outputs,
+            "pwm": self.pwm,
+            "encoder": self.encoder,
+            "communication": list(
+                self.communication
+            ),
+            "motion": self.motion,
+            "sensing": self.sensing,
+            "vision": self.vision,
+        }
