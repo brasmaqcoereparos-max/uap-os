1b926939e272605dfc4bd9fa1d328537cb180adb
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-21 21:24:50 -0300

    Update instruction_get to use runtime state

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_get.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_get.py
@@ -1,16 +1,23 @@
+"""
+Instrução GET do UAP.
+
+Obtém o valor de uma variável do runtime.
+"""
+
 from app.modules.simulator.programming.simulator.runtime.runtime_context import (
     runtime_context,
 )
 
 
 def instruction_get(
-
     variable,
-
+    default=None,
 ):
+    """
+    GET variável
+    """
 
-    return runtime_context.variables.get(
-
+    return runtime_context.state.get(
         variable,
-
+        default,
     )
