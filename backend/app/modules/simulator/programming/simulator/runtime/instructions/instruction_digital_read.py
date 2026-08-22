feb04e6c6d142ad1c840e2c44535871e3e72eb25
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-22 03:22:37 -0300

    Refactor instruction_digital_read to use runtime_context
    Updated the instruction_digital_read function to use runtime_context instead of runtime_gpio for reading the digital state of a pin.

--- backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_digital_read.py
+++ backend/app/modules/simulator/programming/simulator/runtime/instructions/instruction_digital_read.py
@@ -1,12 +1,21 @@
-from app.modules.simulator.programming.simulator.runtime.runtime_gpio import (
-    runtime_gpio,
+"""
+Instrução DIGITAL_READ do UAP.
+
+Lê o estado digital de um pino.
+"""
+
+from app.modules.simulator.programming.simulator.runtime.runtime_context import (
+    runtime_context,
 )
 
 
 def instruction_digital_read(
     pin,
 ):
+    """
+    DIGITAL_READ pino
+    """
 
-    return runtime_gpio.read(
-        pin,
+    return runtime_context.gpio.read(
+        int(pin)
     )
