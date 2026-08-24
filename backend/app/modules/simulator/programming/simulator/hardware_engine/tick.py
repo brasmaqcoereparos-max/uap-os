a1d93dee1228632fb091ffe51f54e55d8915d0d3
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-24 18:18:32 -0300

    Refactor Tick class for FPS handling
    Refactor Tick class to initialize FPSController and set FPS.

--- backend/app/modules/simulator/programming/simulator/hardware_engine/tick.py
+++ backend/app/modules/simulator/programming/simulator/hardware_engine/tick.py
@@ -1,18 +1,34 @@
 import time
 
 from app.modules.simulator.programming.simulator.hardware_engine.fps_controller import (
-    fps_controller,
+    FPSController,
 )
 
 
 class Tick:
 
+    def __init__(
+        self,
+        fps=60,
+    ):
+
+        self.fps_controller = FPSController(
+            fps
+        )
+
+    def set_fps(
+        self,
+        fps,
+    ):
+
+        self.fps_controller.set_fps(
+            fps
+        )
+
     def wait(self):
 
         time.sleep(
-
-            fps_controller.interval,
-
+            self.fps_controller.interval()
         )
 
 
