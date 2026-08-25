b29f2df95e43dd85c673030bd6cb3bb97069e205
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-25 05:02:23 -0300

    Add driver methods for digital and analog operations

--- backend/app/modules/uhal/drivers/driver_base.py
+++ backend/app/modules/uhal/drivers/driver_base.py
@@ -37,3 +37,61 @@ class DriverBase(UHALBase):
     def is_initialized(self):
 
         return self.initialized
+
+    def connect(self):
+
+        return self.initialize()
+
+    def disconnect(self):
+
+        return self.shutdown()
+
+    def update(self):
+
+        return True
+
+    def digital_write(
+        self,
+        pin,
+        value,
+    ):
+
+        raise NotImplementedError
+
+    def digital_read(
+        self,
+        pin,
+    ):
+
+        raise NotImplementedError
+
+    def analog_read(
+        self,
+        pin,
+    ):
+
+        raise NotImplementedError
+
+    def analog_write(
+        self,
+        pin,
+        value,
+    ):
+
+        raise NotImplementedError
+
+    def pwm_write(
+        self,
+        pin,
+        duty,
+    ):
+
+        raise NotImplementedError
+
+    def pwm_frequency(
+        self,
+        pin,
+        frequency,
+    ):
+
+        raise NotImplementedError
