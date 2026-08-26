2066eb52f7ec403c84b1d9aea9d687bacd3dab63
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-26 07:44:34 -0300

    Refactor RuntimeService to use new modules

--- backend/app/runtime/runtime_service.py
+++ backend/app/runtime/runtime_service.py
@@ -1,23 +1,27 @@
-from app.runtime.runtime_controller import (
-    runtime_controller,
-)
+from app.runtime.runtime_boot import runtime_boot
+from app.runtime.runtime_executor import runtime_executor
+from app.runtime.runtime_health import runtime_health
 
 
 class RuntimeService:
 
     def start(self):
-        return runtime_controller.start()
+        return runtime_boot.start()
 
     def stop(self):
-        return runtime_controller.stop()
+        return runtime_boot.stop()
 
     def status(self):
-        return runtime_controller.status()
+        return runtime_health.check()
+
+    def diagnostics(self):
+        return runtime_health.diagnostics()
 
     def execute(self, command):
-        return runtime_controller.execute(
-            command
-        )
+        return runtime_executor.execute(command)
+
+    def execute_many(self, commands):
+        return runtime_executor.execute_many(commands)
 
 
 runtime_service = RuntimeService()
