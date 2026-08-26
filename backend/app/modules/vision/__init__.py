c88a2159b26b9747b6169cc2b000a010e920074e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-26 08:42:08 -0300

    Create __init__.py

---
+++ backend/app/modules/vision/__init__.py
@@ -0,0 +1,9 @@
+from app.modules.vision.vision_manager import vision_manager
+from app.modules.vision.camera_manager import camera_manager
+from app.modules.vision.vision_events import vision_events
+
+__all__ = [
+    "vision_manager",
+    "camera_manager",
+    "vision_events",
+]
