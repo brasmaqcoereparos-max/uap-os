fec828a6ee09935e59281776e7d65b87ab10dd48
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-25 22:39:56 -0300

    Add driver management methods to UHALBridge

--- backend/app/modules/ude/uhal_bridge.py
+++ backend/app/modules/ude/uhal_bridge.py
@@ -6,12 +6,60 @@ from app.modules.uhal.hal_manager import (
 class UHALBridge:
 
     def current_driver(self):
-
         return hal_manager.current()
 
-    def available_drivers(self):
+    def current_board(self):
+        return hal_manager.current_board()
 
+    def available_drivers(self):
         return hal_manager.available()
+
+    def load_driver(
+        self,
+        board,
+    ):
+        return hal_manager.load(
+            board
+        )
+
+    def unload_driver(self):
+        return hal_manager.unload()
+
+    def digital_write(
+        self,
+        pin,
+        value,
+    ):
+        return hal_manager.digital_write(
+            pin,
+            value,
+        )
+
+    def digital_read(
+        self,
+        pin,
+    ):
+        return hal_manager.digital_read(
+            pin
+        )
+
+    def analog_write(
+        self,
+        pin,
+        value,
+    ):
+        return hal_manager.analog_write(
+            pin,
+            value,
+        )
+
+    def analog_read(
+        self,
+        pin,
+    ):
+        return hal_manager.analog_read(
+            pin
+        )
 
 
 uhal_bridge = UHALBridge()
