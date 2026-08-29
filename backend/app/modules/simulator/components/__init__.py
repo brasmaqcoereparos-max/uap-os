ca8e4acf98d75ba3531ed4079c55671d54a6a7d7
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-28 22:03:20 -0300

    Import virtual sensor components and define __all__

--- backend/app/modules/simulator/components/__init__.py
+++ backend/app/modules/simulator/components/__init__.py
@@ -1,1 +1,23 @@
+from app.modules.simulator.components.virtual_sensor import (
+    VirtualSensor,
+)
 
+from app.modules.simulator.components.virtual_temperature import (
+    VirtualTemperature,
+)
+
+from app.modules.simulator.components.virtual_humidity import (
+    VirtualHumidity,
+)
+
+from app.modules.simulator.components.virtual_ultrasonic import (
+    VirtualUltrasonic,
+)
+
+
+__all__ = [
+    "VirtualSensor",
+    "VirtualTemperature",
+    "VirtualHumidity",
+    "VirtualUltrasonic",
+]
