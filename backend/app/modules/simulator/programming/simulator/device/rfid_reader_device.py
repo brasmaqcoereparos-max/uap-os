f5b4e5085266f72d29846b13610af4505b45af82
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-30 03:43:19 -0300

    Refactor RFIDReaderDevice class initialization and methods

--- backend/app/modules/simulator/programming/simulator/device/rfid_reader_device.py
+++ backend/app/modules/simulator/programming/simulator/device/rfid_reader_device.py
@@ -7,38 +7,119 @@ from app.modules.simulator.programming.simulator.device.device_base import (
 )
 
 
-class RFIDReaderDevice(DeviceBase):
+class RFIDReaderDevice(
+    DeviceBase
+):
 
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(
+        self,
+        name,
+    ):
+        super().__init__(
+            name=name,
+            category="reader",
+            description="Leitor RFID",
+            icon="rfid",
+        )
 
         self.uid = None
+        self.last_uid = None
+
         self.data = {}
 
-    def scan(self, uid):
+        self.scan_count = 0
+
+    def scan(
+        self,
+        uid,
+    ):
+        if not self.enabled:
+            return False
+
+        if uid is None:
+            return None
 
         self.uid = str(uid)
+        self.last_uid = self.uid
 
-    def read_uid(self):
+        self.scan_count += 1
 
         return self.uid
 
-    def write_data(self, key, value):
+    def read_uid(self):
+        return self.uid
 
-        self.data[key] = value
+    def has_tag(self):
+        return self.uid is not None
 
-    def read_data(self, key):
+    def write_data(
+        self,
+        key,
+        value,
+    ):
+        if self.uid is None:
+            return False
 
-        return self.data.get(key)
+        self.data[
+            str(key)
+        ] = value
+
+        return value
+
+    def read_data(
+        self,
+        key,
+    ):
+        return self.data.get(
+            str(key)
+        )
+
+    def remove_data(
+        self,
+        key,
+    ):
+        return self.data.pop(
+            str(key),
+            None,
+        )
+
+    def clear_tag(self):
+        self.uid = None
+
+        return True
 
     def clear(self):
-
         self.uid = None
+        self.last_uid = None
         self.data.clear()
 
+        return True
+
     def update(self):
-        pass
+        return self.uid
 
     def reset(self):
+        self.uid = None
+        self.last_uid = None
+        self.data.clear()
+        self.scan_count = 0
 
-        self.clear()
+        return True
+
+    def to_dict(self):
+        data = super().to_dict()
+
+        data.update({
+            "uid": self.uid,
+            "last_uid": (
+                self.last_uid
+            ),
+            "data": dict(
+                self.data
+            ),
+            "scan_count": (
+                self.scan_count
+            ),
+        })
+
+        return data
