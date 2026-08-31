ce386bbeb55a09cf6c90252554ce0d845e09af8d
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-08-31 05:37:11 -0300

    Update __init__.py

--- backend/app/modules/simulator/boards/__init__.py
+++ backend/app/modules/simulator/boards/__init__.py
@@ -1,1 +1,23 @@
+from app.modules.simulator.boards.virtual_board import (
+    VirtualBoard,
+)
 
+from app.modules.simulator.boards.arduino_uno import (
+    ArduinoUNO,
+)
+
+from app.modules.simulator.boards.esp32 import (
+    ESP32Board,
+)
+
+from app.modules.simulator.boards.raspberry_pi import (
+    RaspberryPiBoard,
+)
+
+
+__all__ = [
+    "VirtualBoard",
+    "ArduinoUNO",
+    "ESP32Board",
+    "RaspberryPiBoard",
+]
