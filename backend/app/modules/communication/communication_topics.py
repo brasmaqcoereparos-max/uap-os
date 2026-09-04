5ba54f9236dba5d8d99569c6b6a808e6f7582d6a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-09-04 11:10:25 -0300

    Create communication_topics.py

---
+++ backend/app/modules/communication/communication_topics.py
@@ -0,0 +1,37 @@
+class CommunicationTopics:
+    DEVICE_EVENT = (
+        "device.event"
+    )
+
+    DEVICE_COMMAND = (
+        "device.command"
+    )
+
+    RUNTIME_EVENT = (
+        "runtime.event"
+    )
+
+    AUTOMATION_EVENT = (
+        "automation.event"
+    )
+
+    UI_EVENT = (
+        "ui.event"
+    )
+
+    VOICE_EVENT = (
+        "voice.event"
+    )
+
+    AI_EVENT = (
+        "ai.event"
+    )
+
+    SYSTEM_EVENT = (
+        "system.event"
+    )
+
+
+communication_topics = (
+    CommunicationTopics()
+  )
