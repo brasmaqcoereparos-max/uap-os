e766b2844ab344895a5b8de367b1760461e7c21a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-09-07 02:55:35 -0300

    Create router.py

---
+++ backend/app/modules/security/router.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter
+
+from app.modules.security.activation_api import (
+    router as activation_router,
+)
+from app.modules.security.health_api import (
+    router as health_router,
+)
+from app.modules.security.identity_api import (
+    router as identity_router,
+)
+from app.modules.security.license_api import (
+    router as license_router,
+)
+from app.modules.security.signing_api import (
+    router as signing_router,
+)
+
+
+router = APIRouter(
+    prefix="/security",
+    tags=["Security"],
+)
+
+
+router.include_router(
+    health_router
+)
+
+router.include_router(
+    identity_router
+)
+
+router.include_router(
+    license_router
+)
+
+router.include_router(
+    activation_router
+)
+
+router.include_router(
+    signing_router
+)
