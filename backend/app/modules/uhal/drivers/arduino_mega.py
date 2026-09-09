1e35e4a1af8851ca7c15005f2b6c91c00db71f7e
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-09-09 05:13:40 -0300

    Update arduino_mega.py

--- backend/app/modules/uhal/drivers/arduino_mega.py
+++ backend/app/modules/uhal/drivers/arduino_mega.py
@@ -5,16 +5,61 @@ from app.modules.uhal.drivers.simulator import (
 
 class ArduinoMegaDriver(SimulatorDriver):
 
-    BOARD_NAME = "Arduino Mega"
+    BOARD_NAME = (
+        "Arduino Mega"
+    )
+
+    MANUFACTURER = (
+        "Arduino"
+    )
 
     GPIO_COUNT = 70
 
     PWM_PINS = [
-
-        2, 3, 4, 5, 6, 7,
-
-        8, 9, 10, 11, 12, 13
-
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
     ]
 
     ADC_CHANNELS = 16
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.board.name = (
+            self.BOARD_NAME
+        )
+
+        self.board.manufacturer = (
+            self.MANUFACTURER
+        )
+
+        self.board.capabilities.gpio = (
+            self.GPIO_COUNT
+        )
+
+        self.board.capabilities.pwm = (
+            len(
+                self.PWM_PINS
+            )
+        )
+
+        self.board.capabilities.adc = (
+            self.ADC_CHANNELS
+        )
+
+        self.board.capabilities.uart = 4
+
+        self.board.capabilities.i2c = 1
+
+        self.board.capabilities.spi = 1
