3d855e84aa4151ca7b529bbc444bcbb17c656b9b
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-09-09 05:05:46 -0300

    Update esp8266.py

--- backend/app/modules/uhal/drivers/esp8266.py
+++ backend/app/modules/uhal/drivers/esp8266.py
@@ -7,8 +7,46 @@ class ESP8266Driver(SimulatorDriver):
 
     BOARD_NAME = "ESP8266"
 
+    MANUFACTURER = "Espressif"
+
     GPIO_COUNT = 17
 
-    PWM_PINS = list(range(17))
+    PWM_PINS = list(
+        range(17)
+    )
 
     ADC_CHANNELS = 1
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.board.name = (
+            self.BOARD_NAME
+        )
+
+        self.board.manufacturer = (
+            self.MANUFACTURER
+        )
+
+        self.board.capabilities.gpio = (
+            self.GPIO_COUNT
+        )
+
+        self.board.capabilities.pwm = (
+            len(
+                self.PWM_PINS
+            )
+        )
+
+        self.board.capabilities.adc = (
+            self.ADC_CHANNELS
+        )
+
+        self.board.capabilities.uart = 1
+
+        self.board.capabilities.i2c = 1
+
+        self.board.capabilities.spi = 1
+
+        self.board.capabilities.wifi = True
