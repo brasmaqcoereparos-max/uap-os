037a0b46655301e9b0c306ae6e073487c306807a
brasmaqcoereparos-max <brasmaqcoereparos@gmail.com>
2026-09-21 16:14:24 -0300

    Create __init__.py

---
+++ backend/app/modules/products/__init__.py
@@ -0,0 +1,16 @@
+from app.modules.products.catalog_service import (
+    product_catalog_service,
+)
+from app.modules.products.product_service import (
+    product_service,
+)
+from app.modules.products.supplier_service import (
+    supplier_service,
+)
+
+
+__all__ = [
+    "product_catalog_service",
+    "product_service",
+    "supplier_service",
+]
